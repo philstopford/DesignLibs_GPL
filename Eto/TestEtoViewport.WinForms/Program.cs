37ce6c639ac8c684e1d4d058d9de627b057e68a7
philstopford <phil.stopford@gmail.com>
2021-01-11 16:10:33 -0600

    Adding Eto.OpenTK tests.

---
+++ Eto/TestEtoViewport.WinForms/Program.cs
@@ -0,0 +1,21 @@
+﻿using Eto.OpenTK;
+using Eto.Forms;
+using System;
+
+namespace TestEtoGl.WPF_Framebuffer
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var platform = new Eto.WinForms.Platform();
+            platform.Add<GLSurface.IHandler>(() => new Eto.OpenTK.WinForms.WinGLSurfaceHandler());
+
+            new Application(platform).Run(new MainForm());
+        }
+    }
+}

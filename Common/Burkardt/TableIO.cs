5989f1472a1251f00a8924df8f95aafd900b4509
philstopford <phil.stopford@gmail.com>
2021-05-23 14:37:43 -0500

    Update TableIO.cs

--- Common/Burkardt/TableIO.cs
+++ Common/Burkardt/TableIO.cs
@@ -19,7 +19,7 @@ namespace TableIO
     
     public static class TableReader
     {
-        static int s_len_trim(string line)
+        public static int s_len_trim(string line)
         {
             //****************************************************************************80
             //
@@ -932,5 +932,271 @@ namespace TableIO
             }
         }
         
+    }
+
+    public static class TableSummary
+    {
+        static void r8block_print ( int l, int m, int n, double[] a, string title )
+
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    R8BLOCK_PRINT prints a double precision block (a 3D matrix).
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    13 October 2005
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, int L, M, N, the dimensions of the block.
+        //
+        //    Input, double A[L*M*N], the matrix to be printed.
+        //
+        //    Input, char *TITLE, a title to be printed first.
+        //    TITLE may be blank.
+        //
+        {
+            if ( 0 < TableReader.s_len_trim ( title ) )
+            {
+                Console.WriteLine();
+                Console.WriteLine(title);
+            }
+
+            for (int k = 1; k <= n; k++ )
+            {
+                Console.WriteLine();
+                Console.WriteLine("  K = " + k);
+                Console.WriteLine();
+                for (int jlo = 1; jlo <= m; jlo = jlo + 5 )
+                {
+                    int jhi = Math.Min( jlo + 4, m );
+                    Console.WriteLine();
+                    string cout = "      ";
+                    for (int j = jlo; j <= jhi; j++ )
+                    {
+                        string t = j + "       ";
+                        cout += t.PadLeft(7);
+                    }
+                    Console.WriteLine(cout);
+                    Console.WriteLine();
+                    cout = "";
+                    for (int i = 1; i <= l; i++ )
+                    {
+                        string t = i.ToString();
+                        t = t.PadLeft(4);
+                        cout += "  " + t;
+                        for (int j = jlo; j <= jhi; j++ )
+                        {
+                            t = a[i-1+(j-1)*l+(k-1)*l*m].ToString();
+                            t = t.PadLeft(12);
+                            cout += "  " +t;
+                        }
+                        Console.WriteLine(cout);
+                    }
+                }
+            }
+        }
+
+
+        static void r8mat_print ( int m, int n, double[] a, string title )
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    R8MAT_PRINT prints an R8MAT, with an optional title.
+        //
+        //  Discussion:
+        //
+        //    An R8MAT is a doubly dimensioned array of R8 values,  stored as a vector
+        //    in column-major order.
+        //
+        //    Entry A(I,J) is stored as A[I+J*M]
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    29 August 2003
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, int M, the number of rows in A.
+        //
+        //    Input, int N, the number of columns in A.
+        //
+        //    Input, double A[M*N], the M by N matrix.
+        //
+        //    Input, char *TITLE, a title to be printed.
+        //
+        {
+            r8mat_print_some ( m, n, a, 1, 1, m, n, title );
+        }
+
+
+        static void r8mat_print_some ( int m, int n, double[] a, int ilo, int jlo, int ihi,
+  int jhi, string title )
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    R8MAT_PRINT_SOME prints some of an R8MAT.
+        //
+        //  Discussion:
+        //
+        //    An R8MAT is a doubly dimensioned array of R8 values,  stored as a vector
+        //    in column-major order.
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    09 April 2004
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, int M, the number of rows of the matrix.
+        //    M must be positive.
+        //
+        //    Input, int N, the number of columns of the matrix.
+        //    N must be positive.
+        //
+        //    Input, double A[M*N], the matrix.
+        //
+        //    Input, int ILO, JLO, IHI, JHI, designate the first row and
+        //    column, and the last row and column to be printed.
+        //
+        //    Input, char *TITLE, a title for the matrix.
+        //
+        {
+            int INCX = 5;
+            
+            if ( 0 < TableReader.s_len_trim ( title ) )
+            {
+                Console.WriteLine();
+                Console.WriteLine(title);
+            }
+            //
+            //  Print the columns of the matrix, in strips of 5.
+            //
+            for (int j2lo = jlo; j2lo <= jhi; j2lo = j2lo + INCX )
+            {
+                int j2hi = j2lo + INCX - 1;
+                j2hi = Math.Min( j2hi, n );
+                j2hi = Math.Min( j2hi, jhi );
+
+                Console.WriteLine();
+                //
+                //  For each column J in the current range...
+                //
+                //  Write the header.
+                //
+                string cout = "  Col:    ";
+                for (int  j = j2lo; j <= j2hi; j++ )
+                {
+                    string t = j + "       ";
+                    cout += t.PadLeft(7);
+                }
+                Console.WriteLine(cout);
+                Console.WriteLine("  Row");
+                Console.WriteLine();
+                //
+                //  Determine the range of the rows in this strip.
+                //
+                int i2lo = Math.Max ( ilo, 1 );
+                int i2hi = Math.Min ( ihi, m );
+
+                cout = "";
+                for (int  i = i2lo; i <= i2hi; i++ )
+                {
+                    //
+                    //  Print out (up to) 5 entries in row I, that lie in the current strip.
+                    //
+                    string t = i + "  ";
+                    cout += t.PadLeft(5);
+                    for (int  j = j2lo; j <= j2hi; j++ )
+                    {
+                        t = a[i - 1 + (j - 1) * m] + "  ";
+                        cout += t.PadLeft(12);
+                    }
+                    Console.WriteLine(cout);
+                }
+            }
+
+            Console.WriteLine();
+        }
+
+        static void r8vec_print ( int n, double[] a, string title )
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    R8VEC_PRINT prints an R8VEC.
+            //
+            //  Discussion:
+            //
+            //    An R8VEC is a vector of R8's.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    16 August 2004
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Parameters:
+            //
+            //    Input, int N, the number of components of the vector.
+            //
+            //    Input, double A[N], the vector to be printed.
+            //
+            //    Input, string TITLE, a title.
+            //
+        {
+            Console.WriteLine();
+            Console.WriteLine(title);
+            Console.WriteLine();
+            for (int i = 0; i < n; i++ )
+            {
+                string cout = "  ";
+                
+                string t = i.ToString();
+                t = t.PadLeft(8);
+
+                string t2 = a[i].ToString();
+                t2 = t2.PadLeft(14);
+                
+                cout += t + ": " + t2;  
+                
+                Console.WriteLine(cout);;
+            }
+        }
+
     }
 }

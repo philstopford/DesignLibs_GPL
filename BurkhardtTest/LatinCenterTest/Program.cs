6beb1f17d6a1e46dac70bcc5e4e466132262ab12
philstopford <phil.stopford@gmail.com>
2021-05-24 17:04:59 -0500

    Initial commit for Latin Center

---
+++ BurkhardtTest/LatinCenterTest/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Burkardt.Latin.Center;
+using entropyRNG;
+
+namespace Burkardt.LatinCenterTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int seed = RNG.nextint(0, Int32.MaxValue);
+            int seed_save = seed;
+            Console.WriteLine();
+            Console.WriteLine("LATIN_CENTER_TEST:");
+            Console.WriteLine("  Test the LATIN_CENTER library.");
+
+            test01 ( seed );
+
+            Console.WriteLine();
+            Console.WriteLine("LATIN_CENTER_TEST:");
+            Console.WriteLine("  Repeat TEST01, but with different seed from first run.");
+
+            seed = RNG.nextint(0, Int32.MaxValue);
+            
+            test01 ( seed );
+
+            Console.WriteLine();
+            Console.WriteLine("LATIN_CENTER_TEST:");
+            Console.WriteLine("  Repeat TEST01 with same seed as first run.");
+
+            seed = seed_save;
+            test01 ( seed );
+
+            Console.WriteLine();
+            Console.WriteLine("LATIN_CENTER_TEST:");
+            Console.WriteLine("  Normal end of execution.");
+            Console.WriteLine();
+            
+        }
+        
+        
+        static void test01 ( int seed )
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    TEST01 tests LATIN_CENTER.
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    01 March 2007
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        {
+            int DIM_NUM = 2;
+            int POINT_NUM = 10;
+
+            Console.WriteLine();
+            Console.WriteLine("TEST01");
+            Console.WriteLine("  LATIN_CENTER chooses a Latin cell arrangement,");
+            Console.WriteLine("  and returns the centers of those cells.");
+            Console.WriteLine();
+            Console.WriteLine("  Spatial dimension = " + DIM_NUM);
+            Console.WriteLine("  Number of points =  " + POINT_NUM);
+            Console.WriteLine("  Initial seed for UNIFORM = " + seed);
+
+            double[] x = LatinCenter.latin_center( DIM_NUM, POINT_NUM, seed );
+
+            Console.WriteLine();
+            Console.WriteLine("  The Latin centers:");
+            Console.WriteLine();
+
+            int k = 0;
+            for (int j = 0; j < POINT_NUM; j++ )
+            {
+                int kk = k;
+                string cout = "";
+                for (int i = 0; i < DIM_NUM; i++ )
+                {
+                    cout += x[kk].ToString("0.########").PadLeft(10) + "  ";
+                    kk = kk + POINT_NUM;
+                }
+                Console.WriteLine(cout);
+                k = k + 1;
+            }
+        }
+    }
+}
---
+++ Common/Burkardt/Latin.Center.cs
@@ -0,0 +1,132 @@
+﻿using entropyRNG;
+
+namespace Burkardt.Latin.Center
+{
+    public static class LatinCenter
+    {
+        static int[] perm_uniform ( int n, int base_, int seed )
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    PERM_UNIFORM selects a random permutation of N objects.
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license. 
+        //
+        //  Modified:
+        //
+        //    31 October 2008
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Reference:
+        //
+        //    Albert Nijenhuis, Herbert Wilf,
+        //    Combinatorial Algorithms,
+        //    Academic Press, 1978, second edition,
+        //    ISBN 0-12-519260-6.
+        //
+        //  Parameters:
+        //
+        //    Input, int N, the number of objects to be permuted.
+        //
+        //    Input, int BASE, is 0 for a 0-based permutation and 1 for 
+        //    a 1-based permutation.
+        //
+        //    Input/output, int *SEED, a seed for the random number generator.
+        //
+        //    Output, int PERM_UNIFORM[N], a permutation of (BASE, BASE+1, ..., BASE+N-1).
+        //
+        {
+            int i;
+            int j;
+            int k;
+            int[] p;
+
+            p = new int[n];
+ 
+            for ( i = 0; i < n; i++ )
+            {
+                p[i] = i + base_;
+            }
+
+            for ( i = 0; i < n; i++ )
+            {
+                j = RNG.nextint( i, n - 1, seed );
+                k    = p[i];
+                p[i] = p[j];
+                p[j] = k;
+            }
+ 
+            return p;
+        }
+
+        
+        public static double[] latin_center ( int dim_num, int point_num, int seed )
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    LATIN_CENTER returns center points in a Latin square.
+        //
+        //  Discussion:
+        //
+        //    In each spatial dimension, there will be exactly one
+        //    point with the coordinate value 
+        //
+        //      ( 1, 3, 5, ..., 2*point_num-1 ) / ( 2 * point_num )
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license. 
+        //
+        //  Modified:
+        //
+        //    12 March 2003
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, int DIM_NUM, the spatial dimension.
+        //
+        //    Input, int POINT_NUM, the number of points.
+        //
+        //    Input/output, int *SEED, a seed for UNIFORM.
+        //
+        //    Output, double LATIN_CENTER[DIM_NUM*POINT_NUM], the points.
+        //
+        {
+            int base_ = 0;
+            double[] x = new double[dim_num * point_num];
+            //
+            //  For spatial dimension I, 
+            //    pick a random permutation of 1 to POINT_NUM,
+            //    force the corresponding I-th components of X to lie in the
+            //    interval ( PERM[J]-1, PERM[J] ) / POINT_NUM.
+            //
+            int k = 0;
+            for (int i = 0; i < dim_num; i++ )
+            {
+                int[] perm = perm_uniform ( point_num, base_, seed );
+
+                for (int j = 0; j < point_num; j++ )
+                {
+                    double r = 0.5;
+                    x[k] = ( ( ( double ) perm[j] ) + r ) / ( ( double ) point_num );
+                    k = k + 1;
+                }
+            }
+
+            return x;
+        }
+
+        
+    }
+}

1bcb57b3788f68e6e9bd18ddce8fb003ca9913c4
philstopford <phil.stopford@gmail.com>
2021-05-24 16:32:24 -0500

    More features for Table

--- Common/Burkardt/Table/TableMisc_i4.cs
+++ Common/Burkardt/Table/TableMisc_i4.cs
@@ -231,6 +231,162 @@ namespace Burkardt.Table
 
     public static partial class TableMisc
     {
+        public static void i4mat_transpose_print ( int m, int n, int[] a, string title )
+
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    I4MAT_TRANSPOSE_PRINT prints an I4MAT, transposed.
+        //
+        //  Discussion:
+        //
+        //    An I4MAT is an MxN array of I4's, stored by (I,J) -> [I+J*M].
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    31 January 2005
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, int M, the number of rows in A.
+        //
+        //    Input, int N, the number of columns in A.
+        //
+        //    Input, int A[M*N], the M by N matrix.
+        //
+        //    Input, string TITLE, a title.
+        //
+        {
+            i4mat_transpose_print_some ( m, n, a, 1, 1, m, n, title );
+        }
+        //****************************************************************************80
+
+        static void i4mat_transpose_print_some ( int m, int n, int[] a, int ilo, int jlo,
+        int ihi, int jhi, string title )
+
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    I4MAT_TRANSPOSE_PRINT_SOME prints some of an I4MAT, transposed.
+        //
+        //  Discussion:
+        //
+        //    An I4MAT is an MxN array of I4's, stored by (I,J) -> [I+J*M].
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    15 October 2014
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, int M, the number of rows of the matrix.
+        //    M must be positive.
+        //
+        //    Input, int N, the number of columns of the matrix.
+        //    N must be positive.
+        //
+        //    Input, int A[M*N], the matrix.
+        //
+        //    Input, int ILO, JLO, IHI, JHI, designate the first row and
+        //    column, and the last row and column to be printed.
+        //
+        //    Input, string TITLE, a title.
+        //
+        {
+            int INCX = 10;
+
+            Console.WriteLine();
+            Console.WriteLine(title);
+
+            if ( m <= 0 || n <= 0 )
+            {
+                Console.WriteLine();
+                Console.WriteLine("  (None)");
+                return;
+            }
+            //
+            //  Print the columns of the matrix, in strips of INCX.
+            //
+            for (int i2lo = ilo; i2lo <= ihi; i2lo = i2lo + INCX )
+            {
+                int i2hi = i2lo + INCX - 1;
+                if ( m < i2hi )
+                {
+                    i2hi = m;
+                }
+                if ( ihi < i2hi )
+                {
+                    i2hi = ihi;
+                }
+                Console.WriteLine();
+                //
+                //  For each row I in the current range...
+                //
+                //  Write the header.
+                //
+                string line = "  Row: ";
+                for (int  i = i2lo; i <= i2hi; i++ )
+                {
+                    string t = (i - 1) + "  ";
+                    line += t.PadLeft(6);
+                }
+                Console.WriteLine(line);
+                Console.WriteLine();
+                Console.WriteLine("  Col");
+                Console.WriteLine();
+                //
+                //  Determine the range of the rows in this strip.
+                //
+                int j2lo = jlo;
+                if ( j2lo < 1 )
+                {
+                    j2lo = 1;
+                }
+                int j2hi = jhi;
+                if ( n < j2hi )
+                {
+                    j2hi = n;
+                }
+                for (int  j = j2lo; j <= j2hi; j++ )
+                {
+                    //
+                    //  Print out (up to INCX) entries in column J, that lie in the current strip.
+                    //
+                    string t = (j - 1) + ":";
+                    line = "";
+                    line += t.PadLeft(5);
+
+                    for (int  i = i2lo; i <= i2hi; i++ )
+                    {
+                        t = a[i-1+(j-1)*m] + "  ";
+                        line += t.PadLeft(6);
+                    }
+                    Console.WriteLine(line);
+
+                    Console.WriteLine();
+                }
+            }
+        }        
+        
+        
         public static void i4mat_print ( int m, int n, int[] a, string title )
         //****************************************************************************80
         //

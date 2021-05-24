9868e783f6df19da2f26df8c89fdedc067991667
philstopford <phil.stopford@gmail.com>
2021-05-24 16:38:19 -0500

    Refactor

--- Common/Burkardt/Latinize.cs
+++ Common/Burkardt/Latinize.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using Burkardt.Table;
 
 namespace Burkardt.Latinizer
 {
@@ -53,7 +54,7 @@ namespace Burkardt.Latinizer
 				}
 				double v_min = v.Min();
 				double v_max = v.Max();
-				int[] indx = r8vec_sort_heap_index_a_new ( n, v );
+				int[] indx = TableMisc.r8vec_sort_heap_index_a_new ( n, v );
 
 				for (int j = 0; j < n; j++ )
 				{
@@ -67,115 +68,5 @@ namespace Burkardt.Latinizer
 			return table;
 		}        
 		
-		static int[] r8vec_sort_heap_index_a_new ( int n, double[] a )
-
-		//****************************************************************************80
-		//
-		//  Purpose:
-		//
-		//    R8VEC_SORT_HEAP_INDEX_A_NEW does an indexed heap ascending sort of an R8VEC.
-		//
-		//  Discussion:
-		//
-		//    The sorting is not actually carried out.  Rather an index array is
-		//    created which defines the sorting.  This array may be used to sort
-		//    or index the array, or to sort or index related arrays keyed on the
-		//    original array.
-		//
-		//    Once the index array is computed, the sorting can be carried out
-		//    "implicitly:
-		//
-		//      A(INDX(I)), I = 1 to N is sorted,
-		//
-		//    after which A(I), I = 1 to N is sorted.
-		//
-		//  Licensing:
-		//
-		//    This code is distributed under the GNU LGPL license. 
-		//
-		//  Modified:
-		//
-		//    30 March 2004
-		//
-		//  Author:
-		//
-		//    John Burkardt
-		//
-		//  Parameters:
-		//
-		//    Input, int N, the number of entries in the array.
-		//
-		//    Input, double A[N], an array to be index-sorted.
-		//
-		//    Output, int R8VEC_SORT_HEAP_INDEX_A_NEW[N], contains the sort index.  The
-		//    I-th element of the sorted array is A(INDX(I)).
-		//
-		{
-			int[] indx = new int[n];
-
-			for (int i = 1; i <= n; i++ )
-			{
-				indx[i-1] = i;
-			}
-
-			int l = n / 2 + 1;
-			int ir = n;
-
-			for ( ; ; )
-			{
-				double aval;
-				int indxt;
-				if ( 1 < l )
-				{
-					l = l - 1;
-					indxt = indx[l-1];
-					aval = a[indxt-1];
-				}
-				else
-				{
-					indxt = indx[ir-1];
-					aval = a[indxt-1];
-					indx[ir-1] = indx[0];
-					ir = ir - 1;
-
-					if ( ir == 1 )
-					{
-						indx[0] = indxt;
-						for (int  i = 0; i < n; i++ )
-						{
-							indx[i] = indx[i] - 1;
-						}
-						break;
-					}
-				}
-
-				int i2 = l;
-				int j = l + l;
-
-				while ( j <= ir )
-				{
-					if ( j < ir )
-					{
-						if ( a[indx[j-1]-1] < a[indx[j]-1] )
-						{
-							j = j + 1;
-						}
-					}
-
-					if ( aval < a[indx[j-1]-1] )
-					{
-						indx[i2-1] = indx[j-1];
-						i2 = j;
-						j = j + j;
-					}
-					else
-					{
-						j = ir + 1;
-					}
-				}
-				indx[i2-1] = indxt;
-			}
-			return indx;
-		}
 	}
 }

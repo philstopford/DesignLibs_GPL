14d8992bc3c1f90d90da98115fbd13f1374b80a4
philstopford <phil.stopford@gmail.com>
2021-05-28 14:07:35 -0500

    WIP

---
+++ Common/Burkardt/Probability/Gompertz.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using Burkardt.Uniform;
+
+namespace Burkardt.Probability
+{
+    public static class Gompertz
+    {
+        public static double gompertz_cdf(double x, double a, double b)
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    GOMPERTZ_CDF evaluates the Gompertz CDF.
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    17 October 2004
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Reference:
+        //
+        //    Johnson, Kotz, and Balakrishnan,
+        //    Continuous Univariate Distributions, Volume 2, second edition,
+        //    Wiley, 1994, pages 25-26.
+        //
+        //  Parameters:
+        //
+        //    Input, double X, the argument of the CDF.
+        //
+        //    Input, double A, B, the parameters of the PDF.
+        //    1 < A, 0 < B.
+        //
+        //    Output, double GOMPERTZ_CDF, the value of the CDF.
+        //
+        {
+            double cdf;
+
+            if (x <= 0.0)
+            {
+                cdf = 0.0;
+            }
+            else
+            {
+                cdf = 1.0 - Math.Exp(-b * (Math.Pow(a, x) - 1.0) / Math.Log(a));
+            }
+
+            return cdf;
+        }
+
+        public static double gompertz_cdf_inv(double cdf, double a, double b)
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    GOMPERTZ_CDF_INV inverts the Gompertz CDF.
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    17 October 2004
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Reference:
+        //
+        //    Johnson, Kotz, and Balakrishnan,
+        //    Continuous Univariate Distributions, Volume 2, second edition,
+        //    Wiley, 1994, pages 25-26.
+        //
+        //  Parameters:
+        //
+        //    Input, double CDF, the value of the CDF.
+        //
+        //    Input, double A, B, the parameters of the PDF.
+        //    1 < A, 0 < B.
+        //
+        //    Output, double GOMPERTZ_CDF_INV, the corresponding argument.
+        //
+        {
+            const double r8_huge = 1.0E+30;
+            double x;
+
+            if (cdf < 0.0 || 1.0 < cdf)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("GOMPERTZ_CDF_INV - Fatal error!");
+                Console.WriteLine("  CDF < 0 or 1 < CDF.");
+                return (1);
+            }
+
+            if (cdf < 1.0)
+            {
+                x = Math.Log(1.0 - Math.Log(1.0 - cdf) * Math.Log(a) / b) / Math.Log(a);
+            }
+            else
+            {
+                x = r8_huge;
+            }
+
+            return x;
+        }
+
+        public static bool gompertz_check(double a, double b)
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    GOMPERTZ_CHECK checks the parameters of the Gompertz PDF.
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    16 October 2004
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Reference:
+        //
+        //    Johnson, Kotz, and Balakrishnan,
+        //    Continuous Univariate Distributions, Volume 2, second edition,
+        //    Wiley, 1994, pages 25-26.
+        //
+        //  Parameters:
+        //
+        //    Input, double A, B, the parameters of the PDF.
+        //    1 < A, 0 < B.
+        //
+        //    Output, bool GOMPERTZ_CHECK, is true if the parameters are legal.
+        //
+        {
+            if (a <= 1.0)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("GOMPERTZ_CHECK - Warning!");
+                Console.WriteLine("  A <= 1.0!");
+                return false;
+            }
+
+            if (b <= 0.0)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("GOMPERTZ_CHECK - Warning!");
+                Console.WriteLine("  B <= 0.0!");
+                return false;
+            }
+
+            return true;
+        }
+
+        public static double gompertz_pdf(double x, double a, double b)
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    GOMPERTZ_PDF evaluates the Gompertz PDF.
+        //
+        //  Discussion:
+        //
+        //    PDF(A,B;X) = B * A**X / exp ( B * ( A**X - 1 ) / log ( A ) )
+        //
+        //    for
+        //
+        //      0.0 <= X
+        //      1.0 <  A
+        //      0.0 <  B
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    17 October 2004
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Reference:
+        //
+        //    Johnson, Kotz, and Balakrishnan,
+        //    Continuous Univariate Distributions, Volume 2, second edition,
+        //    Wiley, 1994, pages 25-26.
+        //
+        //  Parameters:
+        //
+        //    Input, double X, the argument of the PDF.
+        //
+        //    Input, double A, B, the parameters of the PDF.
+        //    1 < A, 0 < B.
+        //
+        //    Output, double GOMPERTZ_PDF, the value of the PDF.
+        //
+        {
+            double pdf = 0;
+
+            if (x < 0.0)
+            {
+                pdf = 0.0;
+            }
+            else if (1.0 < a)
+            {
+                pdf = Math.Exp(Math.Log(b) + x * Math.Log(a)
+                               - (b / Math.Log(a)) * (Math.Pow(a, x) - 1.0));
+            }
+
+            return pdf;
+        }
+
+        public static double gompertz_sample(double a, double b, ref int seed)
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    GOMPERTZ_SAMPLE samples the Gompertz PDF.
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    17 October 2004
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, double A, B, the parameters of the PDF.
+        //    1 < A, 0 < B.
+        //
+        //    Input/output, int &SEED, a seed for the random number generator.
+        //
+        //    Output, double GOMPERTZ_SAMPLE, a sample of the PDF.
+        //
+        {
+            double cdf;
+            double x;
+
+            cdf = UniformRNG.r8_uniform_01(ref seed);
+
+            x = gompertz_cdf_inv(cdf, a, b);
+
+            return x;
+        }
+    }
+}
---
+++ Common/Burkardt/Probability/Half.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using Burkardt.Uniform;
+
+namespace Burkardt.Probability
+{
+    public static class Half
+    {
+        public static double half_normal_cdf(double x, double a, double b)
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    HALF_NORMAL_CDF evaluates the Half Normal CDF.
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    17 October 2004
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, double X, the argument of the CDF.
+        //
+        //    Input, double A, B, the parameters of the PDF.
+        //    0.0 < B.
+        //
+        //    Output, double HALF_NORMAL_CDF, the value of the CDF.
+        //
+        {
+            double cdf;
+
+            if (x <= a)
+            {
+                cdf = 0.0;
+            }
+            else
+            {
+                double cdf2 = Normal.normal_cdf(x, a, b);
+                cdf = 2.0 * cdf2 - 1.0;
+            }
+
+            return cdf;
+        }
+
+        public static double half_normal_cdf_inv(double cdf, double a, double b)
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    HALF_NORMAL_CDF_INV inverts the Half Normal CDF.
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    17 October 2004
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, double CDF, the value of the CDF.
+        //    0.0 <= CDF <= 1.0.
+        //
+        //    Input, double A, B, the parameters of the PDF.
+        //    0.0 < B.
+        //
+        //    Output, double HALF_NORMAL_CDF_INV, the corresponding argument.
+        //
+        {
+            if (cdf < 0.0 || 1.0 < cdf)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("HALF_NORMAL_CDF_INV - Fatal error!");
+                Console.WriteLine("  CDF < 0 or 1 < CDF.");
+                return (1);
+            }
+
+            double cdf2 = 0.5 * (cdf + 1.0);
+
+            double x = Normal.normal_cdf_inv(cdf2, a, b);
+
+            return x;
+        }
+
+        public static bool half_normal_check(double a, double b)
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    HALF_NORMAL_CHECK checks the parameters of the Half Normal PDF.
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    16 October 2004
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, double A, B, the parameters of the PDF.
+        //    0.0 < B.
+        //
+        //    Output, bool HALF_NORMAL_CHECK, is true if the parameters are legal.
+        //
+        {
+            if (b <= 0.0)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("HALF_NORMAL_CHECK - Warning!");
+                Console.WriteLine("  B <= 0.");
+                return false;
+            }
+
+            return true;
+        }
+        
+        public static double half_normal_mean(double a, double b)
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    HALF_NORMAL_MEAN returns the mean of the Half Normal PDF.
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    17 October 2004
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, double A, B, the parameters of the PDF.
+        //    0.0 < B.
+        //
+        //    Output, double HALF_NORMAL_MEAN, the mean of the PDF.
+        //
+        {
+            const double r8_pi = 3.14159265358979323;
+
+            double mean = a + b * Math.Sqrt(2.0 / r8_pi);
+
+            return mean;
+        }
+
+        public static double half_normal_pdf(double x, double a, double b)
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    HALF_NORMAL_PDF evaluates the Half Normal PDF.
+        //
+        //  Discussion:
+        //
+        //    PDF(A,B;X) =
+        //      sqrt ( 2 / PI ) * ( 1 / B ) * exp ( - 0.5 * ( ( X - A ) / B )^2 )
+        //
+        //    for A <= X
+        //
+        //    The Half Normal PDF is a special case of both the Chi PDF and the
+        //    Folded Normal PDF.
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    17 October 2004
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, double X, the argument of the PDF.
+        //    A <= X
+        //
+        //    Input, double A, B, the parameters of the PDF.
+        //    0.0 < B.
+        //
+        //    Output, double HALF_NORMAL_PDF, the value of the PDF.
+        //
+        {
+            double pdf;
+            const double r8_pi = 3.14159265358979323;
+
+            if (x <= a)
+            {
+                pdf = 0.0;
+            }
+            else
+            {
+                double y = (x - a) / b;
+
+                pdf = Math.Sqrt(2.0 / r8_pi) * (1.0 / b) * Math.Exp(-0.5 * y * y);
+            }
+
+            return pdf;
+        }
+
+        public static double half_normal_sample(double a, double b, ref int seed)
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    HALF_NORMAL_SAMPLE samples the Half Normal PDF.
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    17 October 2004
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, double A, B, the parameters of the PDF.
+        //    0.0 < B.
+        //
+        //    Input/output, int &SEED, a seed for the random number generator.
+        //
+        //    Output, double HALF_NORMAL_SAMPLE, a sample of the PDF.
+        //
+        {
+            double cdf = UniformRNG.r8_uniform_01(ref seed);
+
+            double x = half_normal_cdf_inv(cdf, a, b);
+
+            return x;
+        }
+
+        public static double half_normal_variance(double a, double b)
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    HALF_NORMAL_VARIANCE returns the variance of the Half Normal PDF.
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    17 October 2004
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, double A, B, the parameters of the PDF.
+        //    0.0 < B.
+        //
+        //    Output, double HALF_NORMAL_VARIANCE, the variance of the PDF.
+        //
+        {
+            const double r8_pi = 3.14159265358979323;
+
+            double variance = b * b * (1.0 - 2.0 / r8_pi);
+
+            return variance;
+        }
+    }
+}

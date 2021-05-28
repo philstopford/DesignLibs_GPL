3601180ae87bdca645dc24e904448704836fd5b0
philstopford <phil.stopford@gmail.com>
2021-05-28 17:30:16 -0500

    Create Reciprocal.cs

---
+++ Common/Burkardt/Probability/Reciprocal.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using Burkardt.Uniform;
+
+namespace Burkardt.Probability
+{
+    public static class Reciprocal
+    {
+        public static double reciprocal_cdf(double x, double a, double b)
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    RECIPROCAL_CDF evaluates the Reciprocal CDF.
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    18 October 2004
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, double X, the argument of the PDF.
+        //
+        //    Input, double A, B, the parameters of the PDF.
+        //    0.0 < A <= B.
+        //
+        //    Output, double RECIPROCAL_CDF, the value of the CDF.
+        //
+        {
+            double cdf = 0;
+
+            if (x <= 0.0)
+            {
+                cdf = 0.0;
+            }
+            else if (0.0 < x)
+            {
+                cdf = Math.Log(a / x) / Math.Log(a / b);
+            }
+
+            return cdf;
+        }
+
+        public static double reciprocal_cdf_inv(double cdf, double a, double b)
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    RECIPROCAL_CDF_INV inverts the Reciprocal CDF.
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    18 October 2004
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, double CDF, the value of the CDF.
+        //
+        //    Input, double A, B, the parameters of the PDF.
+        //    0.0 < A <= B.
+        //
+        //    Output, double RECIPROCAL_CDF_INV, the corresponding argument of the CDF.
+        //
+        {
+            double x = 0;
+
+            if (cdf < 0.0 || 1.0 < cdf)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("RECIPROCAL_CDF_INV - Fatal error!");
+                Console.WriteLine("  CDF < 0 or 1 < CDF.");
+                return (1);
+            }
+
+            if (cdf == 0.0)
+            {
+                x = 0.0;
+            }
+            else if (0.0 < cdf)
+            {
+                x = Math.Pow(b, cdf) / Math.Pow(a, (cdf - 1.0));
+            }
+
+            return x;
+        }
+
+        public static bool reciprocal_check(double a, double b)
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    RECIPROCAL_CHECK checks the parameters of the Reciprocal CDF.
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    18 October 2004
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, double A, B, the parameters of the PDF.
+        //    0.0 < A <= B.
+        //
+        //    Output, bool RECIPROCAL_CHECK, is true if the parameters are legal.
+        //
+        {
+            if (a <= 0.0)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("RECIPROCAL_CHECK - Warning!");
+                Console.WriteLine("  A <= 0.0");
+                return false;
+            }
+
+            if (b < a)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("RECIPROCAL_CHECK - Warning!");
+                Console.WriteLine("  B < A");
+                return false;
+            }
+
+            return true;
+        }
+
+        public static double reciprocal_mean(double a, double b)
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    RECIPROCAL_MEAN returns the mean of the Reciprocal PDF.
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    18 October 2004
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, double A, B, the parameters of the PDF.
+        //    0.0 < A <= B.
+        //
+        //    Output, double RECIPROCAL_MEAN, the mean of the PDF.
+        //
+        {
+            double mean = (a - b) / Math.Log(a / b);
+
+            return mean;
+        }
+
+        public static double reciprocal_pdf(double x, double a, double b)
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    RECIPROCAL_PDF evaluates the Reciprocal PDF.
+        //
+        //  Discussion:
+        //
+        //    PDF(A,B;X) = 1.0 / ( X * LOG ( B / A ) )
+        //    for 0.0 <= X
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    18 October 2004
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, double X, the argument of the PDF.
+        //
+        //    Input, double A, B, the parameters of the PDF.
+        //    0.0 < A <= B.
+        //
+        //    Output, double RECIPROCAL_PDF, the value of the PDF.
+        //
+        {
+            double pdf = 0;
+
+            if (x <= 0.0)
+            {
+                pdf = 0.0;
+            }
+            else if (0.0 < x)
+            {
+                pdf = 1.0 / (x * Math.Log(b / a));
+            }
+
+            return pdf;
+        }
+
+        public static double reciprocal_sample(double a, double b, ref int seed)
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    RECIPROCAL_SAMPLE samples the Reciprocal PDF.
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    18 October 2004
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, double A, B, the parameters of the PDF.
+        //    0.0 < A <= B.
+        //
+        //    Input/output, int &SEED, a seed for the random number generator.
+        //
+        //    Output, double RECIPROCAL_SAMPLE, a sample of the PDF.
+        //
+        {
+            double cdf = UniformRNG.r8_uniform_01(ref seed);
+
+            double x = Math.Pow(b, cdf) / Math.Pow(a, (cdf - 1.0));
+
+            return x;
+        }
+
+        public static double reciprocal_variance(double a, double b)
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    RECIPROCAL_VARIANCE returns the variance of the Reciprocal PDF.
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    18 October 2004
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, double A, B, the parameters of the PDF.
+        //    0.0 < A <= B.
+        //
+        //    Output, double RECIPROCAL_VARIANCE, the variance of the PDF.
+        //
+        {
+            double d = Math.Log(a / b);
+
+            double variance = (a - b) * (a * (d - 2.0) + b * (d + 2.0))
+                              / (2.0 * d * d);
+
+            return variance;
+        }
+    }
+}

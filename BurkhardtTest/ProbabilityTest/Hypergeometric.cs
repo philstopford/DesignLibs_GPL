40244ce7fa3afda5ea9a150342eece4859dccdb2
philstopford <phil.stopford@gmail.com>
2021-05-29 11:11:56 -0500

    WIP

---
+++ BurkhardtTest/ProbabilityTest/Hypergeometric.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using Burkardt.Probability;
+using Burkardt.Types;
+
+namespace Burkardt.ProbabilityTest
+{
+    partial class Program
+    {
+        static void hypergeometric_cdf_test()
+
+//****************************************************************************80
+//
+//  Purpose:
+//
+//    HYPERGEOMETRIC_CDF_TEST tests HYPERGEOMETRIC_CDF.
+//
+//  Licensing:
+//
+//    This code is distributed under the GNU LGPL license.
+//
+//  Modified:
+//
+//    08 April 2016
+//
+//  Author:
+//
+//    John Burkardt
+//
+        {
+            double cdf;
+            int l;
+            int m;
+            int n;
+            double pdf;
+            int x;
+
+            Console.WriteLine("");
+            Console.WriteLine("HYPERGEOMETRIC_CDF_TEST");
+            Console.WriteLine("  HYPERGEOMETRIC_CDF evaluates the Hypergeometric CDF.");
+            Console.WriteLine("  HYPERGEOMETRIC_PDF evaluates the Hypergeometric PDF.");
+
+            n = 10;
+            m = 7;
+            l = 100;
+
+            Console.WriteLine("");
+            Console.WriteLine("  Total number of balls L =         " + l + "");
+            Console.WriteLine("  Number of white balls M =         " + m + "");
+            Console.WriteLine("  Number of balls taken N =         " + n + "");
+
+            if (!Hypergeometric.hypergeometric_check(n, m, l))
+            {
+                Console.WriteLine("");
+                Console.WriteLine("HYPERGEOMETRIC_CDF_TEST - Fatal error!");
+                Console.WriteLine("  The parameters are not legal.");
+                return;
+            }
+
+            x = 7;
+
+            pdf = Hypergeometric.hypergeometric_pdf(x, n, m, l);
+
+            cdf = Hypergeometric.hypergeometric_cdf(x, n, m, l);
+
+            Console.WriteLine("  PDF argument X =                " + x + "");
+            Console.WriteLine("  PDF value =                   = " + pdf + "");
+            Console.WriteLine("  CDF value =                   = " + cdf + "");
+
+            return;
+        }
+
+        static void hypergeometric_sample_test()
+
+//****************************************************************************80
+//
+//  Purpose:
+//
+//    HYPERGEOMETRIC_SAMPLE_TEST tests HYPERGEOMETRIC_SAMPLE.
+//
+//  Licensing:
+//
+//    This code is distributed under the GNU LGPL license.
+//
+//  Modified:
+//
+//    08 April 2016
+//
+//  Author:
+//
+//    John Burkardt
+//
+        {
+            int SAMPLE_NUM = 1000;
+
+            int j;
+            int l;
+            int m;
+            double mean;
+            int n;
+            int seed = 123456789;
+            double variance;
+            int[] x = new int[SAMPLE_NUM];
+            int xmax;
+            int xmin;
+
+            Console.WriteLine("");
+            Console.WriteLine("HYPERGEOMETRIC_SAMPLE_TEST");
+            Console.WriteLine("  HYPERGEOMETRIC_MEAN computes the Hypergeometric mean;");
+            Console.WriteLine("  HYPERGEOMETRIC_SAMPLE samples the Hypergeometric distribution;");
+            Console.WriteLine("  HYPERGEOMETRIC_VARIANCE computes the Hypergeometric variance.");
+
+            n = 10;
+            m = 7;
+            l = 100;
+
+            Console.WriteLine("");
+            Console.WriteLine("  Total number of balls L =         " + l + "");
+            Console.WriteLine("  Number of white balls M =         " + m + "");
+            Console.WriteLine("  Number of balls taken N =         " + n + "");
+
+            if (!Hypergeometric.hypergeometric_check(n, m, l))
+            {
+                Console.WriteLine("");
+                Console.WriteLine("HYPERGEOMETRIC_SAMPLE_TEST - Fatal error!");
+                Console.WriteLine("  The parameters are not legal.");
+                return;
+            }
+
+            mean = Hypergeometric.hypergeometric_mean(n, m, l);
+            variance = Hypergeometric.hypergeometric_variance(n, m, l);
+
+            Console.WriteLine("  PDF mean =                    " + mean + "");
+            Console.WriteLine("  PDF variance =                " + variance + "");
+
+            for (j = 0; j < SAMPLE_NUM; j++)
+            {
+                x[j] = Hypergeometric.hypergeometric_sample(n, m, l, ref seed );
+            }
+
+            mean = typeMethods.i4vec_mean(SAMPLE_NUM, x);
+            variance = typeMethods.i4vec_variance(SAMPLE_NUM, x);
+            xmax = typeMethods.i4vec_max(SAMPLE_NUM, x);
+            xmin = typeMethods.i4vec_min(SAMPLE_NUM, x);
+
+            Console.WriteLine("");
+            Console.WriteLine("  Sample size =     " + SAMPLE_NUM + "");
+            Console.WriteLine("  Sample mean =     " + mean + "");
+            Console.WriteLine("  Sample variance = " + variance + "");
+            Console.WriteLine("  Sample maximum =  " + xmax + "");
+            Console.WriteLine("  Sample minimum =  " + xmin + "");
+
+        }
+
+    }
+}
---
+++ BurkhardtTest/ProbabilityTest/i4.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using Burkardt.Types;
+using Burkardt.Uniform;
+
+namespace Burkardt.ProbabilityTest
+{
+    partial class Program
+    {
+        static void i4_choose_test()
+
+//****************************************************************************80
+//
+//  Purpose:
+//
+//    I4_CHOOSE_TEST tests I4_CHOOSE.
+//
+//  Licensing:
+//
+//    This code is distributed under the GNU LGPL license. 
+//
+//  Modified:
+//
+//    27 October 2014
+//
+//  Author:
+//
+//    John Burkardt
+//
+        {
+            int cnk;
+            int k;
+            int n;
+
+            Console.WriteLine("");
+            Console.WriteLine("I4_CHOOSE_TEST");
+            Console.WriteLine("  I4_CHOOSE evaluates C(N,K).");
+            Console.WriteLine("");
+            Console.WriteLine("       N       K     CNK");
+
+            for (n = 0; n <= 4; n++)
+            {
+                Console.WriteLine("");
+                for (k = 0; k <= n; k++)
+                {
+                    cnk = typeMethods.i4_choose(n, k);
+
+                    Console.WriteLine( "  "
+                         + n.ToString().PadLeft(6) + "  "
+                         + k.ToString().PadLeft(6) + "  "
+                         + cnk.ToString().PadLeft(6) + "");
+                }
+            }
+
+            return;
+        }
+
+        static void i4_choose_log_test()
+
+//****************************************************************************80
+//
+//  Purpose:
+//
+//    I4_CHOOSE_LOG_TEST tests I4_CHOOSE_LOG.
+//
+//  Licensing:
+//
+//    This code is distributed under the GNU LGPL license. 
+//
+//  Modified:
+//
+//    05 March 2016
+//
+//  Author:
+//
+//    John Burkardt
+//
+        {
+            int cnk;
+            double elcnk;
+            int k;
+            double lcnk;
+            int n;
+
+            Console.WriteLine("");
+            Console.WriteLine("I4_CHOOSE_LOG_TEST");
+            Console.WriteLine("  I4_CHOOSE_LOG evaluates log(C(N,K)).");
+            Console.WriteLine("");
+            Console.WriteLine("       N       K            lCNK           elCNK     CNK");
+
+            for (n = 0; n <= 4; n++)
+            {
+                Console.WriteLine("");
+                for (k = 0; k <= n; k++)
+                {
+                    lcnk = typeMethods.i4_choose_log(n, k);
+                    elcnk = Math.Exp(lcnk);
+                    cnk = typeMethods.i4_choose(n, k);
+
+                    Console.WriteLine("  " + n.ToString().PadLeft(6)
+                                      + "  " + k.ToString().PadLeft(6)
+                                      + "  " + lcnk.ToString().PadLeft(14)
+                                      + "  " + elcnk.ToString().PadLeft(14)
+                                      + "  " + cnk.ToString().PadLeft(6) + "");
+                }
+            }
+
+            return;
+        }
+
+        static void i4_is_power_of_10_test()
+
+//****************************************************************************80
+//
+//  Purpose:
+//
+//    I4_IS_POWER_OF_10_TEST tests I4_IS_POWER_OF_10.
+//
+//  Licensing:
+//
+//    This code is distributed under the GNU LGPL license.
+//
+//  Modified:
+//
+//    03 March 2016
+//
+//  Author:
+//
+//    John Burkardt
+//
+        {
+            int i;
+
+            Console.WriteLine("");
+            Console.WriteLine("I4_IS_POWER_OF_10_TEST");
+            Console.WriteLine("  I4_IS_POWER_OF_10 reports whether an I4 is a power of 10.");
+            Console.WriteLine("");
+            Console.WriteLine("  I     I4_IS_POWER_OF_10(I)");
+            Console.WriteLine("");
+
+            for (i = 97; i <= 103; i++)
+            {
+                Console.WriteLine("  " + i.ToString().PadLeft(6)
+                                  + "  " + typeMethods.i4_is_power_of_10(i) + "");
+            }
+
+            return;
+        }
+
+        static void i4_uniform_ab_test()
+
+//****************************************************************************80
+//
+//  Purpose:
+//
+//    I4_UNIFORM_AB_TEST tests I4_UNIFORM_AB.
+//
+//  Licensing:
+//
+//    This code is distributed under the GNU LGPL license. 
+//
+//  Modified:
+//
+//    27 October 2014
+//
+//  Author:
+//
+//    John Burkardt
+//
+        {
+            int a = -100;
+            int b = 200;
+            int i;
+            int j;
+            int seed = 123456789;
+
+            Console.WriteLine("");
+            Console.WriteLine("I4_UNIFORM_AB_TEST");
+            Console.WriteLine("  I4_UNIFORM_AB computes pseudorandom values");
+            Console.WriteLine("  in an interval [A,B].");
+
+            Console.WriteLine("");
+            Console.WriteLine("  The lower endpoint A = " + a + "");
+            Console.WriteLine("  The upper endpoint B = " + b + "");
+            Console.WriteLine("  The initial seed is " + seed + "");
+            Console.WriteLine("");
+
+            for (i = 1; i <= 20; i++)
+            {
+                j = UniformRNG.i4_uniform_ab(a, b, ref seed);
+
+                Console.WriteLine("  " + i.ToString().PadLeft(8)
+                                  + "  " + j.ToString().PadLeft(8) + "");
+            }
+
+            return;
+        }
+
+        static void i4vec_uniform_ab_new_test()
+
+//****************************************************************************80
+//
+//  Purpose:
+//
+//    I4VEC_UNIFORM_AB_NEW_TEST tests I4VEC_UNIFORM_AB_NEW.
+//
+//  Licensing:
+//
+//    This code is distributed under the GNU LGPL license. 
+//
+//  Modified:
+//
+//    27 October 2014
+//
+//  Author:
+//
+//    John Burkardt
+//
+        {
+            int a = -100;
+            int b = 200;
+            int n = 20;
+            int seed = 123456789;
+            int[] v;
+
+            Console.WriteLine("");
+            Console.WriteLine("I4VEC_UNIFORM_AB_NEW_TEST");
+            Console.WriteLine("  I4VEC_UNIFORM_AB_NEW computes pseudorandom values");
+            Console.WriteLine("  in an interval [A,B].");
+
+            Console.WriteLine("");
+            Console.WriteLine("  The lower endpoint A = " + a + "");
+            Console.WriteLine("  The upper endpoint B = " + b + "");
+            Console.WriteLine("  The initial seed is " + seed + "");
+            Console.WriteLine("");
+
+            v = UniformRNG.i4vec_uniform_ab_new(n, a, b, ref seed);
+
+            typeMethods.i4vec_print(n, v, "  The random vector:");
+
+        }
+
+        static void i4vec_unique_count_test()
+
+//****************************************************************************80
+//
+//  Purpose:
+//
+//    I4VEC_UNIQUE_COUNT_TEST tests I4VEC_UNIQUE_COUNT.
+//
+//  Licensing:
+//
+//    This code is distributed under the GNU LGPL license.
+//
+//  Modified:
+//
+//    09 March 2016
+//
+//  Author:
+//
+//    John Burkardt
+//
+        {
+            int[] a;
+            int a_hi;
+            int a_lo;
+            int a_unique;
+            int n;
+            int seed;
+
+            Console.WriteLine("");
+            Console.WriteLine("I4VEC_UNIQUE_COUNT_TEST");
+            Console.WriteLine("  I4VEC_UNIQUE_COUNT counts unique entries in an I4VEC.");
+
+            n = 20;
+            a_lo = 0;
+            a_hi = n;
+            seed = 123456789;
+
+            a = UniformRNG.i4vec_uniform_ab_new(n, a_lo, a_hi, ref seed);
+
+            typeMethods.i4vec_print(n, a, "  Array:");
+
+            a_unique = typeMethods.i4vec_unique_count(n, a);
+
+            Console.WriteLine("");
+            Console.WriteLine("  Number of unique entries is " + a_unique + "");
+            
+        }
+
+    }
+}

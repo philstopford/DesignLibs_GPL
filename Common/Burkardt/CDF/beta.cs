501e208ad89e754b52d54434dadb73fda496a075
philstopford <phil.stopford@gmail.com>
2021-06-04 16:23:49 -0500

    CDFTest

--- Common/Burkardt/CDF/beta.cs
+++ Common/Burkardt/CDF/beta.cs
@@ -4,6 +4,381 @@ namespace Burkardt.CDFLib
 {
     public static partial class CDF
     {
+
+        public static void cdfbet(int which, ref double p, ref double q, ref double x, ref double y,
+                ref double a, ref double b, ref int status, ref double bound)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    CDFBET evaluates the CDF of the Beta Distribution.
+            //
+            //  Discussion:
+            //
+            //    This routine calculates any one parameter of the beta distribution
+            //    given the others.
+            //
+            //    The value P of the cumulative distribution function is calculated
+            //    directly by code associated with the reference.
+            //
+            //    Computation of the other parameters involves a seach for a value that
+            //    produces the desired value of P.  The search relies on the
+            //    monotonicity of P with respect to the other parameters.
+            //
+            //    The beta density is proportional to t^(A-1) * (1-t)^(B-1).
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license. 
+            //
+            //  Modified:
+            //
+            //    14 February 2021
+            //
+            //  Author:
+            //
+            //    Barry Brown, James Lovato, Kathy Russell.
+            //
+            //  Reference:
+            //
+            //    Armido DiDinato and Alfred Morris,
+            //    Algorithm 708:
+            //    Significant Digit Computation of the Incomplete Beta Function Ratios,
+            //    ACM Transactions on Mathematical Software,
+            //    Volume 18, 1993, pages 360-373.
+            //
+            //  Parameters:
+            //
+            //    Input, int *WHICH, indicates which of the next four argument
+            //    values is to be calculated from the others.
+            //    1: Calculate P and Q from X, Y, A and B;
+            //    2: Calculate X and Y from P, Q, A and B;
+            //    3: Calculate A from P, Q, X, Y and B;
+            //    4: Calculate B from P, Q, X, Y and A.
+            //
+            //    Input/output, double *P, the integral from 0 to X of the
+            //    chi-square distribution.  Input range: [0, 1].
+            //
+            //    Input/output, double *Q, equals 1-P.  Input range: [0, 1].
+            //
+            //    Input/output, double *X, the upper limit of integration
+            //    of the beta density.  If it is an input value, it should lie in
+            //    the range [0,1].  If it is an output value, it will be searched for
+            //    in the range [0,1].
+            //
+            //    Input/output, double *Y, equal to 1-X.  If it is an input
+            //    value, it should lie in the range [0,1].  If it is an output value,
+            //    it will be searched for in the range [0,1].
+            //
+            //    Input/output, double *A, the first parameter of the beta
+            //    density.  If it is an input value, it should lie in the range
+            //    (0, +infinity).  If it is an output value, it will be searched
+            //    for in the range [1D-300,1D300].
+            //
+            //    Input/output, double *B, the second parameter of the beta
+            //    density.  If it is an input value, it should lie in the range
+            //    (0, +infinity).  If it is an output value, it will be searched
+            //    for in the range [1D-300,1D300].
+            //
+            //    Output, int *STATUS, reports the status of the computation.
+            //     0, if the calculation completed correctly;
+            //    -I, if the input parameter number I is out of range;
+            //    +1, if the answer appears to be lower than lowest search bound;
+            //    +2, if the answer appears to be higher than greatest search bound;
+            //    +3, if P + Q /= 1;
+            //    +4, if X + Y /= 1.
+            //
+            //    Output, double *BOUND, is only defined if STATUS is nonzero.
+            //    If STATUS is negative, then this is the value exceeded by parameter I.
+            //    if STATUS is 1 or 2, this is the search bound that was exceeded.
+            //
+        {
+            double tol = (1.0e-8);
+            double atol = (1.0e-50);
+            double zero = (1.0e-300);
+            double inf = 1.0e300;
+            double one = 1.0e0;
+
+            double ccum = 0;
+            double cum = 0;
+            double fx = 0;
+            int K1 = 1;
+            double K2 = 0.0e0;
+            double K3 = 1.0e0;
+            double K8 = 0.5e0;
+            double K9 = 5.0e0;
+            double pq;
+            bool qhi = false;
+            bool qleft = false;
+            bool qporq = false;
+            double xhi = 0;
+            double xlo = 0;
+            double xy;
+
+            double T4, T5, T6, T7, T10, T11, T12, T13, T14, T15;
+
+            status = 0;
+            bound = 0.0;
+            //
+            //  Check arguments
+            //
+            if (!(which < 1 || which > 4)) goto S30;
+            if (!(which < 1)) goto S10;
+            bound = 1.0e0;
+            goto S20;
+            S10:
+            bound = 4.0e0;
+            S20:
+            status = -1;
+            return;
+            S30:
+            if (which == 1) goto S70;
+            //
+            //  P
+            //
+            if (!(p < 0.0e0 || p > 1.0e0)) goto S60;
+            if (!(p < 0.0e0)) goto S40;
+            bound = 0.0e0;
+            goto S50;
+            S40:
+            bound = 1.0e0;
+            S50:
+            status = -2;
+            return;
+            S70:
+            S60:
+            if (which == 1) goto S110;
+            //
+            //  Q
+            //
+            if (!(q < 0.0e0 || q > 1.0e0)) goto S100;
+            if (!(q < 0.0e0)) goto S80;
+            bound = 0.0e0;
+            goto S90;
+            S80:
+            bound = 1.0e0;
+            S90:
+            status = -3;
+            return;
+            S110:
+            S100:
+            if (which == 2) goto S150;
+            //
+            //  X
+            //
+            if (!(x < 0.0e0 || x > 1.0e0)) goto S140;
+            if (!(x < 0.0e0)) goto S120;
+            bound = 0.0e0;
+            goto S130;
+            S120:
+            bound = 1.0e0;
+            S130:
+            status = -4;
+            return;
+            S150:
+            S140:
+            if (which == 2) goto S190;
+            //
+            //  Y
+            //
+            if (!(y < 0.0e0 || y > 1.0e0)) goto S180;
+            if (!(y < 0.0e0)) goto S160;
+            bound = 0.0e0;
+            goto S170;
+            S160:
+            bound = 1.0e0;
+            S170:
+            status = -5;
+            return;
+            S190:
+            S180:
+            if (which == 3) goto S210;
+            //
+            //  A
+            //
+            if (!(a <= 0.0e0)) goto S200;
+            bound = 0.0e0;
+            status = -6;
+            return;
+            S210:
+            S200:
+            if (which == 4) goto S230;
+            //
+            //  B
+            //
+            if (!(b <= 0.0e0)) goto S220;
+            bound = 0.0e0;
+            status = -7;
+            return;
+            S230:
+            S220:
+            if (which == 1) goto S270;
+            //
+            //  P + Q
+            //
+            pq = p + q;
+            if (!(Math.Abs(pq - 0.5e0 - 0.5e0) > 3.0e0 * dpmpar(K1))) goto S260;
+            if (!(pq < 0.0e0)) goto S240;
+            bound = 0.0e0;
+            goto S250;
+            S240:
+            bound = 1.0e0;
+            S250:
+            status = 3;
+            return;
+            S270:
+            S260:
+            if (which == 2) goto S310;
+            //
+            //  X + Y
+            //
+            xy = x + y;
+            if (!(Math.Abs(xy - 0.5e0 - 0.5e0) > 3.0e0 * dpmpar(K1))) goto S300;
+            if (!(xy < 0.0e0)) goto S280;
+            bound = 0.0e0;
+            goto S290;
+            S280:
+            bound = 1.0e0;
+            S290:
+            status = 4;
+            return;
+            S310:
+            S300:
+            if (!(which == 1)) qporq = p <= q;
+            //
+            //     Select the minimum of P or Q
+            //     Calculate ANSWERS
+            //
+            if (1 == which)
+            {
+                //
+                //  Calculating P and Q
+                //
+                cumbet(x, y, a, b, ref p, ref q);
+                status = 0;
+            }
+            else if (2 == which)
+            {
+                //
+                //  Calculating X and Y
+                //
+                T4 = atol;
+                T5 = tol;
+                dstzr(K2, K3, T4, T5);
+                if (!qporq) goto S340;
+                status = 0;
+                dzror(ref status, ref x, ref fx, ref xlo, ref xhi, ref qleft, ref qhi);
+                y = one - x;
+                S320:
+                if (!(status == 1)) goto S330;
+                cumbet(x, y, a, b, ref cum, ref ccum);
+                fx = cum - p;
+                dzror(ref status, ref x, ref fx, ref xlo, ref xhi, ref qleft, ref qhi);
+                y = one - x;
+                goto S320;
+                S330:
+                goto S370;
+                S340:
+                status = 0;
+                dzror(ref status, ref y, ref fx, ref xlo, ref xhi, ref qleft, ref qhi);
+                x = one - y;
+                S350:
+                if (!(status == 1)) goto S360;
+                cumbet(x, y, a, b, ref cum, ref ccum);
+                fx = ccum - q;
+                dzror(ref status, ref y, ref fx, ref xlo, ref xhi, ref qleft, ref qhi);
+                x = one - y;
+                goto S350;
+                S370:
+                S360:
+                if (!(status == -1)) goto S400;
+                if (!qleft) goto S380;
+                status = 1;
+                bound = 0.0e0;
+                goto S390;
+                S380:
+                status = 2;
+                bound = 1.0e0;
+                S400:
+                S390: ;
+            }
+            else if (3 == which)
+            {
+                //
+                //  Computing A
+                //
+                a = 5.0e0;
+                T6 = zero;
+                T7 = inf;
+                T10 = atol;
+                T11 = tol;
+                dstinv(T6, T7, K8, K8, K9, T10, T11);
+                status = 0;
+                dinvr(status, ref a, fx, ref qleft, ref qhi);
+                S410:
+                if (!(status == 1)) goto S440;
+                cumbet(x, y, a, b, ref cum, ref ccum);
+                if (!qporq) goto S420;
+                fx = cum - p;
+                goto S430;
+                S420:
+                fx = ccum - q;
+                S430:
+                dinvr(status, ref a, fx, ref qleft, ref qhi);
+                goto S410;
+                S440:
+                if (!(status == -1)) goto S470;
+                if (!qleft) goto S450;
+                status = 1;
+                bound = zero;
+                goto S460;
+                S450:
+                status = 2;
+                bound = inf;
+                S470:
+                S460: ;
+            }
+            else if (4 == which)
+            {
+                //
+                //  Computing B
+                //
+                b = 5.0e0;
+                T12 = zero;
+                T13 = inf;
+                T14 = atol;
+                T15 = tol;
+                dstinv(T12, T13, K8, K8, K9, T14, T15);
+                status = 0;
+                dinvr(status, ref b, fx, ref qleft, ref qhi);
+                S480:
+                if (!(status == 1)) goto S510;
+                cumbet(x, y, a, b, ref cum, ref ccum);
+                if (!qporq) goto S490;
+                fx = cum - p;
+                goto S500;
+                S490:
+                fx = ccum - q;
+                S500:
+                dinvr(status, ref b, fx, ref qleft, ref qhi);
+                goto S480;
+                S510:
+                if (!(status == -1)) goto S540;
+                if (!qleft) goto S520;
+                status = 1;
+                bound = zero;
+                goto S530;
+                S520:
+                status = 2;
+                bound = inf;
+                S530: ;
+            }
+
+            S540:
+            return;
+        }
+
         public static double beta(double a, double b)
 
             //****************************************************************************80
@@ -1603,8 +1978,8 @@ namespace Burkardt.CDFLib
             bup = bup * w;
             return bup;
         }
-        
-        public static double dbetrm ( double a, double b )
+
+        public static double dbetrm(double a, double b)
 
             //****************************************************************************80
             //
@@ -1649,15 +2024,15 @@ namespace Burkardt.CDFLib
             //
             //  Sum from smallest to largest
             //
-            T1 = a+b;
+            T1 = a + b;
             dbetrm = -dstrem(T1);
-            T2 = Math.Max(a,b);
+            T2 = Math.Max(a, b);
             dbetrm = dbetrm + dstrem(T2);
-            T3 = Math.Min(a,b);
+            T3 = Math.Min(a, b);
             dbetrm = dbetrm + dstrem(T3);
 
             return dbetrm;
         }
-        
+
     }
 }
--- Common/Burkardt/CDF/binomial.cs
+++ Common/Burkardt/CDF/binomial.cs
@@ -454,54 +454,58 @@ namespace Burkardt.CDFLib
         {
             int N_MAX = 27;
 
-            double[] cdf_vec =  {
-                0.6367, 0.3633, 0.1445,
-                0.5000, 0.2266, 0.0625,
-                0.3438, 0.1094, 0.0156,
-                0.1792, 0.0410, 0.0041,
-                0.0705, 0.0109, 0.0007,
-                0.9862, 0.9150, 0.7472,
-                0.8499, 0.5497, 0.2662,
-                0.6513, 0.2639, 0.0702,
-                1.0000, 0.0199, 0.0001
-            }
-            ;
-            int[] f_vec =  {
-                4, 3, 2,
-                3, 2, 1,
-                2, 1, 0,
-                2, 1, 0,
-                2, 1, 0,
-                11, 10, 9,
-                17, 16, 15,
-                9, 8, 7,
-                2, 1, 0
-            }
-            ;
-            double[] p_vec =  {
-                0.50, 0.50, 0.50,
-                0.50, 0.50, 0.50,
-                0.50, 0.50, 0.50,
-                0.40, 0.40, 0.40,
-                0.30, 0.30, 0.30,
-                0.30, 0.30, 0.30,
-                0.10, 0.10, 0.10,
-                0.10, 0.10, 0.10,
-                0.01, 0.01, 0.01
-            }
-            ;
-            int[] s_vec =  {
-                4, 5, 6,
-                4, 5, 6,
-                4, 5, 6,
-                4, 5, 6,
-                4, 5, 6,
-                1, 2, 3,
-                1, 2, 3,
-                1, 2, 3,
-                0, 1, 2
-            }
-            ;
+            double[] cdf_vec =
+                {
+                    0.6367, 0.3633, 0.1445,
+                    0.5000, 0.2266, 0.0625,
+                    0.3438, 0.1094, 0.0156,
+                    0.1792, 0.0410, 0.0041,
+                    0.0705, 0.0109, 0.0007,
+                    0.9862, 0.9150, 0.7472,
+                    0.8499, 0.5497, 0.2662,
+                    0.6513, 0.2639, 0.0702,
+                    1.0000, 0.0199, 0.0001
+                }
+                ;
+            int[] f_vec =
+                {
+                    4, 3, 2,
+                    3, 2, 1,
+                    2, 1, 0,
+                    2, 1, 0,
+                    2, 1, 0,
+                    11, 10, 9,
+                    17, 16, 15,
+                    9, 8, 7,
+                    2, 1, 0
+                }
+                ;
+            double[] p_vec =
+                {
+                    0.50, 0.50, 0.50,
+                    0.50, 0.50, 0.50,
+                    0.50, 0.50, 0.50,
+                    0.40, 0.40, 0.40,
+                    0.30, 0.30, 0.30,
+                    0.30, 0.30, 0.30,
+                    0.10, 0.10, 0.10,
+                    0.10, 0.10, 0.10,
+                    0.01, 0.01, 0.01
+                }
+                ;
+            int[] s_vec =
+                {
+                    4, 5, 6,
+                    4, 5, 6,
+                    4, 5, 6,
+                    4, 5, 6,
+                    4, 5, 6,
+                    1, 2, 3,
+                    1, 2, 3,
+                    1, 2, 3,
+                    0, 1, 2
+                }
+                ;
 
             if (n_data < 0)
             {
@@ -527,6 +531,113 @@ namespace Burkardt.CDFLib
             }
         }
 
+        public static void binomial_cdf_values(ref int n_data, ref int a, ref double b, ref int x, ref double fx)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    BINOMIAL_CDF_VALUES returns some values of the binomial CDF.
+            //
+            //  Discussion:
+            //
+            //    CDF(X)(A,B) is the probability of at most X successes in A trials,
+            //    given that the probability of success on a single trial is B.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license. 
+            //
+            //  Modified:
+            //
+            //    14 February 2021
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Reference:
+            //
+            //    Milton Abramowitz and Irene Stegun,
+            //    Handbook of Mathematical Functions,
+            //    US Department of Commerce, 1964.
+            //
+            //    Daniel Zwillinger,
+            //    CRC Standard Mathematical Tables and Formulae,
+            //    30th Edition, CRC Press, 1996, pages 651-652.
+            //
+            //  Parameters:
+            //
+            //    Input/output, int *N_DATA.  The user sets N_DATA to 0 before the
+            //    first call.  On each call, the routine increments N_DATA by 1, and
+            //    returns the corresponding data; when there is no more data, the
+            //    output value of N_DATA will be 0 again.
+            //
+            //    Output, int *A, double *B, the parameters of the function.
+            //
+            //    Output, int *X, the argument of the function.
+            //
+            //    Output, double *FX, the value of the function.
+            //
+        {
+            int N_MAX = 17;
+
+            int[] a_vec =  {
+                2, 2, 2, 2,
+                2, 4, 4, 4,
+                4, 10, 10, 10,
+                10, 10, 10, 10,
+                10
+            }
+            ;
+            double[] b_vec =  {
+                0.05E+00, 0.05E+00, 0.05E+00, 0.50E+00,
+                0.50E+00, 0.25E+00, 0.25E+00, 0.25E+00,
+                0.25E+00, 0.05E+00, 0.10E+00, 0.15E+00,
+                0.20E+00, 0.25E+00, 0.30E+00, 0.40E+00,
+                0.50E+00
+            }
+            ;
+            double[] fx_vec =  {
+                0.9025E+00, 0.9975E+00, 1.0000E+00, 0.2500E+00,
+                0.7500E+00, 0.3164E+00, 0.7383E+00, 0.9492E+00,
+                0.9961E+00, 0.9999E+00, 0.9984E+00, 0.9901E+00,
+                0.9672E+00, 0.9219E+00, 0.8497E+00, 0.6331E+00,
+                0.3770E+00
+            }
+            ;
+            int[] x_vec =  {
+                0, 1, 2, 0,
+                1, 0, 1, 2,
+                3, 4, 4, 4,
+                4, 4, 4, 4,
+                4
+            }
+            ;
+
+            if (n_data < 0)
+            {
+                n_data = 0;
+            }
+
+            n_data = n_data + 1;
+
+            if (N_MAX < n_data)
+            {
+                n_data = 0;
+                a = 0;
+                b = 0.0E+00;
+                x = 0;
+                fx = 0.0E+00;
+            }
+            else
+            {
+                a = a_vec[n_data - 1];
+                b = b_vec[n_data - 1];
+                x = x_vec[n_data - 1];
+                fx = fx_vec[n_data - 1];
+            }
+        }
 
     }
 }

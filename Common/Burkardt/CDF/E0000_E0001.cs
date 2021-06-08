8e584c30b6eac6e37c5dab8486701555c63f1d55
philstopford <phil.stopford@gmail.com>
2021-06-07 20:36:46 -0500

    WIP

--- Common/Burkardt/CDF/E0000_E0001.cs
+++ Common/Burkardt/CDF/E0000_E0001.cs
@@ -600,6 +600,37 @@ namespace Burkardt.CDFLib
             }
         }
 
+        public static double fifdsign ( double mag, double sign )
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    FIFDSIGN transfers the sign of the variable "sign" to the variable "mag"
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license. 
+            //
+            //  Modified:
+            //
+            //    14 February 2021
+            //
+            //  Author:
+            //
+            //    Barry Brown, James Lovato, Kathy Russell.
+            //
+            //  Parameters:
+            //
+            //  mag     -     magnitude
+            //  sign    -     sign to be transfered
+            //
+        {
+            if (mag < 0) mag = -mag;
+            if (sign < 0) mag = -mag;
+            return mag;
+
+        }
 
         public static void E0001(int IENTRY, ref E0000_E0001_Data data)
 

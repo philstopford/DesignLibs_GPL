2a0e49d7c995b6b83c40865bb058b2139a9e916e
philstopford <phil.stopford@gmail.com>
2021-06-09 08:10:16 -0500

    ZeroRC
    Testing my understanding of the static porting approach

---
+++ BurkhardtTest/ZeroRCTest/Program.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using Burkardt.ZeroRC;
+
+namespace Burkardt.ZeroRCTest
+{
+  class Program
+  {
+    static void Main(string[] args)
+      //****************************************************************************80
+      //
+      //  Purpose:
+      //
+      //    zero_rc_test() tests zero_rc().
+      //
+      //  Licensing:
+      //
+      //    This code is distributed under the GNU LGPL license.
+      //
+      //  Modified:
+      //
+      //    29 May 2021
+      //
+      //  Author:
+      //
+      //    John Burkardt
+      //
+    {
+      double a;
+      double b;
+
+      Console.WriteLine("");
+      Console.WriteLine("zero_rc_test()");
+      Console.WriteLine("  C++ version");
+      Console.WriteLine("  zero_rc() seeks a root of a function F(X)");
+      Console.WriteLine("  in an interval [A,B] using reverse communication.");
+
+      a = 1.0;
+      b = 2.0;
+      example_test(a, b, f_01, "f_01(x) = sin ( x ) - x / 2");
+
+      a = 0.0;
+      b = 1.0;
+      example_test(a, b, f_02, "f_02(x) = 2 * x - exp ( - x )");
+
+      a = -1.0;
+      b = 0.5;
+      example_test(a, b, f_03, "f_03(x) = x * exp ( - x )");
+
+      a = 0.0001;
+      b = 20.0;
+      example_test(a, b, f_04, "f_04(x) = exp ( x ) - 1 / ( 100 * x * x )");
+
+      a = -5.0;
+      b = 2.0;
+      example_test(a, b, f_05, "f_05(x) = (x+3) * (x-1) * (x-1)");
+      //
+      //  Terminate.
+      //
+      Console.WriteLine("");
+      Console.WriteLine("zero_rc_test()");
+      Console.WriteLine("  Normal end of execution.");
+      Console.WriteLine("");
+
+    }
+
+    static void example_test(double a, double b, Func<double, double> f, string title )
+
+    //****************************************************************************80
+    //
+    //  Purpose:
+    //
+    //    example_test() tests zero_rc() on one test function.
+    //
+    //  Licensing:
+    //
+    //    This code is distributed under the GNU LGPL license.
+    //
+    //  Modified:
+    //
+    //    29 May 2021
+    //
+    //  Author:
+    //
+    //    John Burkardt
+    //
+    //  Input:
+    //
+    //    double A, B, the endpoints of the change of sign interval.
+    //
+    //    double F ( double x ), the function whose zero is being sought.
+    //
+    //    string TITLE, a title for the problem.
+    //
+    {
+      double arg = 0;
+      double value = 0;
+
+      Console.WriteLine("");
+      Console.WriteLine("  " + title + "");
+      Console.WriteLine("");
+      Console.WriteLine("    STATUS      X               F(X)");
+      Console.WriteLine("");
+
+      double t = Double.Epsilon;
+
+      int status = 0;
+
+      zerorc_data data = new zerorc_data();
+      
+      for (;;)
+      {
+        zerorc.zero_rc(ref data, a, b, t, ref arg, ref status, ref value);
+
+        if (status < 0)
+        {
+          Console.WriteLine("");
+          Console.WriteLine("  zero_rc() returned an error flag!");
+          break;
+        }
+
+        value = f(arg);
+
+        Console.WriteLine("  " + status.ToString().PadLeft(8)
+          + "  " + arg.ToString("0.################").PadLeft(24)
+          + "  " + value.ToString("0.######").PadLeft(14) + "");
+
+        if (status == 0)
+        {
+          break;
+        }
+      }
+    }
+
+    static double f_01(double x)
+
+      //****************************************************************************80
+      //
+      //  Purpose:
+      //
+      //    f_01() evaluates sin ( x ) - x / 2.
+      //
+      //  Licensing:
+      //
+      //    This code is distributed under the GNU LGPL license.
+      //
+      //  Modified:
+      //
+      //    13 April 2008
+      //
+      //  Author:
+      //
+      //    John Burkardt
+      //
+      //  Input:
+      //
+      //    double X, the evaluation point.
+      //
+      //  Output:
+      //
+      //    double F_01, the value of the function at X.
+      //
+    {
+      double value = Math.Sin(x) - 0.5 * x;
+
+      return value;
+    }
+
+    static double f_02(double x)
+
+      //****************************************************************************80
+      //
+      //  Purpose:
+      //
+      //    f_02() evaluates 2*x-exp(-x).
+      //
+      //  Licensing:
+      //
+      //    This code is distributed under the GNU LGPL license.
+      //
+      //  Modified:
+      //
+      //    13 April 2008
+      //
+      //  Author:
+      //
+      //    John Burkardt
+      //
+      //  Input:
+      //
+      //    double X, the evaluation point.
+      //
+      //  Output:
+      //
+      //    double F_02, the value of the function at X.
+      //
+    {
+      double value = 2.0 * x - Math.Exp(-x);
+
+      return value;
+    }
+
+    static double f_03(double x)
+
+      //****************************************************************************80
+      //
+      //  Purpose:
+      //
+      //    f_03() evaluates x*exp(-x).
+      //
+      //  Licensing:
+      //
+      //    This code is distributed under the GNU LGPL license.
+      //
+      //  Modified:
+      //
+      //    13 April 2008
+      //
+      //  Author:
+      //
+      //    John Burkardt
+      //
+      //  Input:
+      //
+      //    double X, the evaluation point.
+      //
+      //  Output:
+      //
+      //    double F_03, the value of the function at X.
+      //
+    {
+      double value = x * Math.Exp(-x);
+
+      return value;
+    }
+
+    static double f_04(double x)
+
+      //****************************************************************************80
+      //
+      //  Purpose:
+      //
+      //    f_04() evaluates exp(x) - 1 / (100*x*x).
+      //
+      //  Licensing:
+      //
+      //    This code is distributed under the GNU LGPL license.
+      //
+      //  Modified:
+      //
+      //    13 April 2008
+      //
+      //  Author:
+      //
+      //    John Burkardt
+      //
+      //  Input:
+      //
+      //    double X, the evaluation point.
+      //
+      //  Output:
+      //
+      //    double F_04, the value of the function at X.
+      //
+    {
+      double value = Math.Exp(x) - 1.0 / 100.0 / x / x;
+
+      return value;
+    }
+
+    static double f_05(double x)
+
+      //****************************************************************************80
+      //
+      //  Purpose:
+      //
+      //    f_05() evaluates (x+3)*(x-1)*(x-1).
+      //
+      //  Licensing:
+      //
+      //    This code is distributed under the GNU LGPL license.
+      //
+      //  Modified:
+      //
+      //    13 April 2008
+      //
+      //  Author:
+      //
+      //    John Burkardt
+      //
+      //  Input:
+      //
+      //    double X, the evaluation point.
+      //
+      //  Output:
+      //
+      //    double F_05, the value of the function at X.
+      //
+    {
+      double value = (x + 3.0) * (x - 1.0) * (x - 1.0);
+
+      return value;
+    }
+  }
+}
---
+++ Common/Burkardt/zerorc.cs
@@ -0,0 +1,238 @@
+﻿using System;
+
+namespace Burkardt.ZeroRC
+{
+    public class zerorc_data
+    {
+        public double c;
+        public double d;
+        public double e;
+        public double fa;
+        public double fb;
+        public double fc;
+        public double sa;
+        public double sb;
+    }
+
+    public static class zerorc
+    {
+        public static void zero_rc(ref zerorc_data data, double a, double b, double t, ref double arg, ref int status,
+        ref double value )
+
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    zero_rc() seeks a root of a function F(X) using reverse communication.
+        //
+        //  Discussion:
+        //
+        //    The interval [A,B] must be a change of sign interval for F.
+        //    That is, F(A) and F(B) must be of opposite signs.  Then
+        //    assuming that F is continuous implies the existence of at least
+        //    one value C between A and B for which F(C) = 0.
+        //
+        //    The location of the zero is determined to within an accuracy
+        //    of 6 * EPSILON * r8_abs ( C ) + 2 * T.
+        //
+        //    The routine is a revised version of the Brent zero finder
+        //    algorithm, using reverse communication.
+        //
+        //    Thanks to Thomas Secretin for pointing out a transcription error in the
+        //    setting of the value of P, 11 February 2013.
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    29 May 2021
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Reference:
+        //
+        //    Richard Brent,
+        //    Algorithms for Minimization Without Derivatives,
+        //    Dover, 2002,
+        //    ISBN: 0-486-41998-3,
+        //    LC: QA402.5.B74.
+        //
+        //  Parameters:
+        //
+        //    Input, double A, B, the endpoints of the change of sign interval.
+        //
+        //    Input, double T, a positive error tolerance.
+        //
+        //    Output, double &ARG, the currently considered point.  The user
+        //    does not need to initialize this value.  On return with STATUS positive,
+        //    the user is requested to evaluate the function at ARG, and return
+        //    the value in VALUE.  On return with STATUS zero, ARG is the routine's
+        //    estimate for the function's zero.
+        //
+        //    Input/output, int &STATUS, used to communicate between
+        //    the user and the routine.  The user only sets STATUS to zero on the first
+        //    call, to indicate that this is a startup call.  The routine returns STATUS
+        //    positive to request that the function be evaluated at ARG, or returns
+        //    STATUS as 0, to indicate that the iteration is complete and that
+        //    ARG is the estimated zero
+        //
+        //    Input, double VALUE, the function value at ARG, as requested
+        //    by the routine on the previous call.
+        //
+        {
+            double m;
+            double p;
+            double q;
+            double r;
+            double s;
+            double tol;
+            //
+            //  Input STATUS = 0.
+            //  Initialize, request F(A).
+            //
+            if (status == 0)
+            {
+                data.sa = a;
+                data.sb = b;
+                data.e = data.sb - data.sa;
+                data.d = data.e;
+
+                status = 1;
+                arg = a;
+                return;
+            }
+            //
+            //  Input STATUS = 1.
+            //  Receive F(A), request F(B).
+            //
+            else if (status == 1)
+            {
+                data.fa = value;
+                status = 2;
+                arg = data.sb;
+                return;
+            }
+            //
+            //  Input STATUS = 2
+            //  Receive F(B).
+            //
+            else if (status == 2)
+            {
+                data.fb = value;
+
+                if (0.0 < data.fa * data.fb)
+                {
+                    status = -1;
+                    return;
+                }
+
+                data.c = data.sa;
+                data.fc = data.fa;
+            }
+            else
+            {
+                data.fb = value;
+
+                if ((0.0 < data.fb && 0.0 < data.fc) || (data.fb <= 0.0 && data.fc <= 0.0))
+                {
+                    data.c = data.sa;
+                    data.fc = data.fa;
+                    data.e = data.sb - data.sa;
+                    data.d = data.e;
+                }
+            }
+
+            //
+            //  Compute the next point at which a function value is requested.
+            //
+            if (Math.Abs(data.fc) < Math.Abs(data.fb))
+            {
+                data.sa = data.sb;
+                data.sb = data.c;
+                data.c = data.sa;
+                data.fa = data.fb;
+                data.fb = data.fc;
+                data.fc = data.fa;
+            }
+
+            tol = 2.0 * double.Epsilon * Math.Abs(data.sb) + t;
+            m = 0.5 * (data.c - data.sb);
+
+            if (Math.Abs(m) <= tol || data.fb == 0.0)
+            {
+                status = 0;
+                arg = data.sb;
+                return;
+            }
+
+            if (Math.Abs(data.e) < tol || Math.Abs(data.fa) <= Math.Abs(data.fb))
+            {
+                data.e = m;
+                data.d = data.e;
+            }
+            else
+            {
+                s = data.fb / data.fa;
+
+                if (data.sa == data.c)
+                {
+                    p = 2.0 * m * s;
+                    q = 1.0 - s;
+                }
+                else
+                {
+                    q = data.fa / data.fc;
+                    r = data.fb / data.fc;
+                    p = s * (2.0 * m * q * (q - r) - (data.sb - data.sa) * (r - 1.0));
+                    q = (q - 1.0) * (r - 1.0) * (s - 1.0);
+                }
+
+                if (0.0 < p)
+                {
+                    q = -q;
+                }
+                else
+                {
+                    p = -p;
+                }
+
+                s = data.e;
+                data.e = data.d;
+
+                if (2.0 * p < 3.0 * m * q - Math.Abs(tol * q) &&
+                    p < Math.Abs(0.5 * s * q))
+                {
+                    data.d = p / q;
+                }
+                else
+                {
+                    data.e = m;
+                    data.d = data.e;
+                }
+            }
+
+            data.sa = data.sb;
+            data.fa = data.fb;
+
+            if (tol < Math.Abs(data.d))
+            {
+                data.sb = data.sb + data.d;
+            }
+            else if (0.0 < m)
+            {
+                data.sb = data.sb + tol;
+            }
+            else
+            {
+                data.sb = data.sb - tol;
+            }
+
+            arg = data.sb;
+            status = status + 1;
+        }
+    }
+}

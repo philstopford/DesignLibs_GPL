ee01823a05d294a2fa5dd4a95c26534acf110c58
philstopford <phil.stopford@gmail.com>
2021-06-17 12:11:04 -0500

    Circle Rules

---
+++ BurkhardtTest/Circle/CircleRulesTest/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Burkardt.CircleNS;
+
+namespace CircleRulesTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    MAIN is the main program for CIRCLE_RULE_TEST.
+            //
+            //  Discussion:
+            //
+            //    CIRCLE_RULE_TEST tests the CIRCLE_RULE library.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    06 April 2014
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            int nt;
+
+            Console.WriteLine("");
+            Console.WriteLine("CIRCLE_RULE:");
+            Console.WriteLine("  C++ version");
+            Console.WriteLine("  Test the CIRCLE_RULE library.");
+
+            nt = 8;
+            test01(nt);
+
+            nt = 32;
+            test01(nt);
+
+            Console.WriteLine("");
+            Console.WriteLine("CIRCLE_RULE:");
+            Console.WriteLine("  Normal end of execution.");
+            Console.WriteLine("");
+        }
+
+        static void test01(int nt)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    TEST01 tests CIRCLE_RULE.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    06 April 2014
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            int[] e = new int[2];
+            int e1;
+            int e2;
+            double exact;
+            int i;
+            double q;
+            double r8_pi = 3.141592653589793;
+            double[] t;
+            double[] w;
+            double x;
+            double y;
+
+            Console.WriteLine("");
+            Console.WriteLine("TEST01");
+            Console.WriteLine("  CIRCLE_RULE can compute a rule Q(f) for the unit circle");
+            Console.WriteLine("  using NT equally spaced angles.");
+            Console.WriteLine("  Estimate integrals I(f) where f = x^e(1) * y^e(2)");
+            Console.WriteLine("  using " + nt + " points.");
+            //
+            //  Compute the quadrature rule.
+            //
+            w = new double[nt];
+            t = new double[nt];
+
+            QuadratureRule.circle_rule(nt, ref w, ref t);
+            //
+            //  Apply it to integrands.
+            //
+            Console.WriteLine("");
+            Console.WriteLine("  E(1)  E(2)    I(f)            Q(f)");
+            Console.WriteLine("");
+            //
+            //  Specify a monomial.
+            //
+            for (e1 = 0; e1 <= 6; e1 = e1 + 2)
+            {
+                e[0] = e1;
+
+                for (e2 = e1; e2 <= 6; e2 = e2 + 2)
+                {
+                    e[1] = e2;
+
+                    q = 0.0;
+                    for (i = 0; i < nt; i++)
+                    {
+                        x = Math.Cos(t[i]);
+                        y = Math.Sin(t[i]);
+                        q = q + w[i] * Math.Pow(x, e[0]) * Math.Pow(y, e[1]);
+                    }
+
+                    q = 2.0 * r8_pi * q;
+
+                    exact = Integrals.circle01_monomial_integral(e);
+
+                    Console.WriteLine("  " + e[0].ToString().PadLeft(2)
+                        + "  " + e[1].ToString().PadLeft(2)
+                        + "  " + exact.ToString().PadLeft(14)
+                        + "  " + q.ToString().PadLeft(14) + "");
+                }
+            }
+        }
+    }
+}
---
+++ Common/Burkardt/Circle/QuadratureRule.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Burkardt.CircleNS
+{
+    public static class QuadratureRule
+    {
+        public static void circle_rule ( int nt, ref double[] w, ref double[] t )
+
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    CIRCLE_RULE computes a quadrature rule for the unit circle.
+        //
+        //  Discussion:
+        //
+        //    The unit circle is the region:
+        //
+        //      x * x + y * y = 1.
+        //
+        //    The integral I(f) is then approximated by
+        //
+        //      Q(f) = 2 * pi * sum ( 1 <= i <= NT ) W(i) * F ( cos(T(i)), sin(T(i)) ).
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    06 April 2014
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, int NT, the number of angles to use.
+        //
+        //    Output, double W[NT], the weights for the rule.
+        //
+        //    Output, double T[NT], the angles for the rule.
+        //
+        {
+            for ( int it = 0; it < nt; it++ )
+            {
+                w[it] = 1.0 / ( double ) ( nt );
+                t[it] = 2.0 * Math.PI * ( double ) ( it ) / ( double ) ( nt );
+            }
+        }
+    }
+}

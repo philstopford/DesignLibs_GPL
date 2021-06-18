c8f97950f91c39131090afd912428b7b09828df4
philstopford <phil.stopford@gmail.com>
2021-06-18 17:39:24 -0500

    FD1D Steady Heat

---
+++ BurkhardtTest/FDM/FD1DHeatSteadyTest/Program.cs
@@ -0,0 +1,574 @@
+﻿using System;
+using Burkardt.FDM;
+using Burkardt.Types;
+
+namespace FD1DHeatSteadyTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            problem1();
+        }
+
+        static void problem1()
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    MAIN is the main program for problem 1.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    19 May 2009
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            double a;
+            double b;
+            int n = 11;
+            double[] u;
+            string u_file;
+            double ua;
+            double ub;
+            double[] x;
+            string x_file;
+
+            Console.WriteLine("");
+            Console.WriteLine("PROBLEM1:");
+            Console.WriteLine("  A test problem for FD1D_HEAT_STEADY.");
+
+            a = 0.0;
+            b = 1.0;
+
+            x = typeMethods.r8vec_even(n, a, b);
+
+            ua = 0.0;
+            ub = 1.0;
+
+            u = FD1D_Heat_Steady.fd1d_heat_steady(n, a, b, ua, ub, k1, f1, x);
+
+            x_file = "problem1_nodes.txt";
+            typeMethods.r8mat_write(x_file, 1, n, x);
+
+            Console.WriteLine("");
+            Console.WriteLine("  X data written to \"" + x_file + "\".");
+
+            u_file = "problem1_values.txt";
+            typeMethods.r8mat_write(u_file, 1, n, u);
+
+            Console.WriteLine("  U data written to \"" + u_file + "\".");
+            //
+            //  Terminate.
+            //
+            Console.WriteLine("");
+            Console.WriteLine("PROBLEM1:");
+            Console.WriteLine("  Normal end of execution.");
+            Console.WriteLine("");
+        }
+
+        static double k1(double x)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    K1 evaluates the heat transfer coefficient K(X).
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    18 May 2009
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Parameters:
+            //
+            //    Input, double X, the position.
+            //
+            //    Output, double K1, the value of K(X).
+            //
+        {
+            double value = 1.0;
+
+            return value;
+        }
+
+        static double f1(double x)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    F1 evaluates the right hand side of the steady state heat equation.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    18 May 2009
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Parameters:
+            //
+            //    Input, double X, the position.
+            //
+            //    Output, double F1, the value of F(X).
+            //
+        {
+            double value = 0.0;
+
+            return value;
+        }
+
+        static void problem2()
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    MAIN is the main program for problem 2.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    18 May 2009
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            double a;
+            double b;
+            int n = 11;
+            double[] u;
+            string u_file;
+            double ua;
+            double ub;
+            double[] x;
+            string x_file;
+
+            Console.WriteLine("");
+            Console.WriteLine("PROBLEM2:");
+            Console.WriteLine("  C++ version");
+            Console.WriteLine("  A test problem for FD1D_HEAT_STEADY.");
+            Console.WriteLine("  Low K, then high K, then moderate K.");
+
+            a = 0.0;
+            b = 1.0;
+
+            x = typeMethods.r8vec_even(n, a, b);
+
+            ua = 0.0;
+            ub = 1.0;
+
+            u = FD1D_Heat_Steady.fd1d_heat_steady(n, a, b, ua, ub, k2, f2, x);
+
+            x_file = "problem2_nodes.txt";
+            typeMethods.r8mat_write(x_file, 1, n, x);
+
+            Console.WriteLine("");
+            Console.WriteLine("  X data written to \"" + x_file + "\".");
+
+            u_file = "problem2_values.txt";
+            typeMethods.r8mat_write(u_file, 1, n, u);
+
+            Console.WriteLine("  U data written to \"" + u_file + "\".");
+            //
+            //  Terminate.
+            //
+            Console.WriteLine("");
+            Console.WriteLine("PROBLEM2");
+            Console.WriteLine("  Normal end of execution.");
+            Console.WriteLine("");
+        }
+
+        static double k2(double x)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    K2 evaluates the heat transfer coefficient K(X).
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    18 May 2009
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Parameters:
+            //
+            //    Input, double X, the position.
+            //
+            //    Output, double K2, the value of K(X).
+            //
+        {
+            double value;
+
+            if (x < 0.5)
+            {
+                value = 0.25;
+            }
+            else if (x < 0.75)
+            {
+                value = 4.0;
+            }
+            else
+            {
+                value = 1.0;
+            }
+
+            return value;
+        }
+
+        static double f2(double x)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    F2 evaluates the right hand side of the steady state heat equation.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    18 May 2009
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Parameters:
+            //
+            //    Input, double X, the position.
+            //
+            //    Output, double F2, the value of F(X).
+            //
+        {
+            double value = 0.0;
+
+            return value;
+        }
+
+        static void problem3()
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    MAIN is the main program for problem 3.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    19 May 2009
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            double a;
+            double b;
+            int n = 21;
+            double[] u;
+            string u_file;
+            double ua;
+            double ub;
+            double[] x;
+            string x_file;
+
+            Console.WriteLine("");
+            Console.WriteLine("PROBLEM3:");
+            Console.WriteLine("  C++ version");
+            Console.WriteLine("  A test problem for FD1D_HEAT_STEADY.");
+            Console.WriteLine("  Interior source term.");
+
+            a = 0.0;
+            b = 1.0;
+
+            x = typeMethods.r8vec_even(n, a, b);
+
+            ua = 0.0;
+            ub = 100.0;
+
+            u = FD1D_Heat_Steady.fd1d_heat_steady(n, a, b, ua, ub, k3, f3, x);
+
+            x_file = "problem3_nodes.txt";
+            typeMethods.r8mat_write(x_file, 1, n, x);
+
+            Console.WriteLine("");
+            Console.WriteLine("  X data written to \"" + x_file + "\".");
+
+            u_file = "problem3_values.txt";
+            typeMethods.r8mat_write(u_file, 1, n, u);
+
+            Console.WriteLine("  U data written to \"" + u_file + "\".");
+            //
+            //  Terminate.
+            //
+            Console.WriteLine("");
+            Console.WriteLine("PROBLEM3:");
+            Console.WriteLine("  Normal end of execution.");
+            Console.WriteLine("");
+        }
+
+        static double k3(double x)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    K3 evaluates the heat transfer coefficient K(X).
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    18 May 2009
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Parameters:
+            //
+            //    Input, double X, the position.
+            //
+            //    Output, double K3, the value of K(X).
+            //
+        {
+            double value = 1.0;
+
+            return value;
+        }
+
+        static double f3(double x)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    F1 evaluates the right hand side of the steady state heat equation.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    18 May 2009
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Parameters:
+            //
+            //    Input, double X, the position.
+            //
+            //    Output, double F3, the value of F(X).
+            //
+        {
+            double value;
+
+            if (x < 0.15)
+            {
+                value = 0.0;
+            }
+            else if (x < 0.45)
+            {
+                value = 200.0;
+            }
+            else
+            {
+                value = 0.0;
+            }
+
+            return value;
+        }
+
+        static void problem4()
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    MAIN is the main program for problem 4.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    19 May 2009
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            double a;
+            double b;
+            int n = 21;
+            double[] u;
+            string u_file;
+            double ua;
+            double ub;
+            double[] x;
+            string x_file;
+
+            Console.WriteLine("");
+            Console.WriteLine("PROBLEM4:");
+            Console.WriteLine("  A test problem for FD1D_HEAT_STEADY.");
+            Console.WriteLine("  A heat source and a heat sink.");
+
+            a = 0.0;
+            b = 1.0;
+
+            x = typeMethods.r8vec_even(n, a, b);
+
+            ua = 0.0;
+            ub = 1.0;
+
+            u = FD1D_Heat_Steady.fd1d_heat_steady(n, a, b, ua, ub, k4, f4, x);
+
+            x_file = "problem4_nodes.txt";
+            typeMethods.r8mat_write(x_file, 1, n, x);
+
+            Console.WriteLine("");
+            Console.WriteLine("  X data written to \"" + x_file + "\".");
+
+            u_file = "problem4_values.txt";
+            typeMethods.r8mat_write(u_file, 1, n, u);
+
+            Console.WriteLine("  U data written to \"" + u_file + "\".");
+            //
+            //  Terminate.
+            //
+            Console.WriteLine("");
+            Console.WriteLine("PROBLEM4:");
+            Console.WriteLine("  Normal end of execution.");
+            Console.WriteLine("");
+        }
+
+        static double k4(double x)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    K4 evaluates the heat transfer coefficient K(X).
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    18 May 2009
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Parameters:
+            //
+            //    Input, double X, the position.
+            //
+            //    Output, double K4, the value of K(X).
+            //
+        {
+            double value = 1.0;
+
+            return value;
+        }
+
+        static double f4(double x)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    F4 evaluates the right hand side of the steady state heat equation.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    18 May 2009
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Parameters:
+            //
+            //    Input, double X, the position.
+            //
+            //    Output, double F4, the value of F(X).
+            //
+        {
+            double value;
+
+            if (x < 0.15)
+            {
+                value = 0.0;
+            }
+            else if (x < 0.35)
+            {
+                value = 1.0;
+            }
+            else if (x < 0.75)
+            {
+                value = 0.0;
+            }
+            else if (x < 0.85)
+            {
+                value = -2.0;
+            }
+            else
+            {
+                value = 0.0;
+            }
+
+            return value;
+        }
+   }
+}
---
+++ Common/Burkardt/FDM/FD1D_Heat_Steady.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Burkardt.Types;
+
+namespace Burkardt.FDM
+{
+    public static class FD1D_Heat_Steady
+    {
+        public static double[] fd1d_heat_steady(int n, double a, double b, double ua, double ub,
+            Func < double, double > k, Func<double, double> f, double[] x )
+
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    FD1D_HEAT_STEADY solves the steady 1D heat equation.
+        //
+        //  Discussion:
+        //
+        //    This program seeks a solution of the steady heat equation:
+        //
+        //      - d/dx ( K(X) dUdx ) = F(X)
+        //
+        //    over the interval [A,B] with boundary conditions
+        //
+        //      U(A) = UA,
+        //      U(B) = UB.
+        //
+        //    The code uses the finite difference method to approximate the
+        //    second derivative in space.  This results in a sparse linear system.
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    18 May 2009
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, int N, the number of grid points.
+        //
+        //    Input, double A, B, the interval endpoints.
+        //
+        //    Input, double UA, UB, the values prescribed for U at the endpoints.
+        //
+        //    Input, double K ( double X ), evaluates the thermal conductance at the N
+        //    points X.  Set K(X) = 1 if you don't care about this coefficient.
+        //
+        //    Input, double F ( double X ), evaluates the heat source term at the N
+        //    points X.  Set F(X) = 0 if you don't want any heat sources.
+        //
+        //    Input, double X[N], the grid points.
+        //
+        //    Output, double FD1D_HEAT_STEADY[N], the approximation to the solution
+        //    at the grid points.
+        //
+        {
+            double dx;
+            int i;
+            double[] rhs;
+            double[] tri;
+            double[] u;
+            double xm;
+            double xp;
+
+            Console.WriteLine("");
+            Console.WriteLine("FD1D_HEAT_STEADY");
+            Console.WriteLine("  C++ version");
+            Console.WriteLine("");
+            Console.WriteLine("  Finite difference solution of");
+            Console.WriteLine("  the steady 1D heat equation");
+            Console.WriteLine("");
+            Console.WriteLine("    - d/dx ( k(x) dUdx ) = F(x)");
+            Console.WriteLine("");
+            Console.WriteLine("  for space interval A <= X <= B with boundary conditions");
+            Console.WriteLine("");
+            Console.WriteLine("    U(A) = UA");
+            Console.WriteLine("    U(B) = UB");
+            Console.WriteLine("");
+            Console.WriteLine("  A second order difference approximation is used.");
+            //
+            //  Set the spacing.
+            //
+            dx = (b - a) / (double) (n - 1);
+            //
+            //  Set up the tridiagonal matrix.
+            //
+            tri = new double[3 * n];
+            rhs = new double[n];
+
+            tri[0 + 0 * 3] = 0.0;
+            tri[1 + 0 * 3] = 1.0;
+            tri[2 + 0 * 3] = 0.0;
+            rhs[0] = ua;
+
+            for (i = 1; i < n - 1; i++)
+            {
+                xm = (x[i - 1] + x[i]) / 2.0;
+                xp = (x[i] + x[i + 1]) / 2.0;
+
+                tri[0 + i * 3] = -k(xm) / dx / dx;
+                tri[1 + i * 3] = (k(xm) + k(xp)) / dx / dx;
+                tri[2 + i * 3] = -k(xp) / dx / dx;
+
+                rhs[i] = f(x[i]);
+            }
+
+            tri[0 + (n - 1) * 3] = 0.0;
+            tri[1 + (n - 1) * 3] = 1.0;
+            tri[2 + (n - 1) * 3] = 0.0;
+            rhs[n - 1] = ub;
+            //
+            //  Solve the linear system.
+            //
+            u = typeMethods.r83np_fs(n, ref tri, rhs);
+
+            return u;
+        }
+    }
+}

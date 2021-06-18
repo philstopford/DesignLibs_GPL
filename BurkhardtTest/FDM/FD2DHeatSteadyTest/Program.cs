31a218db848ee260577936104a5a0b935babaed5
philstopford <phil.stopford@gmail.com>
2021-06-18 18:06:52 -0500

    FD2D Heat Steady

---
+++ BurkhardtTest/FDM/FD2DHeatSteadyTest/Program.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Burkardt.FDM;
+using Burkardt.Types;
+
+namespace FD2DHeatSteadyTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    MAIN is the main program for FD2D_HEAT_STEADY_TEST.
+            //
+            //  Discussion:
+            //
+            //    FD2D_HEAT_STEADY_TEST tests the FD2D_HEAT_STEADY library.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    29 August 2013
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            Console.WriteLine("");
+            Console.WriteLine("FD2D_HEAT_STEADY_TEST:");
+            Console.WriteLine("  C++ version");
+            Console.WriteLine("  Test the FD2D_HEAT_STEADY library.");
+
+            test01();
+            Console.WriteLine("");
+            Console.WriteLine("FD2D_HEAT_STEADY_TEST:");
+            Console.WriteLine("  Normal end of execution.");
+            Console.WriteLine("");
+        }
+
+        static void test01()
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    TEST01 computes the solution for a steady state heat equation problem.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    29 August 2013
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            string command_filename = "test01_commands.txt";
+            List<string> command_unit = new List<string>();
+            string data_filename = "test01_data.txt";
+            List<string> data_unit = new List<string>();
+            int i;
+            int j;
+            int nx;
+            int ny;
+            double[] umat;
+            double u_mean;
+            double[] xmat;
+            double[] xvec;
+            double[] ymat;
+            double[] yvec;
+            //
+            //  Specify the spatial grid.
+            //
+            nx = 41;
+            xvec = typeMethods.r8vec_linspace_new(nx, 0.0, 2.0);
+
+            ny = 21;
+            yvec = typeMethods.r8vec_linspace_new(ny, 0.0, 1.0);
+
+            xmat = new double[nx * ny];
+            ymat = new double[nx * ny];
+            typeMethods.r8vec_mesh_2d(nx, ny, xvec, yvec, ref xmat, ref ymat);
+            //
+            //  Solve the finite difference approximation to the steady 2D heat equation.
+            //
+            umat = FD2D_Heat_Steady.fd2d_heat_steady(nx, ny, xvec, yvec, d, f);
+            //
+            //  Create a data file.
+            //
+            for (j = 0; j < ny; j++)
+            {
+                for (i = 0; i < nx; i++)
+                {
+                    data_unit.Add("  " + xmat[i + j * nx].ToString().PadLeft(14)
+                        + "  " + ymat[i + j * nx].ToString().PadLeft(14)
+                        + "  " + umat[i + j * nx].ToString().PadLeft(14) + "");
+                }
+
+                data_unit.Add("");
+            }
+            
+            File.WriteAllLines(data_filename, data_unit);
+
+            Console.WriteLine("");
+            Console.WriteLine("  Created graphics data file '" + data_filename + "'");
+            //
+            //  Create the command file.
+            //
+
+            command_unit.Add("# " + command_filename + "");
+            command_unit.Add("#");
+            command_unit.Add("# Usage:");
+            command_unit.Add("#  gnuplot < " + command_filename + "");
+            command_unit.Add("#");
+            command_unit.Add("set term png");
+            command_unit.Add("set output 'test01.png'");
+            command_unit.Add("set xlabel '<---X--->'");
+            command_unit.Add("set ylabel '<---Y--->'");
+            command_unit.Add("set zlabel '<---U(X,Y)--->'");
+            command_unit.Add("set title 'Sample Solution'");
+            command_unit.Add("set contour");
+            command_unit.Add("set timestamp");
+            command_unit.Add("set cntrparam levels 10");
+            command_unit.Add("set view 75, 75");
+            command_unit.Add("unset key");
+            command_unit.Add("splot '" + data_filename + "'");
+
+            File.WriteAllLines(command_filename, command_unit);
+            
+            Console.WriteLine("  Created graphics command file '" + command_filename + "'");
+            //
+            //  Report the average value of U.
+            //
+            u_mean = 0.0;
+            for (j = 0; j < ny; j++)
+            {
+                for (i = 0; i < nx; i++)
+                {
+                    u_mean = u_mean + umat[i + j * nx];
+                }
+            }
+
+            u_mean = u_mean / (double) (nx * ny);
+
+            Console.WriteLine("");
+            Console.WriteLine("  Mean value of U is " + u_mean + "");
+        }
+
+        static double d(double x, double y)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    D evaluates the heat conductivity coefficient.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    29 August 2013
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Parameters:
+            //
+            //    Input, double X, Y, the evaluation point.
+            //
+            //    Output, double D, the value of the heat conductivity at (X,Y).
+            //
+        {
+            double value = 1.0;
+
+            return value;
+        }
+
+        static double f(double x, double y)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    F evaluates the heat source term.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    29 August 2013
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Parameters:
+            //
+            //    Input, double X, Y, the evaluation point.
+            //
+            //    Output, double F, the value of the heat source term at (X,Y).
+            //
+        {
+            double value = 0.0;
+
+            return value;
+        }
+    }
+}
---
+++ Common/Burkardt/FDM/FD2D_Heat_Steady.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using Burkardt.Types;
+
+namespace Burkardt.FDM
+{
+    public static class FD2D_Heat_Steady
+    {
+        public static double[] fd2d_heat_steady(int nx, int ny, double[] x, double[] y,
+                Func<double, double, double> d, Func<double, double, double> f)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    FD2D_HEAT_STEADY solves the steady 2D heat equation.
+            //
+            //  Discussion:
+            //
+            //    Nodes are assigned a single index K, which increases as:
+            //
+            //    (NY-1)*NX+1  (NY-1)*NX+2  ...  NY * NX
+            //           ....         ....  ...    .....
+            //           NX+1         NX+2  ...   2 * NX
+            //              1            2  ...       NX
+            //
+            //    Therefore, the neighbors of an interior node numbered C are
+            //
+            //             C+NY
+            //              |
+            //      C-1 --- C --- C+1
+            //              |
+            //             C-NY
+            //
+            //    Nodes on the lower boundary satisfy:
+            //      1 <= K <= NX
+            //    Nodes on the upper boundary satisfy:
+            //      (NY-1)*NX+1 <= K <= NY * NX
+            //    Nodes on the left boundary satisfy:
+            //      mod ( K, NX ) = 1
+            //    Nodes on the right boundary satisfy:
+            //      mod ( K, NX ) = 0
+            //
+            //    If we number rows from bottom I = 1 to top I = NY
+            //    and columns from left J = 1 to right J = NX, we have
+            //      K = ( I - 1 ) * NX + J
+            //    and
+            //      J = 1 + mod ( K - 1, NX )
+            //      I = 1 + ( K - J ) / NX
+            //      
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    29 August 2013
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Parameters:
+            //
+            //    Input, int NX, NY, the number of grid points in X and Y.
+            //
+            //    Input, double X[NX], Y[NY], the coordinates of grid lines.
+            //
+            //    Input, double D ( X, Y ), evaluates the thermal
+            //    conductivity.
+            //
+            //    Input, double F ( X, Y ), evaluates the heat 
+            //    source term.
+            //
+            //    Output, double FD2D_HEAT_STEADY[NX*NY], the approximation to the solution
+            //    at the grid points.
+            //
+        {
+            double[] a;
+            int n;
+            double[] u;
+            //
+            //  Set the total number of unknowns.
+            //
+            n = nx * ny;
+            //
+            //  Set up the matrix and right hand side.
+            //
+            a = new double[n * n];
+            u = new double[n];
+            //
+            //  Define the matrix at interior points.
+            //
+            interior(nx, ny, x, y, d, f, n, ref a, ref u);
+            //
+            //  Handle boundary conditions.
+            //
+            boundary(nx, ny, x, y, n, a, u);
+            //
+            //  Solve the linear system.
+            //
+            typeMethods.r8mat_fs(n, a, ref u);
+
+            return u;
+        }
+
+        public static void interior(int nx, int ny, double[] x, double[] y,
+                Func<double, double, double> d, Func<double, double, double> f, int n,
+                ref double[] a, ref double[] rhs)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    INTERIOR sets up the matrix and right hand side at interior nodes.
+            //
+            //  Discussion:
+            //
+            //    Nodes are assigned a single index K, which increases as:
+            //
+            //    (NY-1)*NX+1  (NY-1)*NX+2  ...  NY * NX
+            //           ....         ....  ...    .....
+            //           NX+1         NX+2  ...   2 * NX
+            //              1            2  ...       NX
+            //
+            //    Therefore, the neighbors of an interior node numbered C are
+            //
+            //             C+NY
+            //              |
+            //      C-1 --- C --- C+1
+            //              |
+            //             C-NY
+            //
+            //    If we number rows from bottom I = 1 to top I = NY
+            //    and columns from left J = 1 to right J = NX, then the relationship
+            //    between the single index K and the row and column indices I and J is:
+            //      K = ( I - 1 ) * NX + J
+            //    and
+            //      J = 1 + mod ( K - 1, NX )
+            //      I = 1 + ( K - J ) / NX
+            //      
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    29 August 2013
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Parameters:
+            //
+            //    Input, int NX, NY, the number of grid points in X and Y.
+            //
+            //    Input, double X[NX], Y[NY], the coordinates of grid lines.
+            //
+            //    Input, double D ( double X, double Y ), evaluates the thermal
+            //    conductivity.
+            //
+            //    Input, double function F ( double X, double Y ), evaluates the heat 
+            //    source term.
+            //
+            //    Input, int N, the number of nodes.
+            //
+            //    Output, double A[N*N], the system matrix, with the entries for 
+            //    the interior nodes filled in.
+            //
+            //    Output, double RHS[N], the system right hand side, with the 
+            //    entries for the interior nodes filled in.
+            //
+        {
+            double dce;
+            double dcn;
+            double dcs;
+            double dcw;
+            double dx;
+            double dy;
+            int ic;
+            int in_;
+            int is_;
+            int jc;
+            int je;
+            int jw;
+            int kc;
+            int ke;
+            int kn;
+            int ks;
+            int kw;
+            //
+            //  For now, assume X and Y are equally spaced.
+            //
+            dx = x[1] - x[0];
+            dy = y[1] - y[0];
+
+            for (ic = 1; ic < ny - 1; ic++)
+            {
+                for (jc = 1; jc < nx - 1; jc++)
+                {
+                    in_ = ic + 1;
+                    is_ = ic - 1;
+                    je = jc + 1;
+                    jw = jc - 1;
+
+                    kc = ic * nx + jc;
+                    ke = kc + 1;
+                    kw = kc - 1;
+                    kn = kc + nx;
+                    ks = kc - nx;
+
+                    dce = d(0.5 * (x[jc] + x[je]), y[ic]);
+                    dcw = d(0.5 * (x[jc] + x[jw]), y[ic]);
+                    dcn = d(x[jc], 0.5 * (y[ic] + y[in_]));
+                    dcs = d(x[jc], 0.5 * (y[ic] + y[is_]));
+
+                    a[kc + kc * n] = (dce + dcw) / dx / dx + (dcn + dcs) / dy / dy;
+                    a[kc + ke * n] = -dce / dx / dx;
+                    a[kc + kw * n] = -dcw / dx / dx;
+                    a[kc + kn * n] = -dcn / dy / dy;
+                    a[kc + ks * n] = -dcs / dy / dy;
+
+                    rhs[kc] = f(x[jc], y[ic]);
+                }
+            }
+        }
+
+        public static void boundary(int nx, int ny, double[] x, double[] y, int n, double[] a,
+        double[] rhs )
+
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    BOUNDARY sets up the matrix and right hand side at boundary nodes.
+        //
+        //  Discussion:
+        //
+        //    For this simple problem, the boundary conditions specify that the solution
+        //    is 100 on the left side, and insulated on the right, top and bottom.
+        //
+        //    Nodes are assigned a single index K, which increases as:
+        //
+        //    (NY-1)*NX+1  (NY-1)*NX+2  ...  NY * NX
+        //           ....         ....  ...    .....
+        //           NX+1         NX+2  ...   2 * NX
+        //              1            2  ...       NX
+        //
+        //    The index K of a node on the lower boundary satisfies:
+        //      1 <= K <= NX
+        //    The index K of a node on the upper boundary satisfies:
+        //      (NY-1)*NX+1 <= K <= NY * NX
+        //    The index K of a node on the left boundary satisfies:
+        //      mod ( K, NX ) = 1
+        //    The index K of a node on the right boundary satisfies:
+        //      mod ( K, NX ) = 0
+        //
+        //    If we number rows from bottom I = 1 to top I = NY
+        //    and columns from left J = 1 to right J = NX, then the relationship
+        //    between the single index K and the row and column indices I and J is:
+        //      K = ( I - 1 ) * NX + J
+        //    and
+        //      J = 1 + mod ( K - 1, NX )
+        //      I = 1 + ( K - J ) / NX
+        //      
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    29 August 2013
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, int NX, NY, the number of grid points in X and Y.
+        //
+        //    Input, double X[NX], Y[NY], the coordinates of grid lines.
+        //
+        //    Input, int N, the number of nodes.
+        //
+        //    Input/output, double A[N*N].  On input, the system matrix, with the 
+        //    entries for the interior nodes filled in.  On output, the entries for
+        //    the boundary nodes have been set as well.
+        //
+        //    Input, double RHS[N], on input, the system right hand side, 
+        //    with the entries for the interior nodes filled in.  On output, the entries for
+        //    the boundary nodes have been set as well.
+        //
+        {
+            int i;
+            int j;
+            int kc;
+            //
+            //  Left boundary.
+            //
+            j = 0;
+            for (i = 1; i < ny - 1; i++)
+            {
+                kc = i * nx + j;
+                a[kc + kc * n] = a[kc + kc * n] + 1.0;
+                rhs[kc] = 10.0;
+            }
+
+            //
+            //  Right boundary.
+            //
+            j = nx - 1;
+            for (i = 1; i < ny - 1; i++)
+            {
+                kc = i * nx + j;
+                a[kc + kc * n] = a[kc + kc * n] + 1.0;
+                rhs[kc] = 100.0;
+            }
+
+            //
+            //  Lower boundary.
+            //
+            i = 0;
+            for (j = 0; j < nx; j++)
+            {
+                kc = i * nx + j;
+                a[kc + kc * n] = a[kc + kc * n] + 1.0;
+                rhs[kc] = 0.0;
+            }
+
+            //
+            //  Upper boundary.
+            //
+            i = ny - 1;
+            for (j = 0; j < nx; j++)
+            {
+                kc = i * nx + j;
+                a[kc + kc * n] = a[kc + kc * n] + 1.0;
+                rhs[kc] = 0.0;
+            }
+        }
+    }
+}

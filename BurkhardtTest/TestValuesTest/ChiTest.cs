2fa2cc62363b43a389d8734f4b6a5ac0c311a8e2
philstopford <phil.stopford@gmail.com>
2021-07-01 10:51:26 -0500

    TestValuesTest WIP

---
+++ BurkhardtTest/TestValuesTest/ChiTest.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using TestValues;
+
+namespace TestValuesTest
+{
+    public class ChiTest
+    {
+        public static void chi_values_test()
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    CHI_VALUES_TEST tests CHI_VALUES.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license. 
+            //
+            //  Modified:
+            //
+            //    11 June 2007
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            double fx = 0;
+            int n_data;
+            double x = 0;
+            Console.WriteLine("");
+            Console.WriteLine("CHI_VALUES_TEST:");
+            Console.WriteLine("  CHI_VALUES stores values of");
+            Console.WriteLine("  the Hyperbolic Cosine Integral function CHI(X).");
+            Console.WriteLine("");
+            Console.WriteLine("      X            CHI(X)");
+            Console.WriteLine("");
+            n_data = 0;
+            for (;;)
+            {
+                Chi.chi_values(ref n_data, ref x, ref fx);
+                if (n_data == 0)
+                {
+                    break;
+                }
+
+                Console.WriteLine("  "
+                                  +x.ToString().PadLeft(12) + "  "
+                    + fx.ToString().PadLeft(12) + "");
+            }
+        }
+
+        public static void chi_square_cdf_values_test()
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    CHI_SQUARE_CDF_VALUES_TEST tests CHI_SQUARE_CDF_VALUES.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license. 
+            //
+            //  Modified:
+            //
+            //    09 February 2007
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            int a = 0;
+            double fx = 0;
+            int n_data;
+            double x = 0;
+            Console.WriteLine("");
+            Console.WriteLine("CHI_SQUARE_CDF_VALUES_TEST:");
+            Console.WriteLine("  CHI_SQUARE_CDF_VALUES returns values of ");
+            Console.WriteLine("  the Chi-Squared Cumulative Density Function.");
+            Console.WriteLine("");
+            Console.WriteLine("     N       X    CDF(X)");
+            Console.WriteLine("");
+            n_data = 0;
+            for (;;)
+            {
+                Chi.chi_square_cdf_values(ref n_data, ref a, ref x, ref fx);
+                if (n_data == 0)
+                {
+                    break;
+                }
+
+                Console.WriteLine("  "
+                                  +a.ToString().PadLeft(6) + "  "
+                    + x.ToString().PadLeft(8) + "  "
+                    + fx.ToString().PadLeft(12) + "");
+            }
+        }
+
+        public static void chi_square_pdf_values_test()
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    CHI_SQUARE_PDF_VALUES_TEST tests CHI_SQUARE_PDF_VALUES.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license. 
+            //
+            //  Modified:
+            //
+            //    01 August 2015
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            double df = 0;
+            double fx = 0;
+            int n_data;
+            double x = 0;
+            Console.WriteLine("");
+            Console.WriteLine("CHI_SQUARE_PDF_VALUES_TEST:");
+            Console.WriteLine("  CHI_SQUARE_PDF_VALUES returns values of ");
+            Console.WriteLine("  the Chi-Squared Probability Density Function.");
+            Console.WriteLine("");
+            Console.WriteLine("     DF         X    PDF(X)");
+            Console.WriteLine("");
+            n_data = 0;
+            for (;;)
+            {
+                Chi.chi_square_pdf_values(ref n_data, ref df, ref x, ref fx);
+                if (n_data == 0)
+                {
+                    break;
+                }
+
+                Console.WriteLine("  "
+                                  +df.ToString().PadLeft(8) + "  "
+                    + x.ToString().PadLeft(8) + "  "
+                    + fx.ToString().PadLeft(12) + "");
+            }
+        }
+
+        public static void chi_square_noncentral_cdf_values_test()
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    CHI_SQUARE_NONCENTRAL_CDF_VALUES_TEST tests CHI_SQUARE_NONCENTRAL_CDF_VALUES.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license. 
+            //
+            //  Modified:
+            //
+            //    09 February 2007
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            int df = 0;
+            double fx = 0;
+            double lambda = 0;
+            ;
+            int n_data;
+            double x = 0;
+            Console.WriteLine("");
+            Console.WriteLine("CHI_SQUARE_NONCENTRAL_CDF_VALUES_TEST:");
+            Console.WriteLine("  CHI_SQUARE_NONCENTRAL_CDF_VALUES returns values of");
+            Console.WriteLine("  the noncentral Chi-Squared Cumulative Density Function.");
+            Console.WriteLine("");
+            Console.WriteLine("      X      LAMBDA     DF     CDF");
+            Console.WriteLine("");
+            n_data = 0;
+            for (;;)
+            {
+                Chi.chi_square_noncentral_cdf_values(ref n_data, ref df, ref lambda, ref x, ref fx);
+                if (n_data == 0)
+                {
+                    break;
+                }
+
+                Console.WriteLine("  "
+                                  +x.ToString().PadLeft(10) + "  "
+                    + lambda.ToString().PadLeft(8) + "  "
+                    + df.ToString().PadLeft(4) + "  "
+                    + fx.ToString().PadLeft(12) + "");
+            }
+        }
+
+    }
+}
---
+++ BurkhardtTest/TestValuesTest/CosineTest.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using TestValues;
+
+namespace TestValuesTest
+{
+    public static class CosineTest
+    {
+        public static void ci_values_test()
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    CI_VALUES_TEST tests CI_VALUES.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license. 
+            //
+            //  Modified:
+            //
+            //    09 February 2007
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            double fx = 0;
+            int n_data;
+            double x = 0;
+            Console.WriteLine("");
+            Console.WriteLine("CI_VALUES_TEST:");
+            Console.WriteLine("  CI_VALUES stores values of");
+            Console.WriteLine("  the Cosine Integral function CI(X).");
+            Console.WriteLine("");
+            Console.WriteLine("      X            CI(X)");
+            Console.WriteLine("");
+            n_data = 0;
+            for (;;)
+            {
+                Cosine.ci_values(ref n_data, ref x, ref fx);
+                if (n_data == 0)
+                {
+                    break;
+                }
+
+                Console.WriteLine("  "
+                                  + x.ToString().PadLeft(12) + "  "
+                                  + fx.ToString().PadLeft(12) + "");
+            }
+        }
+
+        public static void cin_values_test()
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    CIN_VALUES_TEST tests CIN_VALUES.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license. 
+            //
+            //  Modified:
+            //
+            //    09 February 2007
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            double fx = 0;
+            int n_data;
+            double x = 0;
+            Console.WriteLine("");
+            Console.WriteLine("CIN_VALUES_TEST:");
+            Console.WriteLine("  CIN_VALUES stores values of");
+            Console.WriteLine("  the Cosine Integral function CIN(X).");
+            Console.WriteLine("");
+            Console.WriteLine("      X            CIN(X)");
+            Console.WriteLine("");
+            n_data = 0;
+            for (;;)
+            {
+                Cosine.cin_values(ref n_data, ref x, ref fx);
+                if (n_data == 0)
+                {
+                    break;
+                }
+
+                Console.WriteLine("  "
+                                  + x.ToString().PadLeft(12) + "  "
+                                  + fx.ToString().PadLeft(12) + "");
+            }
+        }
+
+        public static void cinh_values_test()
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    CINH_VALUES_TEST tests CINH_VALUES.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license. 
+            //
+            //  Modified:
+            //
+            //    21 January 2015
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            double fx = 0;
+            int n_data;
+            double x = 0;
+            Console.WriteLine("");
+            Console.WriteLine("CINH_VALUES_TEST:");
+            Console.WriteLine("  CINH_VALUES stores values of");
+            Console.WriteLine("  the Hyperbolic Cosine Integral function CINH(X).");
+            Console.WriteLine("");
+            Console.WriteLine("      X            CINH(X)");
+            Console.WriteLine("");
+            n_data = 0;
+            for (;;)
+            {
+                Cosine.cinh_values(ref n_data, ref x, ref fx);
+                if (n_data == 0)
+                {
+                    break;
+                }
+
+                Console.WriteLine("  "
+                                  + x.ToString().PadLeft(12) + "  "
+                                  + fx.ToString().PadLeft(12) + "");
+            }
+        }
+
+        public static void cos_values_test()
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    COS_VALUES_TEST tests COS_VALUES.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license. 
+            //
+            //  Modified:
+            //
+            //    12 June 2007
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            double fx = 0;
+            int n_data;
+            double x = 0;
+            Console.WriteLine("");
+            Console.WriteLine("COS_VALUES_TEST:");
+            Console.WriteLine("   COS_VALUES stores values of the cosine function.");
+            Console.WriteLine("");
+            Console.WriteLine("                X                     FX");
+            Console.WriteLine("");
+            n_data = 0;
+            for (;;)
+            {
+                Cosine.cos_values(ref n_data, ref x, ref fx);
+                if (n_data == 0)
+                {
+                    break;
+                }
+
+                Console.WriteLine("  "
+                                  + x.ToString("0.################").PadLeft(24) + "  "
+                                  + fx.ToString("0.################").PadLeft(24) + "");
+            }
+        }
+
+        public static void cos_degree_values_test()
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    COS_DEGREE_VALUES_TEST tests COS_DEGREE_VALUES.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license. 
+            //
+            //  Modified:
+            //
+            //    12 June 2007
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            double fx = 0;
+            int n_data;
+            double x = 0;
+            Console.WriteLine("");
+            Console.WriteLine("COS_DEGREE_VALUES_TEST:");
+            Console.WriteLine("   COS_DEGREE_VALUES stores values of the cosine function.");
+            Console.WriteLine("");
+            Console.WriteLine("                X                     FX");
+            Console.WriteLine("");
+            n_data = 0;
+            for (;;)
+            {
+                Cosine.cos_degree_values(ref n_data, ref x, ref fx);
+                if (n_data == 0)
+                {
+                    break;
+                }
+
+                Console.WriteLine("  "
+                                  + x.ToString("0.################").PadLeft(24) + x + "  "
+                                  + fx.ToString("0.################").PadLeft(24) + "");
+            }
+        }
+
+        public static void cos_power_int_values_test()
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    COS_POWER_INT_VALUES_TEST tests COS_POWER_INT_VALUES.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license. 
+            //
+            //  Modified:
+            //
+            //    21 January 2015
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            double a = 0;
+            double b = 0;
+            double fx = 0;
+            int n = 0;
+            int n_data;
+            Console.WriteLine("");
+            Console.WriteLine("COS_POWER_INT_VALUES_TEST:");
+            Console.WriteLine("  COS_POWER_INT_VALUES returns values of");
+            Console.WriteLine("  the integral of the N-th power of the cosine function.");
+            Console.WriteLine("");
+            Console.WriteLine("         A         B       N        FX");
+            Console.WriteLine("");
+            n_data = 0;
+            for (;;)
+            {
+                Cosine.cos_power_int_values(ref n_data, ref a, ref b, ref n, ref fx);
+                if (n_data == 0)
+                {
+                    break;
+                }
+
+                Console.WriteLine("  "
+                                  + a.ToString().PadLeft(8) + "  "
+                                  + b.ToString().PadLeft(8) + "  "
+                                  + n.ToString().PadLeft(6) + "  "
+                                  + fx.ToString("0.################").PadLeft(24) + "");
+            }
+        }
+
+        public static void cosh_values_test()
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    COSH_VALUES_TEST tests COSH_VALUES.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license. 
+            //
+            //  Modified:
+            //
+            //    23 June 2007
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            double fx = 0;
+            int n_data;
+            double x = 0;
+            Console.WriteLine("");
+            Console.WriteLine("COSH_VALUES_TEST:");
+            Console.WriteLine("   COSH_VALUES stores values of the hyperbolic cosine function.");
+            Console.WriteLine("");
+            Console.WriteLine("                X                     FX");
+            Console.WriteLine("");
+            n_data = 0;
+            for (;;)
+            {
+                Cosine.cosh_values(ref n_data, ref x, ref fx);
+                if (n_data == 0)
+                {
+                    break;
+                }
+
+                Console.WriteLine("  "
+                                  + x.ToString("0.################").PadLeft(24) + "  "
+                                  + fx.ToString("0.################").PadLeft(24) + "");
+            }
+        }
+
+    }
+}

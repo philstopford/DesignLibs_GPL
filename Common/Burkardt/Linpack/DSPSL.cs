cdae00f737b8ddb0b966f88c6e32997ee8474bcf
philstopford <phil.stopford@gmail.com>
2021-07-18 11:30:04 -0500

    Linpack WIP

---
+++ Common/Burkardt/Linpack/DSPSL.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using Burkardt.BLAS;
+
+namespace Burkardt.Linpack
+{
+    public static class DSPSL
+    {
+        public static void dspsl(double[] ap, int n, int[] kpvt, double[] b)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    DSPSL solves the real symmetric system factored by DSPFA.
+            //
+            //  Discussion:
+            //
+            //    To compute inverse(A) * C where C is a matrix with P columns:
+            //
+            //      call dspfa ( ap, n, kpvt, info )
+            //
+            //      if ( info /= 0 ) go to ...
+            //
+            //      do j = 1, p
+            //        call dspsl ( ap, n, kpvt, c(1,j) )
+            //      end do
+            //
+            //    A division by zero may occur if DSPCO has set RCOND == 0.0D+00
+            //    or DSPFA has set INFO /= 0.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license. 
+            //
+            //  Modified:
+            //
+            //    25 May 2005
+            //
+            //  Author:
+            //
+            //    Original FORTRAN77 version by Jack Dongarra, Cleve Moler, Jim Bunch, 
+            //    Pete Stewart.
+            //    C++ version by John Burkardt.
+            //
+            //  Reference:
+            //
+            //    Jack Dongarra, Cleve Moler, Jim Bunch and Pete Stewart,
+            //    LINPACK User's Guide,
+            //    SIAM, (Society for Industrial and Applied Mathematics),
+            //    3600 University City Science Center,
+            //    Philadelphia, PA, 19104-2688.
+            //    ISBN 0-89871-172-X
+            //
+            //  Parameters:
+            //
+            //    Input, double AP[(N*(N+1))/2], the output from DSPFA.
+            //
+            //    Input, int N, the order of the matrix.
+            //
+            //    Input, int KPVT[N], the pivot vector from DSPFA.
+            //
+            //    Input/output, double B[N].  On input, the right hand side.
+            //    On output, the solution.
+            //
+        {
+            double ak;
+            double akm1;
+            double bk;
+            double bkm1;
+            double denom;
+            int ik;
+            int ikm1;
+            int ikp1;
+            int k;
+            int kk;
+            int km1k;
+            int km1km1;
+            int kp;
+            double temp;
+            //
+            //  Loop backward applying the transformations and D inverse to B.
+            //
+            k = n;
+            ik = (n * (n - 1)) / 2;
+
+            while (0 < k)
+            {
+                kk = ik + k;
+
+                if (0 <= kpvt[k - 1])
+                {
+                    //
+                    //  1 x 1 pivot block.
+                    //
+                    if (k != 1)
+                    {
+                        kp = kpvt[k - 1];
+                        //
+                        //  Interchange.
+                        //
+                        if (kp != k)
+                        {
+                            temp = b[k - 1];
+                            b[k - 1] = b[kp - 1];
+                            b[kp - 1] = temp;
+                        }
+
+                        //
+                        //  Apply the transformation.
+                        //
+                        BLAS1D.daxpy(k - 1, b[k - 1], ap, 1, ref b, 1, xIndex: +ik);
+                    }
+
+                    //
+                    //  Apply D inverse.
+                    //
+                    b[k - 1] = b[k - 1] / ap[kk - 1];
+                    k = k - 1;
+                    ik = ik - k;
+                }
+                else
+                {
+                    //
+                    //  2 x 2 pivot block.
+                    //
+                    ikm1 = ik - (k - 1);
+
+                    if (k != 2)
+                    {
+                        kp = Math.Abs(kpvt[k - 1]);
+                        //
+                        //  Interchange.
+                        //
+                        if (kp != k - 1)
+                        {
+                            temp = b[k - 2];
+                            b[k - 2] = b[kp - 1];
+                            b[kp - 1] = temp;
+                        }
+
+                        //
+                        //  Apply the transformation.
+                        //
+                        BLAS1D.daxpy(k - 2, b[k - 1], ap, 1, ref b, 1, xIndex: +ik);
+                        BLAS1D.daxpy(k - 2, b[k - 2], ap, 1, ref b, 1, xIndex: +ikm1);
+                    }
+
+                    //
+                    //  Apply D inverse.
+                    //
+                    km1k = ik + k - 1;
+                    kk = ik + k;
+                    ak = ap[kk - 1] / ap[km1k - 1];
+                    km1km1 = ikm1 + k - 1;
+                    akm1 = ap[km1km1 - 1] / ap[km1k - 1];
+                    bk = b[k - 1] / ap[km1k - 1];
+                    bkm1 = b[k - 2] / ap[km1k - 1];
+                    denom = ak * akm1 - 1.0;
+                    b[k - 1] = (akm1 * bk - bkm1) / denom;
+                    b[k - 2] = (ak * bkm1 - bk) / denom;
+                    k = k - 2;
+                    ik = ik - (k + 1) - k;
+                }
+            }
+
+            //
+            //  Loop forward applying the transformations.
+            //
+            k = 1;
+            ik = 0;
+
+            while (k <= n)
+            {
+                if (0 <= kpvt[k - 1])
+                {
+                    //
+                    //  1 x 1 pivot block.
+                    //
+                    if (k != 1)
+                    {
+                        //
+                        //  Apply the transformation.
+                        //
+                        b[k - 1] = b[k - 1] + BLAS1D.ddot(k - 1, ap, 1, b, 1, xIndex: +ik);
+                        kp = kpvt[k - 1];
+                        //
+                        //  Interchange.
+                        //
+                        if (kp != k)
+                        {
+                            temp = b[k - 1];
+                            b[k - 1] = b[kp - 1];
+                            b[kp - 1] = temp;
+                        }
+                    }
+
+                    ik = ik + k;
+                    k = k + 1;
+                }
+                else
+                {
+                    //
+                    //  2 x 2 pivot block.
+                    //
+                    if (k != 1)
+                    {
+                        //
+                        //  Apply the transformation.
+                        //
+                        b[k - 1] = b[k - 1] + BLAS1D.ddot(k - 1, ap, 1, b, 1, xIndex: +ik);
+                        ikp1 = ik + k;
+                        b[k] = b[k] + BLAS1D.ddot(k - 1, ap, 1, b, 1, xIndex: +ikp1);
+                        kp = Math.Abs(kpvt[k - 1]);
+                        //
+                        //  Interchange.
+                        //
+                        if (kp != k)
+                        {
+                            temp = b[k - 1];
+                            b[k - 1] = b[kp - 1];
+                            b[kp - 1] = temp;
+                        }
+                    }
+
+                    ik = ik + k + k + 1;
+                    k = k + 2;
+                }
+            }
+        }
+
+    }
+}
---
+++ Common/Burkardt/Linpack/DTRCO.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using Burkardt.BLAS;
+using Burkardt.Types;
+
+namespace Burkardt.Linpack
+{
+    public static class DTRCO
+    {
+        public static double dtrco(double[] t, int ldt, int n, ref double[] z, int job)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    DTRCO estimates the condition of a real triangular matrix.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license. 
+            //
+            //  Modified:
+            //
+            //    25 May 2005
+            //
+            //  Author:
+            //
+            //    Original FORTRAN77 version by Jack Dongarra, Cleve Moler, Jim Bunch, 
+            //    Pete Stewart.
+            //    C++ version by John Burkardt.
+            //
+            //  Reference:
+            //
+            //    Jack Dongarra, Cleve Moler, Jim Bunch and Pete Stewart,
+            //    LINPACK User's Guide,
+            //    SIAM, (Society for Industrial and Applied Mathematics),
+            //    3600 University City Science Center,
+            //    Philadelphia, PA, 19104-2688.
+            //    ISBN 0-89871-172-X
+            //
+            //  Parameters:
+            //
+            //    Input, double T[LDT*N], the triangular matrix.  The zero
+            //    elements of the matrix are not referenced, and the corresponding
+            //    elements of the array can be used to store other information.
+            //
+            //    Input, int LDT, the leading dimension of the array T.
+            //
+            //    Input, int N, the order of the matrix.
+            //
+            //    Output, double Z[N] a work vector whose contents are usually
+            //    unimportant.  If T is close to a singular matrix, then Z is an
+            //    approximate null vector in the sense that
+            //      norm(A*Z) = RCOND * norm(A) * norm(Z).
+            //
+            //    Input, int JOB, indicates the shape of T:
+            //    0, T is lower triangular.
+            //    nonzero, T is upper triangular.
+            //
+            //    Output, double DTRCO, an estimate of the reciprocal condition RCOND
+            //    of T.  For the system T*X = B, relative perturbations in T and B of size
+            //    EPSILON may cause relative perturbations in X of size EPSILON/RCOND.
+            //    If RCOND is so small that the logical expression
+            //      1.0D+00 + RCOND == 1.0D+00
+            //    is true, then T may be singular to working precision.  In particular,
+            //    RCOND is zero if exact singularity is detected or the estimate underflows.
+            //
+        {
+            double ek;
+            int i;
+            int i1;
+            int j;
+            int j1;
+            int j2;
+            int k;
+            int kk;
+            int l;
+            bool lower;
+            double rcond;
+            double s;
+            double sm;
+            double temp;
+            double tnorm;
+            double w;
+            double wk;
+            double wkm;
+            double ynorm;
+
+            lower = (job == 0);
+            //
+            //  Compute the 1-norm of T.
+            //
+            tnorm = 0.0;
+
+            for (j = 1; j <= n; j++)
+            {
+                if (lower)
+                {
+                    l = n + 1 - j;
+                    i1 = j;
+                }
+                else
+                {
+                    l = j;
+                    i1 = 1;
+                }
+
+                tnorm = Math.Max(tnorm, BLAS1D.dasum(l, t, 1, index: +i1 - 1 + (j - 1) * ldt));
+            }
+
+            //
+            //  RCOND = 1/(norm(T)*(estimate of norm(inverse(T)))).
+            //
+            //  Estimate = norm(Z)/norm(Y) where T * Z = Y and T' * Y = E.
+            //
+            //  T' is the transpose of T.
+            //
+            //  The components of E are chosen to cause maximum local
+            //  growth in the elements of Y.
+            //
+            //  The vectors are frequently rescaled to avoid overflow.
+            //
+            //  Solve T' * Y = E.
+            //
+            ek = 1.0;
+            for (i = 1; i <= n; i++)
+            {
+                z[i - 1] = 0.0;
+            }
+
+            for (kk = 1; kk <= n; kk++)
+            {
+                if (lower)
+                {
+                    k = n + 1 - kk;
+                }
+                else
+                {
+                    k = kk;
+                }
+
+                if (z[k - 1] != 0.0)
+                {
+                    ek = typeMethods.r8_sign(-z[k - 1]) * ek;
+                }
+
+                if (Math.Abs(t[k - 1 + (k - 1) * ldt]) < Math.Abs(ek - z[k - 1]))
+                {
+                    s = Math.Abs(t[k - 1 + (k - 1) * ldt]) / Math.Abs(ek - z[k - 1]);
+                    for (i = 1; i <= n; i++)
+                    {
+                        z[i - 1] = s * z[i - 1];
+                    }
+
+                    ek = s * ek;
+                }
+
+                wk = ek - z[k - 1];
+                wkm = -ek - z[k - 1];
+                s = Math.Abs(wk);
+                sm = Math.Abs(wkm);
+
+                if (t[k - 1 + (k - 1) * ldt] != 0.0)
+                {
+                    wk = wk / t[k - 1 + (k - 1) * ldt];
+                    wkm = wkm / t[k - 1 + (k - 1) * ldt];
+                }
+                else
+                {
+                    wk = 1.0;
+                    wkm = 1.0;
+                }
+
+                if (kk != n)
+                {
+                    if (lower)
+                    {
+                        j1 = 1;
+                        j2 = k - 1;
+                    }
+                    else
+                    {
+                        j1 = k + 1;
+                        j2 = n;
+                    }
+
+                    for (j = j1; j <= j2; j++)
+                    {
+                        sm = sm + Math.Abs(z[j - 1] + wkm * t[k - 1 + (j - 1) * ldt]);
+                        z[j - 1] = z[j - 1] + wk * t[k - 1 + (j - 1) * ldt];
+                        s = s + Math.Abs(z[j - 1]);
+                    }
+
+                    if (s < sm)
+                    {
+                        w = wkm - wk;
+                        wk = wkm;
+                        for (j = j1; j <= j2; j++)
+                        {
+                            z[j - 1] = z[j - 1] + w * t[k - 1 + (j - 1) * ldt];
+                        }
+                    }
+                }
+
+                z[k - 1] = wk;
+            }
+
+            temp = BLAS1D.dasum(n, z, 1);
+
+            for (i = 1; i <= n; i++)
+            {
+                z[i - 1] = z[i - 1] / temp;
+            }
+
+            ynorm = 1.0;
+            //
+            //  Solve T * Z = Y.
+            //
+            for (kk = 1; kk <= n; kk++)
+            {
+                if (lower)
+                {
+                    k = kk;
+                }
+                else
+                {
+                    k = n + 1 - kk;
+                }
+
+                if (Math.Abs(t[k - 1 + (k - 1) * ldt]) < Math.Abs(z[k - 1]))
+                {
+                    s = Math.Abs(t[k - 1 + (k - 1) * ldt]) / Math.Abs(z[k - 1]);
+                    for (i = 1; i <= n; i++)
+                    {
+                        z[i - 1] = s * z[i - 1];
+                    }
+
+                    ynorm = s * ynorm;
+                }
+
+                if (t[k - 1 + (k - 1) * ldt] != 0.0)
+                {
+                    z[k - 1] = z[k - 1] / t[k - 1 + (k - 1) * ldt];
+                }
+                else
+                {
+                    z[k - 1] = 1.0;
+                }
+
+                if (lower)
+                {
+                    i1 = k + 1;
+                }
+                else
+                {
+                    i1 = 1;
+                }
+
+                if (kk < n)
+                {
+                    w = -z[k - 1];
+                    BLAS1D.daxpy(n - kk, w, t, 1, ref z, 1, xIndex: +i1 - 1 + (k - 1) * ldt, yIndex: +i1 - 1);
+                }
+            }
+
+            //
+            //  Make ZNORM = 1.0.
+            //
+            s = 1.0 / BLAS1D.dasum(n, z, 1);
+            for (i = 1; i <= n; i++)
+            {
+                z[i - 1] = s * z[i - 1];
+            }
+
+            ynorm = s * ynorm;
+
+            if (tnorm != 0.0)
+            {
+                rcond = ynorm / tnorm;
+            }
+            else
+            {
+                rcond = 0.0;
+            }
+
+            return rcond;
+        }
+
+    }
+}

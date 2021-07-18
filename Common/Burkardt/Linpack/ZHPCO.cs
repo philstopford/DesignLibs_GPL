717e0e0b8860d6a131f5a79079cb6311d70a7fbc
philstopford <phil.stopford@gmail.com>
2021-07-18 14:28:24 -0500

    Linpack WIP

---
+++ Common/Burkardt/Linpack/ZHPCO.cs
@@ -0,0 +1,444 @@
+﻿using System;
+using System.Numerics;
+using Burkardt.BLAS;
+using Burkardt.Types;
+
+namespace Burkardt.Linpack
+{
+    public static class ZHPCO
+    {
+        public static double zhpco(ref Complex[] ap, int n, ref int[] ipvt)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    ZHPCO factors a complex hermitian packed matrix and estimates its condition.
+            //
+            //  Discussion:
+            //
+            //    If RCOND is not needed, ZHPFA is slightly faster.
+            //
+            //    To solve A*X = B, follow ZHPCO by ZHPSL.
+            //
+            //    To compute inverse(A)*C, follow ZHPCO by ZHPSL.
+            //
+            //    To compute inverse(A), follow ZHPCO by ZHPDI.
+            //
+            //    To compute determinant(A), follow ZHPCO by ZHPDI.
+            //
+            //    To compute inertia(A), follow ZHPCO by ZHPDI.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    21 May 2006
+            //
+            //  Author:
+            //
+            //    C++ version by John Burkardt
+            //
+            //  Reference:
+            //
+            //    Jack Dongarra, Cleve Moler, Jim Bunch and Pete Stewart,
+            //    LINPACK User's Guide,
+            //    SIAM, (Society for Industrial and Applied Mathematics),
+            //    3600 University City Science Center,
+            //    Philadelphia, PA, 19104-2688.
+            //
+            //  Parameters:
+            //
+            //    Input/output, Complex AP[N*(N+1)/2]; on input, the packed form of a
+            //    hermitian matrix A.  The columns of the upper triangle are stored
+            //    sequentially in a one-dimensional array of length N*(N+1)/2.  On
+            //    output, a block diagonal matrix and the multipliers which were used
+            //    to obtain it stored in packed form.  The factorization can be written
+            //    A = U*D*hermitian(U) where U is a product of permutation and unit
+            //    upper triangular matrices, hermitian(U) is the Complex.Conjugateugate transpose
+            //    of U, and D is block diagonal with 1 by 1 and 2 by 2 blocks.
+            //
+            //    Input, int N, the order of the matrix.
+            //
+            //    Output, int IPVT[N], the pivot indices.
+            //
+            //    Output, double ZHPCO, an estimate of RCOND, the reciprocal condition of
+            //    the matrix.  For the system A*X = B, relative perturbations in A and B
+            //    of size EPSILON may cause relative perturbations in X of size
+            //    (EPSILON/RCOND).  If RCOND is so small that the logical expression
+            //      1.0 + RCOND == 1.0
+            //    is true, then A may be singular to working precision.  In particular,
+            //    RCOND is zero if exact singularity is detected or the estimate underflows.
+            //
+            //  Local Parameters:
+            //
+            //    Workspace, Complex Z[N], a work vector whose contents are usually
+            //    unimportant.  If A is close to a singular matrix, then Z is an
+            //    approximate null vector in the sense that
+            //      norm(A*Z) = RCOND * norm(A) * norm(Z).
+            //
+        {
+            Complex ak;
+            Complex akm1;
+            double anorm;
+            Complex bk;
+            Complex bkm1;
+            Complex denom;
+            Complex ek;
+            int i;
+            int ij;
+            int ik;
+            int ikm1;
+            int ikp1;
+            int j;
+            int j1;
+            int k;
+            int kk;
+            int km1k;
+            int km1km1;
+            int kp;
+            int kps;
+            int ks;
+            double rcond;
+            double s;
+            Complex t;
+            double ynorm;
+            Complex[] z;
+
+            z = new Complex [n];
+            //
+            //  Find norm of A using only upper half.
+            //
+            j1 = 1;
+
+            for (j = 1; j <= n; j++)
+            {
+                z[j - 1] = new Complex(BLAS1Z.dzasum(j, ap, 1, index: +j1 - 1), 0.0);
+                ij = j1;
+                j1 = j1 + j;
+
+                for (i = 1; i <= j - 1; i++)
+                {
+                    z[i - 1] = new Complex((z[i - 1].Real) + typeMethods.zabs1(ap[ij - 1]), 0.0);
+                    ij = ij + 1;
+                }
+            }
+
+            anorm = 0.0;
+            for (j = 0; j < n; j++)
+            {
+                anorm = Math.Max(anorm, (z[j].Real));
+            }
+
+            //
+            //  Factor.
+            //
+            ZHPFA.zhpfa(ref ap, n, ref ipvt);
+            //
+            //  RCOND = 1/(norm(A) * (estimate of norm(inverse(A)))).
+            //
+            //  Estimate = norm(Z)/norm(Y) where A*Z = Y and A*Y = E.
+            //
+            //  The components of E are chosen to cause maximum local
+            //  growth in the elements of W where U*D*W = E.
+            //
+            //  The vectors are frequently rescaled to avoid overflow.
+            //
+            //  Solve U*D*W = E.
+            //
+            ek = new Complex(1.0, 0.0);
+            for (i = 0; i < n; i++)
+            {
+                z[i] = new Complex(0.0, 0.0);
+            }
+
+            k = n;
+            ik = (n * (n - 1)) / 2;
+
+            while (0 < k)
+            {
+                kk = ik + k;
+                ikm1 = ik - (k - 1);
+
+                if (ipvt[k - 1] < 0)
+                {
+                    ks = 2;
+                }
+                else
+                {
+                    ks = 1;
+                }
+
+                kp = Math.Abs(ipvt[k - 1]);
+                kps = k + 1 - ks;
+
+                if (kp != kps)
+                {
+                    t = z[kps - 1];
+                    z[kps - 1] = z[kp - 1];
+                    z[kp - 1] = t;
+                }
+
+                if (typeMethods.zabs1(z[k - 1]) != 0.0)
+                {
+                    ek = typeMethods.zsign1(ek, z[k - 1]);
+                }
+
+                z[k - 1] = z[k - 1] + ek;
+                BLAS1Z.zaxpy(k - ks, z[k - 1], ap, 1, ref z, 1, xIndex: +ik);
+
+                if (ks != 1)
+                {
+                    if (typeMethods.zabs1(z[k - 2]) != 0.0)
+                    {
+                        ek = typeMethods.zsign1(ek, z[k - 2]);
+                    }
+
+                    z[k - 2] = z[k - 2] + ek;
+                    BLAS1Z.zaxpy(k - ks, z[k - 2], ap, 1, ref z, 1, xIndex: +ikm1);
+                }
+
+                if (ks != 2)
+                {
+                    if (typeMethods.zabs1(ap[kk - 1]) < typeMethods.zabs1(z[k - 1]))
+                    {
+                        s = typeMethods.zabs1(ap[kk - 1]) / typeMethods.zabs1(z[k - 1]);
+                        BLAS1Z.zdscal(n, s, ref z, 1);
+                        ek = new Complex(s, 0.0) * ek;
+                    }
+
+                    if (typeMethods.zabs1(ap[kk - 1]) != 0.0)
+                    {
+                        z[k - 1] = z[k - 1] / ap[kk - 1];
+                    }
+                    else
+                    {
+                        z[k - 1] = new Complex(1.0, 0.0);
+                    }
+                }
+                else
+                {
+                    km1k = ik + k - 1;
+                    km1km1 = ikm1 + k - 1;
+                    ak = ap[kk - 1] / Complex.Conjugate(ap[km1k - 1]);
+                    akm1 = ap[km1km1 - 1] / ap[km1k - 1];
+                    bk = z[k - 1] / Complex.Conjugate(ap[km1k - 1]);
+                    bkm1 = z[k - 2] / ap[km1k - 1];
+                    denom = ak * akm1 - new Complex(1.0, 0.0);
+                    z[k - 1] = (akm1 * bk - bkm1) / denom;
+                    z[k - 2] = (ak * bkm1 - bk) / denom;
+                }
+
+                k = k - ks;
+                ik = ik - k;
+
+                if (ks == 2)
+                {
+                    ik = ik - (k + 1);
+                }
+            }
+
+            s = 1.0 / BLAS1Z.dzasum(n, z, 1);
+            BLAS1Z.zdscal(n, s, ref z, 1);
+            //
+            //  Solve hermitian(U) * Y = W.
+            //
+            k = 1;
+            ik = 0;
+
+            while (k <= n)
+            {
+                if (ipvt[k - 1] < 0)
+                {
+                    ks = 2;
+                }
+                else
+                {
+                    ks = 1;
+                }
+
+                if (k != 1)
+                {
+                    z[k - 1] = z[k - 1] + BLAS1Z.zdotc(k - 1, ap, 1, z, 1, xIndex: +ik);
+                    ikp1 = ik + k;
+
+                    if (ks == 2)
+                    {
+                        z[k] = z[k] + BLAS1Z.zdotc(k - 1, ap, 1, z, 1, xIndex: +ikp1);
+                    }
+
+                    kp = Math.Abs(ipvt[k - 1]);
+
+                    if (kp != k)
+                    {
+                        t = z[k - 1];
+                        z[k - 1] = z[kp - 1];
+                        z[kp - 1] = t;
+                    }
+                }
+
+                ik = ik + k;
+                if (ks == 2)
+                {
+                    ik = ik + (k + 1);
+                }
+
+                k = k + ks;
+            }
+
+            s = 1.0 / BLAS1Z.dzasum(n, z, 1);
+            BLAS1Z.zdscal(n, s, ref z, 1);
+            ynorm = 1.0;
+            //
+            //  Solve U*D*V = Y.
+            //
+            k = n;
+            ik = (n * (n - 1)) / 2;
+
+            while (0 < k)
+            {
+                kk = ik + k;
+                ikm1 = ik - (k - 1);
+
+                if (ipvt[k - 1] < 0)
+                {
+                    ks = 2;
+                }
+                else
+                {
+                    ks = 1;
+                }
+
+                if (k != ks)
+                {
+                    kp = Math.Abs(ipvt[k - 1]);
+                    kps = k + 1 - ks;
+
+                    if (kp != kps)
+                    {
+                        t = z[kps - 1];
+                        z[kps - 1] = z[kp - 1];
+                        z[kp - 1] = t;
+                    }
+
+                    BLAS1Z.zaxpy(k - ks, z[k - 1], ap, 1, ref z, 1, xIndex: +ik);
+
+                    if (ks == 2)
+                    {
+                        BLAS1Z.zaxpy(k - ks, z[k - 2], ap, 1, ref z, 1, xIndex: +ikm1);
+                    }
+                }
+
+                if (ks != 2)
+                {
+                    if (typeMethods.zabs1(ap[kk - 1]) < typeMethods.zabs1(z[k - 1]))
+                    {
+                        s = typeMethods.zabs1(ap[kk - 1]) / typeMethods.zabs1(z[k - 1]);
+                        BLAS1Z.zdscal(n, s, ref z, 1);
+                        ynorm = s * ynorm;
+                    }
+
+                    if (typeMethods.zabs1(ap[kk - 1]) != 0.0)
+                    {
+                        z[k - 1] = z[k - 1] / ap[kk - 1];
+                    }
+                    else
+                    {
+                        z[k - 1] = new Complex(1.0, 0.0);
+                    }
+                }
+                else
+                {
+                    km1k = ik + k - 1;
+                    km1km1 = ikm1 + k - 1;
+                    ak = ap[kk - 1] / Complex.Conjugate(ap[km1k - 1]);
+                    akm1 = ap[km1km1 - 1] / ap[km1k - 1];
+                    bk = z[k - 1] / Complex.Conjugate(ap[km1k - 1]);
+                    bkm1 = z[k - 2] / ap[km1k - 1];
+                    denom = ak * akm1 - new Complex(1.0, 0.0);
+                    z[k - 1] = (akm1 * bk - bkm1) / denom;
+                    z[k - 2] = (ak * bkm1 - bk) / denom;
+                }
+
+                k = k - ks;
+                ik = ik - k;
+
+                if (ks == 2)
+                {
+                    ik = ik - (k + 1);
+                }
+            }
+
+            s = 1.0 / BLAS1Z.dzasum(n, z, 1);
+            BLAS1Z.zdscal(n, s, ref z, 1);
+            ynorm = s * ynorm;
+            //
+            //  Solve hermitian(U) * Z = V.
+            //
+            k = 1;
+            ik = 0;
+
+            while (k <= n)
+            {
+                if (ipvt[k - 1] < 0)
+                {
+                    ks = 2;
+                }
+                else
+                {
+                    ks = 1;
+                }
+
+                if (k != 1)
+                {
+                    z[k - 1] = z[k - 1] + BLAS1Z.zdotc(k - 1, ap, 1, z, 1, xIndex: +ik);
+                    ikp1 = ik + k;
+
+                    if (ks == 2)
+                    {
+                        z[k] = z[k] + BLAS1Z.zdotc(k - 1, ap, 1, z, 1, xIndex: +ikp1);
+                    }
+
+                    kp = Math.Abs(ipvt[k - 1]);
+
+                    if (kp != k)
+                    {
+                        t = z[k - 1];
+                        z[k - 1] = z[kp - 1];
+                        z[kp - 1] = t;
+                    }
+                }
+
+                ik = ik + k;
+
+                if (ks == 2)
+                {
+                    ik = ik + (k + 1);
+                }
+
+                k = k + ks;
+            }
+
+            //
+            //  Make ZNORM = 1.
+            //
+            s = 1.0 / BLAS1Z.dzasum(n, z, 1);
+            BLAS1Z.zdscal(n, s, ref z, 1);
+            ynorm = s * ynorm;
+
+            if (anorm != 0.0)
+            {
+                rcond = ynorm / anorm;
+            }
+            else
+            {
+                rcond = 0.0;
+            }
+
+            return rcond;
+        }
+
+    }
+}
---
+++ Common/Burkardt/Linpack/ZHPDI.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Numerics;
+using Burkardt.BLAS;
+
+namespace Burkardt.Linpack
+{
+    public static class ZHPDI
+    {
+        public static void zhpdi(ref Complex[] ap, int n, int[] ipvt, ref double[] det,
+                ref int[] inert, int job)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    ZHPDI: determinant, inertia and inverse of a complex hermitian matrix.
+            //
+            //  Discussion:
+            //
+            //    The routine uses the factors from ZHPFA.
+            //
+            //    The matrix is stored in packed form.
+            //
+            //    A division by zero will occur if the inverse is requested and ZHPCO has
+            //    set RCOND == 0.0 or ZHPFA has set INFO != 0.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    21 May 2006
+            //
+            //  Author:
+            //
+            //    C++ version by John Burkardt
+            //
+            //  Reference:
+            //
+            //    Jack Dongarra, Cleve Moler, Jim Bunch and Pete Stewart,
+            //    LINPACK User's Guide,
+            //    SIAM, (Society for Industrial and Applied Mathematics),
+            //    3600 University City Science Center,
+            //    Philadelphia, PA, 19104-2688.
+            //
+            //  Parameters:
+            //
+            //    Input/output, Complex AP[N*(N+1)/2]; on input, the factored matrix
+            //    from ZHPFA.  If the inverse was requested, then on output, AP contains
+            //    the upper triangle of the inverse of the original matrix, stored in packed
+            //    form.  The columns of the upper triangle are stored sequentially in a
+            //    one-dimensional array.
+            //
+            //    Input, int N, the order of the matrix.
+            //
+            //    Input, int IPVT[N], the pivot vector from ZHPFA.
+            //
+            //    Output, double DET[2], if requested, the determinant of the original
+            //    matrix.  Determinant = DET(1) * 10.0**DET(2) with
+            //    1.0 <= abs ( DET(1) ) < 10.0 or DET(1) = 0.0.
+            //
+            //    Output, int INERT[3], if requested, the inertia of the original matrix.
+            //    INERT(1) = number of positive eigenvalues.
+            //    INERT(2) = number of negative eigenvalues.
+            //    INERT(3) = number of zero eigenvalues.
+            //
+            //    Input, int JOB, has the decimal expansion ABC where:
+            //    if C != 0, the inverse is computed,
+            //    if B != 0, the determinant is computed,
+            //    if A != 0, the inertia is computed.
+            //    For example, JOB = 111 gives all three.
+            //
+        {
+            double ak;
+            Complex akkp1;
+            double akp1;
+            double d;
+            int ij;
+            int ik;
+            int ikp1;
+            int iks;
+            int j;
+            int jb;
+            int jk;
+            int jkp1;
+            int k;
+            int kk;
+            int kkp1;
+            int km1;
+            int ks;
+            int ksj;
+            int kskp1;
+            int kstep;
+            bool nodet;
+            bool noert;
+            bool noinv;
+            double t;
+            Complex t2;
+            Complex[] work;
+
+            noinv = (job % 10) == 0;
+            nodet = (job % 100) / 10 == 0;
+            noert = (job % 1000) / 100 == 0;
+
+            if (!nodet || !noert)
+            {
+                if (!noert)
+                {
+                    inert[0] = 0;
+                    inert[1] = 0;
+                    inert[2] = 0;
+                }
+
+                if (!nodet)
+                {
+                    det[0] = 1.0;
+                    det[1] = 0.0;
+                }
+
+                t = 0.0;
+                ik = 0;
+
+                for (k = 1; k <= n; k++)
+                {
+                    kk = ik + k;
+                    d = (ap[kk - 1].Real);
+                    //
+                    //  Check if 1 by 1
+                    //
+                    if (ipvt[k - 1] <= 0)
+                    {
+                        //
+                        //  2 by 2 block
+                        //  Use DET (D  S; S  C)  =  ( D / T * C - T ) * T, T = abs ( S )
+                        //  to avoid underflow/overflow troubles.
+                        //  Take two passes through scaling.  Use T for flag.
+                        //
+                        if (t == 0.0)
+                        {
+                            ikp1 = ik + k;
+                            kkp1 = ikp1 + k;
+                            t = Complex.Abs(ap[kkp1 - 1]);
+                            d = (d / t) * (ap[kkp1].Real) - t;
+                        }
+                        else
+                        {
+                            d = t;
+                            t = 0.0;
+                        }
+                    }
+
+                    if (!noert)
+                    {
+                        if (0.0 < d)
+                        {
+                            inert[0] = inert[0] + 1;
+                        }
+                        else if (d < 0.0)
+                        {
+                            inert[1] = inert[1] + 1;
+                        }
+                        else if (d == 0.0)
+                        {
+                            inert[2] = inert[2] + 1;
+                        }
+                    }
+
+                    if (!nodet)
+                    {
+                        det[0] = det[0] * d;
+
+                        if (det[0] != 0.0)
+                        {
+                            while (Math.Abs(det[0]) < 1.0)
+                            {
+                                det[0] = det[0] * 10.0;
+                                det[1] = det[1] - 1.0;
+                            }
+
+                            while (10.0 <= Math.Abs(det[0]))
+                            {
+                                det[0] = det[0] / 10.0;
+                                det[1] = det[1] + 1.0;
+                            }
+                        }
+                    }
+
+                    ik = ik + k;
+                }
+            }
+
+            //
+            //  Compute inverse(A).
+            //
+            if (!noinv)
+            {
+                work = new Complex [n];
+
+                k = 1;
+                ik = 0;
+
+                while (k <= n)
+                {
+                    km1 = k - 1;
+                    kk = ik + k;
+                    ikp1 = ik + k;
+                    kkp1 = ikp1 + k;
+                    //
+                    //  1 by 1
+                    //
+                    if (0 <= ipvt[k - 1])
+                    {
+                        ap[kk - 1] = new Complex(1.0 / (ap[kk - 1].Real), 0.0);
+
+                        if (1 <= km1)
+                        {
+                            for (j = 1; j <= km1; j++)
+                            {
+                                work[j - 1] = ap[ik + j - 1];
+                            }
+
+                            ij = 0;
+                            for (j = 1; j <= km1; j++)
+                            {
+                                jk = ik + j;
+                                ap[jk - 1] = BLAS1Z.zdotc(j, ap, 1, work, 1, xIndex: +ij);
+                                BLAS1Z.zaxpy(j - 1, work[j - 1], ap, 1, ref ap, 1, xIndex: +ij, yIndex: +ik);
+                                ij = ij + j;
+                            }
+
+                            ap[kk - 1] = ap[kk - 1] + new Complex
+                                ((BLAS1Z.zdotc(km1, work, 1, ap, 1, yIndex: +ik).Real), 0.0);
+                        }
+
+                        kstep = 1;
+                    }
+                    //
+                    //  2 by 2
+                    //
+                    else
+                    {
+                        t = Complex.Abs(ap[kkp1 - 1]);
+                        ak = (ap[kk - 1].Real) / t;
+                        akp1 = (ap[kkp1].Real) / t;
+                        akkp1 = ap[kkp1 - 1] / t;
+                        d = t * (ak * akp1 - 1.0);
+                        ap[kk - 1] = new Complex(akp1 / d, 0.0);
+                        ap[kkp1] = new Complex(ak / d, 0.0);
+                        ap[kkp1 - 1] = -akkp1 / d;
+
+                        if (1 <= km1)
+                        {
+                            for (j = 1; j <= km1; j++)
+                            {
+                                work[j - 1] = ap[ikp1 + j - 1];
+                            }
+
+                            ij = 0;
+                            for (j = 1; j <= km1; j++)
+                            {
+                                jkp1 = ikp1 + j;
+                                ap[jkp1 - 1] = BLAS1Z.zdotc(j, ap, 1, work, 1, xIndex: +ij);
+                                BLAS1Z.zaxpy(j - 1, work[j - 1], ap, 1, ref ap, 1, xIndex: +ij, yIndex: +ikp1);
+                                ij = ij + j;
+                            }
+
+                            ap[kkp1] = ap[kkp1] + new Complex
+                                ((BLAS1Z.zdotc(km1, work, 1, ap, 1, xIndex: +ikp1).Real), 0.0);
+
+                            ap[kkp1 - 1] = ap[kkp1 - 1] + BLAS1Z.zdotc(km1, ap, 1, ap, 1, xIndex: +ik, yIndex: +ikp1);
+                            for (j = 1; j <= km1; j++)
+                            {
+                                work[j - 1] = ap[ik + j - 1];
+                            }
+
+                            ij = 0;
+
+                            for (j = 1; j <= km1; j++)
+                            {
+                                jk = ik + j;
+                                ap[jk - 1] = BLAS1Z.zdotc(j, ap, 1, work, 1, xIndex: +ij);
+                                BLAS1Z.zaxpy(j - 1, work[j - 1], ap, 1, ref ap, 1, xIndex: +ij, yIndex: +ik);
+                                ij = ij + j;
+                            }
+
+                            ap[kk - 1] = ap[kk - 1] + new Complex
+                                ((BLAS1Z.zdotc(km1, work, 1, ap, 1, yIndex: +ik).Real), 0.0);
+                        }
+
+                        kstep = 2;
+                    }
+
+                    //
+                    //  Swap
+                    //
+                    ks = Math.Abs(ipvt[k - 1]);
+
+                    if (ks != k)
+                    {
+                        iks = (ks * (ks - 1)) / 2;
+
+                        BLAS1Z.zswap(ks, ref ap, 1, ref ap, 1, xIndex: +iks, yIndex: +ik);
+                        ksj = ik + ks;
+
+                        for (jb = ks; jb <= k; jb++)
+                        {
+                            j = k + ks - jb;
+                            jk = ik + j;
+
+                            t2 = Complex.Conjugate(ap[jk - 1]);
+                            ap[jk - 1] = Complex.Conjugate(ap[ksj - 1]);
+                            ap[ksj - 1] = t2;
+
+                            ksj = ksj - (j - 1);
+                        }
+
+                        if (kstep != 1)
+                        {
+                            kskp1 = ikp1 + ks;
+
+                            t2 = ap[kskp1 - 1];
+                            ap[kskp1 - 1] = ap[kkp1 - 1];
+                            ap[kkp1 - 1] = t2;
+                        }
+                    }
+
+                    ik = ik + k;
+
+                    if (kstep == 2)
+                    {
+                        ik = ik + k + 1;
+                    }
+
+                    k = k + kstep;
+                }
+            }
+        }
+
+    }
+}

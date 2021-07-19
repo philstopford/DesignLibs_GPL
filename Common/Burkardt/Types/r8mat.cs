c3318f65460df9aaee834b377a6c1d44628543a9
philstopford <phil.stopford@gmail.com>
2021-07-19 14:13:51 -0500

    Linpack Bench

--- Common/Burkardt/Types/r8mat.cs
+++ Common/Burkardt/Types/r8mat.cs
@@ -2617,5 +2617,43 @@ namespace Burkardt.Types
             }
         }
         
+        public static double[] r8mat_gen ( int lda, int n )
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    R8MAT_GEN generates a random R8MAT..
+            //
+            //  Modified:
+            //
+            //    06 June 2005
+            //
+            //  Parameters:
+            //
+            //    Input, integer LDA, the leading dimension of the matrix.
+            //
+            //    Input, integer N, the order of the matrix.
+            //
+            //    Output, double R8MAT_GEN[LDA*N], the N by N matrix.
+            //
+        {
+            double[] a;
+            int i;
+            int[] init = { 1, 2, 3, 1325 };
+            int j;
+
+            a = new double[lda*n];
+
+            for ( j = 1; j <= n; j++ )
+            {
+                for ( i = 1; i <= n; i++ )
+                {
+                    a[i-1+(j-1)*lda] = r8_random ( init ) - 0.5;
+                }
+            }
+
+            return a;
+        }
     }
 }

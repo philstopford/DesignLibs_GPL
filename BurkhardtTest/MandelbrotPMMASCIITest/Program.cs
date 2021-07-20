dc2f5067c15886fda8081d974d4356bf7a6a2c5a
philstopford <phil.stopford@gmail.com>
2021-07-20 15:00:31 -0500

    PPM ASCII and Mandelbrot test

---
+++ BurkhardtTest/MandelbrotPMMASCIITest/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using Burkardt;
+
+namespace MandelbrotPMMASCIITest
+{
+    class Program
+    {
+        static void Main(string[] args)
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    MAIN is the main program for MANDELBROT.
+            //
+            //  Discussion:
+            //
+            //    MANDELBROT computes an image of the Mandelbrot set.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license. 
+            //
+            //  Modified:
+            //
+            //    22 July 2010
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Local Parameters:
+            //
+            //    Local, integer COUNT_MAX, the maximum number of iterations taken
+            //    for a particular pixel.
+            //
+        {
+            int[] b;
+            int c;
+            int c_max;
+            int[] count;
+            int count_max = 400;
+            string filename = "mandelbrot.ppm";
+            int[] g;
+            int i;
+            int j;
+            int k;
+            int n = 501;
+            int[] r;
+            double x;
+            double x_max = 1.25;
+            double x_min = -2.25;
+            double x1;
+            double x2;
+            double y;
+            double y_max = 1.75;
+            double y_min = -1.75;
+            double y1;
+            double y2;
+
+            Console.WriteLine("");
+            Console.WriteLine("MANDELBROT");
+            Console.WriteLine("");
+            Console.WriteLine("  Create an ASCII PPM image of the Mandelbrot set.");
+            Console.WriteLine("");
+            Console.WriteLine("  For each point C = X + i*Y");
+            Console.WriteLine("  with X range [" + x_min + "," + x_max + "]");
+            Console.WriteLine("  and  Y range [" + y_min + "," + y_max + "]");
+            Console.WriteLine("  carry out " + count_max + " iterations of the map");
+            Console.WriteLine("  Z(n+1) = Z(n)^2 + C.");
+            Console.WriteLine("  If the iterates stay bounded (norm less than 2)");
+            Console.WriteLine("  then C is taken to be a member of the set.");
+            Console.WriteLine("");
+            Console.WriteLine("  An ASCII PPM image of the set is created using");
+            Console.WriteLine("    N = " + n + " pixels in the X direction and");
+            Console.WriteLine("    N = " + n + " pixels in the Y direction.");
+            //
+            //  Carry out the iteration for each pixel, determining COUNT.
+            //
+            count = new int[n * n];
+
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    x = ((double) (j) * x_max
+                         + (double) (n - j - 1) * x_min)
+                        / (double) (n - 1);
+
+                    y = ((double) (i) * y_max
+                         + (double) (n - i - 1) * y_min)
+                        / (double) (n - 1);
+
+                    count[i + j * n] = 0;
+
+                    x1 = x;
+                    y1 = y;
+
+                    for (k = 1; k <= count_max; k++)
+                    {
+                        x2 = x1 * x1 - y1 * y1 + x;
+                        y2 = 2 * x1 * y1 + y;
+
+                        if (x2 < -2.0 || 2.0 < x2 || y2 < -2.0 || 2.0 < y2)
+                        {
+                            count[i + j * n] = k;
+                            break;
+                        }
+
+                        x1 = x2;
+                        y1 = y2;
+                    }
+                }
+            }
+
+            //
+            //  Determine the coloring of each pixel.
+            //
+            c_max = 0;
+            for (j = 0; j < n; j++)
+            {
+                for (i = 0; i < n; i++)
+                {
+                    if (c_max < count[i + j * n])
+                    {
+                        c_max = count[i + j * n];
+                    }
+                }
+            }
+
+            //
+            //  Set the image data.
+            //
+            r = new int[n * n];
+            g = new int[n * n];
+            b = new int[n * n];
+
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    if (count[i + j * n] % 2 == 1)
+                    {
+                        r[i + j * n] = 255;
+                        g[i + j * n] = 255;
+                        b[i + j * n] = 255;
+                    }
+                    else
+                    {
+                        c = (int) (255.0 * Math.Sqrt(Math.Sqrt(Math.Sqrt(
+                            ((double) (count[i + j * n]) / (double) (c_max))))));
+                        r[i + j * n] = 3 * c / 5;
+                        g[i + j * n] = 3 * c / 5;
+                        b[i + j * n] = c;
+                    }
+                }
+            }
+
+            //
+            //  Write an image file.
+            //
+            PPM_ASCII.ppma_write(filename, n, n, r, g, b);
+
+            Console.WriteLine("");
+            Console.WriteLine("  ASCII PPM image data stored in \"" + filename + "\".");
+
+            Console.WriteLine("");
+            Console.WriteLine("MANDELBROT");
+            Console.WriteLine("  Normal end of execution.");
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ Common/Burkardt/PPM_ASCII.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Burkardt
+{
+    public static class PPM_ASCII
+    {
+        public static bool ppma_write(string file_out_name, int xsize, int ysize, int[] r,
+                int[] g, int[] b)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    PPMA_WRITE writes the header and data for an ASCII portable pixel map file.
+            // 
+            //  Example:
+            //
+            //    P3
+            //    # feep.ppm
+            //    4 4
+            //    15
+            //     0  0  0    0  0  0    0  0  0   15  0 15
+            //     0  0  0    0 15  7    0  0  0    0  0  0
+            //     0  0  0    0  0  0    0 15  7    0  0  0
+            //    15  0 15    0  0  0    0  0  0    0  0  0
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license. 
+            //
+            //  Modified:
+            // 
+            //    28 February 2003
+            // 
+            //  Author:
+            // 
+            //    John Burkardt
+            //
+            //  Parameters:
+            //
+            //    Input, string FILE_OUT_NAME, the name of the file to contain the ASCII
+            //    portable pixel map data.
+            //
+            //    Input, int XSIZE, YSIZE, the number of rows and columns of data.
+            //
+            //    Input, int *R, *G, *B, the arrays of XSIZE by YSIZE data values.
+            //
+            //    Output, bool PPMA_WRITE, is
+            //    true, if an error was detected, or
+            //    false, if the file was written.
+            //
+        {
+            int[] b_index;
+            bool error;
+            List<string> file_out = new List<string>();
+            int[] g_index;
+            int i;
+            int j;
+            int[] r_index;
+            int rgb_max;
+
+            //
+            //  Compute the maximum.
+            //
+            rgb_max = 0;
+            r_index = r;
+            g_index = g;
+            b_index = b;
+
+            int index = 0;
+            for (j = 0; j < ysize; j++)
+            {
+                for (i = 0; i < xsize; i++)
+                {
+                    if (rgb_max < r_index[index])
+                    {
+                        rgb_max = r_index[index];
+                    }
+
+                    if (rgb_max < g_index[index])
+                    {
+                        rgb_max = g_index[index];
+                    }
+
+                    if (rgb_max < b_index[index])
+                    {
+                        rgb_max = b_index[index];
+                    }
+
+                    index++;
+                }
+            }
+
+            //
+            //  Write the header.
+            //
+            error = ppma_write_header(ref file_out, file_out_name, xsize, ysize, rgb_max);
+
+            if (error)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("PPMA_WRITE - Fatal error!");
+                Console.WriteLine("  PPMA_WRITE_HEADER failed.");
+                return true;
+            }
+
+            //
+            //  Write the data.
+            //
+            error = ppma_write_data(ref file_out, xsize, ysize, r, g, b);
+
+            if (error)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("PPMA_WRITE - Fatal error!");
+                Console.WriteLine("  PPMA_WRITE_DATA failed.");
+                return true;
+            }
+
+            try
+            {
+                File.WriteAllLines(file_out_name, file_out);
+            }
+            catch
+            {
+                Console.WriteLine("");
+                Console.WriteLine("PPMA_WRITE - Fatal error!");
+                Console.WriteLine("  Cannot open the output file \"" + file_out_name + "\".");
+                return true;
+            }
+
+
+            return false;
+        }
+
+        public static bool ppma_write_data(ref List<string> file_out, int xsize, int ysize, int[] r,
+                int[] g, int[] b)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    PPMA_WRITE_DATA writes the data for an ASCII portable pixel map file.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license. 
+            //
+            //  Modified:
+            //
+            //    28 February 2003
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Parameters:
+            //
+            //    Input, ofstream &FILE_OUT, a pointer to the file to contain the ASCII
+            //    portable pixel map data.
+            //
+            //    Input, int XSIZE, YSIZE, the number of rows and columns of data.
+            //
+            //    Input, int *R, *G, *B, the arrays of XSIZE by YSIZE data values.
+            //
+            //    Output, bool PPMA_WRITE_DATA, is
+            //    true, if an error was detected, or
+            //    false, if the data was written.
+            //
+        {
+            int[] b_index;
+            int[] g_index;
+            int i;
+            int j;
+            int[] r_index;
+            int rgb_num;
+
+            r_index = r;
+            g_index = g;
+            b_index = b;
+            rgb_num = 0;
+
+            int index = 0;
+            for (j = 0; j < ysize; j++)
+            {
+                string tmp = "";
+                for (i = 0; i < xsize; i++)
+                {
+                    tmp += r_index[index] + " " + g_index[index] + " " + b_index[index];
+                    rgb_num = rgb_num + 3;
+
+                    if (rgb_num % 12 == 0 || i == xsize - 1 || rgb_num == 3 * xsize * ysize)
+                    {
+                        file_out.Add(tmp);
+                        tmp = "";
+                    }
+                    else
+                    {
+                        tmp += " ";
+                    }
+                }
+
+                index++;
+            }
+
+            return false;
+        }
+
+        public static bool ppma_write_header(ref List<string> file_out, string file_out_name, int xsize,
+                int ysize, int rgb_max)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    PPMA_WRITE_HEADER writes the header of an ASCII portable pixel map file.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license. 
+            //
+            //  Modified:
+            //
+            //    28 February 2003
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Parameters:
+            //
+            //    Input, ofstream &FILE_OUT, a pointer to the file to contain the ASCII
+            //    portable pixel map data.
+            //
+            //    Input, string FILE_OUT_NAME, the name of the file.
+            //
+            //    Input, int XSIZE, YSIZE, the number of rows and columns of data.
+            //
+            //    Input, int RGB_MAX, the maximum RGB value.
+            //
+            //    Output, bool PPMA_WRITE_HEADER, is
+            //    true, if an error was detected, or
+            //    false, if the header was written.
+            //
+        {
+            file_out.Add("P3");
+            file_out.Add("# " + file_out_name + " created by PPMA_WRITE.C.");
+            file_out.Add(xsize + "  " + ysize + "");
+            file_out.Add(rgb_max + "");
+
+            return false;
+        }
+    }
+}

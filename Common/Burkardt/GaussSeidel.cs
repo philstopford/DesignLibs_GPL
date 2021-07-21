8062d4b27b4123ab5ddabd3dcd2b6817f7656c84
philstopford <phil.stopford@gmail.com>
2021-07-21 16:37:55 -0500

    Multigrid Poisson 1D

---
+++ Common/Burkardt/GaussSeidel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Burkardt
+{
+    public static class GaussSeidel
+    {
+        public static void gauss_seidel ( int n, double[] r, ref double[] u, ref double dif_l1, int rIndex = 0, int uIndex = 0 )
+
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    GAUSS_SEIDEL carries out one step of a Gauss-Seidel iteration.
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    07 December 2011
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Reference:
+        //
+        //    William Hager,
+        //    Applied Numerical Linear Algebra,
+        //    Prentice-Hall, 1988,
+        //    ISBN13: 978-0130412942,
+        //    LC: QA184.H33.
+        //
+        //  Parameters:
+        //
+        //    Input, int N, the number of unknowns.
+        //
+        //    Input, double R[N], the right hand side.
+        //
+        //    Input/output, double U[N], the estimated solution.
+        //
+        //    Output, double &DIF_L1, the L1 norm of the difference between the
+        //    input and output solution estimates.
+        //
+        {
+            int i;
+            double u_old;
+
+            dif_l1 = 0.0;
+
+            for ( i = 1; i < n - 1; i++ )
+            {
+                u_old = u[uIndex + i];
+                u[uIndex + i] = 0.5 * ( u[uIndex + i-1] + u[uIndex + i+1] + r[rIndex + i] );
+                dif_l1 = dif_l1 + Math.Abs ( u[uIndex + i] - u_old );
+            }
+        }
+    }
+}
---
+++ Common/Burkardt/Grid/Transfer.cs
@@ -0,0 +1,127 @@
+﻿namespace Burkardt.Grid
+{
+    public static class Transfer
+    {
+        public static void ctof(int nc, double[] uc, int nf, ref double[] uf, int ucIndex = 0, int ufIndex = 0)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    CTOF transfers data from a coarse to a finer grid.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    07 December 2011
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Reference:
+            //
+            //    William Hager,
+            //    Applied Numerical Linear Algebra,
+            //    Prentice-Hall, 1988,
+            //    ISBN13: 978-0130412942,
+            //    LC: QA184.H33.
+            //
+            //  Parameters:
+            //
+            //    Input, int NC, the number of coarse nodes.
+            //
+            //    Input, double UC[NC], the coarse correction data.
+            //
+            //    Input, int NF, the number of fine nodes.
+            //
+            //    Input/output, double UF[NF], on input, the fine grid data.
+            //    On output, the data has been updated with prolonged coarse 
+            //    correction data.
+            //
+        {
+            int ic;
+            int iff;
+
+            for (ic = 0; ic < nc; ic++)
+            {
+                iff = 2 * ic;
+                uf[ufIndex + iff] = uf[ufIndex + iff] + uc[ucIndex + ic];
+            }
+
+            for (ic = 0; ic < nc - 1; ic++)
+            {
+                iff = 2 * ic + 1;
+                uf[ufIndex + iff] = uf[ufIndex + iff] + 0.5 * (uc[ucIndex + ic] + uc[ucIndex + ic + 1]);
+            }
+
+        }
+
+
+        public static void ftoc(int nf, double[] uf, double[] rf, int nc, ref double[] uc,
+                ref double[] rc, int ufIndex = 0, int rfIndex = 0, int ucIndex = 0, int rcIndex = 0)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    FTOC transfers data from a fine grid to a coarser grid.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    07 December 2011
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Reference:
+            //
+            //    William Hager,
+            //    Applied Numerical Linear Algebra,
+            //    Prentice-Hall, 1988,
+            //    ISBN13: 978-0130412942,
+            //    LC: QA184.H33.
+            //
+            //  Parameters:
+            //
+            //    Input, int NF, the number of fine nodes.
+            //
+            //    Input, double UF[NF], the fine data.
+            //
+            //    Input, double RF[NF], the right hand side for the fine grid.
+            //
+            //    Input, int NC, the number of coarse nodes.
+            //
+            //    Output, double UC[NC], the coarse grid data, set to zero.
+            //
+            //    Output, double RC[NC], the right hand side for the coarse grid.
+            //
+        {
+            int ic;
+            int iff;
+
+            for (ic = 0; ic < nc; ic++)
+            {
+                uc[ucIndex + ic] = 0.0;
+            }
+
+            rc[0] = 0.0;
+            for (ic = 1; ic < nc - 1; ic++)
+            {
+                iff = 2 * ic;
+                rc[rcIndex + ic] = 4.0 * (rf[rfIndex + iff] + uf[ufIndex + iff - 1] - 2.0 * uf[ufIndex + iff] + uf[ufIndex + iff + 1]);
+            }
+
+            rc[rcIndex + nc - 1] = 0.0;
+
+        }
+    }
+}

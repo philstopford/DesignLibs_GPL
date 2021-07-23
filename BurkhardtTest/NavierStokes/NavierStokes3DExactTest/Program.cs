4938dfc75b1b16b78103c2ccafd6cd63ecc6b25b
philstopford <phil.stopford@gmail.com>
2021-07-23 10:59:07 -0500

    Navier Stokes 3D exact

---
+++ BurkhardtTest/NavierStokes/NavierStokes3DExactTest/Program.cs
@@ -0,0 +1,379 @@
+﻿using System;
+using Burkardt.NavierStokesNS;
+using Burkardt.Types;
+using Burkardt.Uniform;
+
+namespace NavierStokes3DExactTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    NS3DE_TEST tests the NS3DE library.
+            //
+            //  Location:
+            //
+            //    http://people.sc.fsu.edu/~jburkardt/cpp_src/navier_stokes_3d_exact/ns3de_test.cpp
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    30 July 2015
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            Console.WriteLine("");
+            Console.WriteLine("NS3DE_TEST");
+            Console.WriteLine("  Test the NS3DE library.");
+
+            uvwp_burgers_test();
+            resid_burgers_test();
+
+            uvwp_ethier_test();
+            resid_ethier_test();
+
+            Console.WriteLine("");
+            Console.WriteLine("NS3DE_TEST");
+            Console.WriteLine("  Normal end of execution.");
+            Console.WriteLine("");
+        }
+
+        static void uvwp_burgers_test()
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    UVWP_BURGERS_TEST tests UVWP_BURGERS.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    30 July 2015
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            int n;
+            double nu;
+            double[] p;
+            int seed;
+            double[] t;
+            double[] u;
+            double[] v;
+            double[] w;
+            double[] x;
+            double xyz_hi;
+            double xyz_lo;
+            double[] y;
+            double[] z;
+
+            nu = 0.25;
+
+            Console.WriteLine("");
+            Console.WriteLine("UVWP_BURGERS_TEST");
+            Console.WriteLine("  UVWP_BURGERS evaluates the Burgers solution.");
+            Console.WriteLine("  Estimate the range of velocity and pressure");
+            Console.WriteLine("  at the initial time T = 0, in a region that is the");
+            Console.WriteLine("  cube centered at (0,0,0) with 'radius' 1.0.");
+            Console.WriteLine("  Viscosity = " + nu + "");
+
+            n = 1000;
+
+            p = new double[n];
+            u = new double[n];
+            v = new double[n];
+            w = new double[n];
+
+            xyz_lo = -1.0;
+            xyz_hi = +1.0;
+            seed = 123456789;
+
+            x = UniformRNG.r8vec_uniform_ab_new(n, xyz_lo, xyz_hi, ref seed);
+            y = UniformRNG.r8vec_uniform_ab_new(n, xyz_lo, xyz_hi, ref seed);
+            z = UniformRNG.r8vec_uniform_ab_new(n, xyz_lo, xyz_hi, ref seed);
+            t = typeMethods.r8vec_zeros_new(n);
+
+            Burgers.uvwp_burgers(nu, n, x, y, z, t, ref u, ref v, ref w, ref p);
+
+            Console.WriteLine("");
+            Console.WriteLine("           Minimum       Maximum");
+            Console.WriteLine("");
+            Console.WriteLine("  U:  "
+                              + "  " + typeMethods.r8vec_amin(n, u).ToString().PadLeft(14)
+                              + "  " + typeMethods.r8vec_amax(n, u).ToString().PadLeft(14) + "");
+            Console.WriteLine("  V:  "
+                              + "  " + typeMethods.r8vec_amin(n, v).ToString().PadLeft(14)
+                              + "  " + typeMethods.r8vec_amax(n, v).ToString().PadLeft(14) + "");
+            Console.WriteLine("  W:  "
+                              + "  " + typeMethods.r8vec_amin(n, w).ToString().PadLeft(14)
+                              + "  " + typeMethods.r8vec_amax(n, w).ToString().PadLeft(14) + "");
+            Console.WriteLine("  P:  "
+                              + "  " + typeMethods.r8vec_amin(n, p).ToString().PadLeft(14)
+                              + "  " + typeMethods.r8vec_amax(n, p).ToString().PadLeft(14) + "");
+        }
+
+        static void resid_burgers_test()
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    RESID_BURGERS_TEST tests RESID_BURGERS.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    30 July 2015
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            int n;
+            double nu;
+            double[] pr;
+            int seed;
+            double[] t;
+            double[] ur;
+            double[] vr;
+            double[] wr;
+            double[] x;
+            double xyz_hi;
+            double xyz_lo;
+            double[] y;
+            double[] z;
+
+            nu = 0.25;
+
+            Console.WriteLine("");
+            Console.WriteLine("RESID_BURGERS_TEST");
+            Console.WriteLine("  RESID_BURGERS evaluates the Burgers residual.");
+            Console.WriteLine("  Sample the Navier-Stokes residuals");
+            Console.WriteLine("  at the initial time T = 0, using a region that is");
+            Console.WriteLine("  the cube centered at (0,0,0) with 'radius' 1.0,");
+            Console.WriteLine("  Viscosity = " + nu + "");
+
+            n = 1000;
+
+            pr = new double[n];
+            ur = new double[n];
+            vr = new double[n];
+            wr = new double[n];
+
+            xyz_lo = -1.0;
+            xyz_hi = +1.0;
+            seed = 123456789;
+
+            x = UniformRNG.r8vec_uniform_ab_new(n, xyz_lo, xyz_hi, ref seed);
+            y = UniformRNG.r8vec_uniform_ab_new(n, xyz_lo, xyz_hi, ref seed);
+            z = UniformRNG.r8vec_uniform_ab_new(n, xyz_lo, xyz_hi, ref seed);
+            t = typeMethods.r8vec_zeros_new(n);
+
+            Burgers.resid_burgers(nu, n, x, y, z, t, ref ur, ref vr, ref wr, ref pr);
+
+            Console.WriteLine("");
+            Console.WriteLine("           Minimum       Maximum");
+            Console.WriteLine("");
+            Console.WriteLine("  Ur:  "
+                              + "  " + typeMethods.r8vec_amin(n, ur).ToString().PadLeft(14)
+                              + "  " + typeMethods.r8vec_amax(n, ur).ToString().PadLeft(14) + "");
+            Console.WriteLine("  Vr:  "
+                              + "  " + typeMethods.r8vec_amin(n, vr).ToString().PadLeft(14)
+                              + "  " + typeMethods.r8vec_amax(n, vr).ToString().PadLeft(14) + "");
+            Console.WriteLine("  Wr:  "
+                              + "  " + typeMethods.r8vec_amin(n, wr).ToString().PadLeft(14)
+                              + "  " + typeMethods.r8vec_amax(n, wr).ToString().PadLeft(14) + "");
+            Console.WriteLine("  Pr:  "
+                              + "  " + typeMethods.r8vec_amin(n, pr).ToString().PadLeft(14)
+                              + "  " + typeMethods.r8vec_amax(n, pr).ToString().PadLeft(14) + "");
+        }
+
+        static void uvwp_ethier_test()
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    UVWP_ETHIER_TEST tests UVWP_ETHIER.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    29 July 2015
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            double a;
+            double d;
+            int n;
+            double[] p;
+            const double r8_pi = 3.141592653589793;
+            int seed;
+            double[] t;
+            double[] u;
+            double[] v;
+            double[] w;
+            double[] x;
+            double xyz_hi;
+            double xyz_lo;
+            double[] y;
+            double[] z;
+
+            a = r8_pi / 4.0;
+            d = r8_pi / 2.0;
+
+            Console.WriteLine("");
+            Console.WriteLine("UVWP_ETHIER_TEST");
+            Console.WriteLine("  UVWP_ETHIER evaluates the Ethier solution.");
+            Console.WriteLine("  Estimate the range of velocity and pressure");
+            Console.WriteLine("  at the initial time T = 0, in a region that is the");
+            Console.WriteLine("  cube centered at (0,0,0) with 'radius' 1.0.");
+            Console.WriteLine("  Parameter A = " + a + "");
+            Console.WriteLine("  Parameter D = " + d + "");
+
+            n = 1000;
+
+            p = new double[n];
+            u = new double[n];
+            v = new double[n];
+            w = new double[n];
+
+            xyz_lo = -1.0;
+            xyz_hi = +1.0;
+            seed = 123456789;
+
+            x = UniformRNG.r8vec_uniform_ab_new(n, xyz_lo, xyz_hi, ref seed);
+            y = UniformRNG.r8vec_uniform_ab_new(n, xyz_lo, xyz_hi, ref seed);
+            z = UniformRNG.r8vec_uniform_ab_new(n, xyz_lo, xyz_hi, ref seed);
+            t = typeMethods.r8vec_zeros_new(n);
+
+            Ethier.uvwp_ethier(a, d, n, x, y, z, t, ref u, ref v, ref w, ref p);
+
+            Console.WriteLine("");
+            Console.WriteLine("           Minimum       Maximum");
+            Console.WriteLine("");
+            Console.WriteLine("  U:  "
+                              + "  " + typeMethods.r8vec_amin(n, u).ToString().PadLeft(14)
+                              + "  " + typeMethods.r8vec_amax(n, u).ToString().PadLeft(14) + "");
+            Console.WriteLine("  V:  "
+                              + "  " + typeMethods.r8vec_amin(n, v).ToString().PadLeft(14)
+                              + "  " + typeMethods.r8vec_amax(n, v).ToString().PadLeft(14) + "");
+            Console.WriteLine("  W:  "
+                              + "  " + typeMethods.r8vec_amin(n, w).ToString().PadLeft(14)
+                              + "  " + typeMethods.r8vec_amax(n, w).ToString().PadLeft(14) + "");
+            Console.WriteLine("  P:  "
+                              + "  " + typeMethods.r8vec_amin(n, p).ToString().PadLeft(14)
+                              + "  " + typeMethods.r8vec_amax(n, p).ToString().PadLeft(14) + "");
+        }
+
+        static void resid_ethier_test()
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    RESID_ETHIER_TEST tests RESID_ETHIER.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    29 July 2015
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            double a;
+            double d;
+            int n;
+            double[] pr;
+            const double r8_pi = 3.141592653589793;
+            int seed;
+            double[] t;
+            double[] ur;
+            double[] vr;
+            double[] wr;
+            double[] x;
+            double xyz_hi;
+            double xyz_lo;
+            double[] y;
+            double[] z;
+
+            a = r8_pi / 4.0;
+            d = r8_pi / 2.0;
+
+            Console.WriteLine("");
+            Console.WriteLine("RESID_ETHIER_TEST");
+            Console.WriteLine("  RESID_ETHIER evaluates the Ethier residual.");
+            Console.WriteLine("  Sample the Navier-Stokes residuals");
+            Console.WriteLine("  at the initial time T = 0, using a region that is");
+            Console.WriteLine("  the cube centered at (0,0,0) with 'radius' 1.0,");
+            Console.WriteLine("  Parameter A = " + a + "");
+            Console.WriteLine("  Parameter D = " + d + "");
+
+            n = 1000;
+
+            pr = new double[n];
+            ur = new double[n];
+            vr = new double[n];
+            wr = new double[n];
+
+            xyz_lo = -1.0;
+            xyz_hi = +1.0;
+            seed = 123456789;
+
+            x = UniformRNG.r8vec_uniform_ab_new(n, xyz_lo, xyz_hi, ref seed);
+            y = UniformRNG.r8vec_uniform_ab_new(n, xyz_lo, xyz_hi, ref seed);
+            z = UniformRNG.r8vec_uniform_ab_new(n, xyz_lo, xyz_hi, ref seed);
+            t = typeMethods.r8vec_zeros_new(n);
+
+            Ethier.resid_ethier(a, d, n, x, y, z, t, ref ur, ref vr, ref wr, ref pr);
+
+            Console.WriteLine("");
+            Console.WriteLine("           Minimum       Maximum");
+            Console.WriteLine("");
+            Console.WriteLine("  Ur:  "
+                              + "  " + typeMethods.r8vec_amin(n, ur).ToString().PadLeft(14)
+                              + "  " + typeMethods.r8vec_amax(n, ur).ToString().PadLeft(14) + "");
+            Console.WriteLine("  Vr:  "
+                              + "  " + typeMethods.r8vec_amin(n, vr).ToString().PadLeft(14)
+                              + "  " + typeMethods.r8vec_amax(n, vr).ToString().PadLeft(14) + "");
+            Console.WriteLine("  Wr:  "
+                              + "  " + typeMethods.r8vec_amin(n, wr).ToString().PadLeft(14)
+                              + "  " + typeMethods.r8vec_amax(n, wr).ToString().PadLeft(14) + "");
+            Console.WriteLine("  Pr:  "
+                              + "  " + typeMethods.r8vec_amin(n, pr).ToString().PadLeft(14)
+                              + "  " + typeMethods.r8vec_amax(n, pr).ToString().PadLeft(14) + "");
+
+        }
+    }
+}
---
+++ Common/Burkardt/NavierStokes/Burgers.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using Burkardt.FullertonFnLib;
+using Burkardt.Types;
+
+namespace Burkardt.NavierStokesNS
+{
+    public static class Burgers
+    {
+        public static void uvwp_burgers ( double nu, int n, double[] x, double[] y, 
+        double[] z, double[] t, ref double[] u, ref double[] v, ref double[] w, ref double[] p )
+
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    UVWP_BURGERS evaluates the Burgers solution.
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    17 January 2015
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Reference:
+        //
+        //    Martin Bazant, Henry Moffatt,
+        //    Exact solutions of the Navier-Stokes equations having steady vortex 
+        //    structures,
+        //    Journal of Fluid Mechanics,
+        //    Volume 541, pages 55-64, 2005.
+        //
+        //    Johannes Burgers,
+        //    A mathematical model illustrating the theory of turbulence,
+        //    Advances in Applied Mechanics,
+        //    Volume 1, pages 171-199, 1948.
+        //
+        //  Parameters:
+        //
+        //    Input, double NU, the kinematic viscosity.
+        //
+        //    Input, int N, the number of points at which the solution 
+        //    is to be evaluated.
+        //
+        //    Input, double X[N], Y[N], Z[N], the coordinates of the points.
+        //
+        //    Input, double T[N], the time coordinates.
+        //
+        //    Output, double U[N], V[N], W[N], P[N], the solution values.
+        //
+        {
+            int i;
+
+            for ( i = 0; i < n; i++ )
+            {
+                //
+                //  Form the functions and derivatives.
+                //
+                u[i] =   2.0 * x[i];
+                v[i] =   - 2.0 * y[i];
+                w[i] =   FullertonLib.r8_erf ( y[i] / Math.Sqrt ( nu ) );
+                p[i] = - 2.0 * ( x[i] * x[i] + y[i] * y[i] );
+            }
+        }
+        
+        public static void resid_burgers(double nu, int n, double[] x, double[] y,
+        double[] z, double[] t, ref double[] ur, ref double[] vr, ref double[] wr, ref double[] pr )
+
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    RESID_BURGERS evaluates the Burgers residual.
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    17 January 2015
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Reference:
+        //
+        //    Martin Bazant, Henry Moffatt,
+        //    Exact solutions of the Navier-Stokes equations having steady vortex 
+        //    structures,
+        //    Journal of Fluid Mechanics,
+        //    Volume 541, pages 55-64, 2005.
+        //
+        //    Johannes Burgers,
+        //    A mathematical model illustrating the theory of turbulence,
+        //    Advances in Applied Mechanics,
+        //    Volume 1, pages 171-199, 1948.
+        //
+        //  Parameters:
+        //
+        //    Input, double NU, the kinematic viscosity.
+        //
+        //    Input, int N, the number of points at which the solution 
+        //    is to be evaluated.
+        //
+        //    Input, double X[N], Y[N], Z[N], the coordinates of the points.
+        //
+        //    Input, double T[N], the time coordinates.
+        //
+        //    Output, double UR[N], VR[N], WR[N], PR[N], the residuals.
+        //
+        {
+            int i;
+            double px;
+            double py;
+            double pz;
+            const double r8_pi = 3.141592653589793;
+            double u;
+            double ut;
+            double ux;
+            double uxx;
+            double uy;
+            double uyy;
+            double uz;
+            double uzz;
+            double v;
+            double vt;
+            double vx;
+            double vxx;
+            double vy;
+            double vyy;
+            double vz;
+            double vzz;
+            double w;
+            double wt;
+            double wx;
+            double wxx;
+            double wy;
+            double wyy;
+            double wz;
+            double wzz;
+
+            for (i = 0; i < n; i++)
+            {
+                //
+                //  Form the functions and derivatives.
+                //
+                u = 2.0 * x[i];
+                ux = 2.0;
+                uxx = 0.0;
+                uy = 0.0;
+                uyy = 0.0;
+                uz = 0.0;
+                uzz = 0.0;
+                ut = 0.0;
+
+                v = -2.0 * y[i];
+                vx = 0.0;
+                vxx = 0.0;
+                vy = -2.0;
+                vyy = 0.0;
+                vz = 0.0;
+                vzz = 0.0;
+                vt = 0.0;
+
+                w = FullertonLib.r8_erf(y[i] / Math.Sqrt(nu));
+                wx = 0.0;
+                wxx = 0.0;
+                wy = 2.0 * Math.Sqrt(1.0 / nu / r8_pi) * Math.Exp(-y[i] * y[i] / nu);
+                wyy = -4.0 * Math.Sqrt(1.0 / nu / r8_pi) * y[i] * Math.Exp(-y[i] * y[i] / nu) / nu;
+                wz = 0.0;
+                wzz = 0.0;
+                wt = 0.0;
+
+                //  p = - 2.0 * ( x[i] * x[i] + y[i] * y[i] );
+                px = -4.0 * x[i];
+                py = -4.0 * y[i];
+                pz = 0.0;
+                //
+                //  Evaluate the residuals.
+                //
+                ur[i] = ut
+                        + u * ux + v * uy + w * uz + px
+                        - nu * (uxx + uyy + uzz);
+
+                vr[i] = vt
+                        + u * vx + v * vy + w * vz + py
+                        - nu * (vxx + vyy + vzz);
+
+                wr[i] = wt
+                        + u * wx + v * wy + w * wz + pz
+                        - nu * (wxx + wyy + wzz);
+
+                pr[i] = ux + vy + wz;
+            }
+
+            return;
+        }
+    }
+}

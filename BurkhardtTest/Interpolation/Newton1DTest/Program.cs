c74977a36f44783a80971add155adde73ef54e75
philstopford <phil.stopford@gmail.com>
2021-07-23 11:25:38 -0500

    Newton 1D interpolation

---
+++ BurkhardtTest/Interpolation/Newton1DTest/Program.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Burkardt.Interpolation;
+using Burkardt.Types;
+using InterpTest;
+
+namespace Newton1DTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    NEWTON_INTERP_1D_TEST tests the NEWTON_INTERP_1D library.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    10 July 2015
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            int prob;
+            int prob_num;
+
+            Console.WriteLine("");
+            Console.WriteLine("NEWTON_INTERP_1D_TEST:");
+            Console.WriteLine("  Test the NEWTON_INTERP_1D library.");
+            Console.WriteLine("  The R8LIB library is needed.");
+            Console.WriteLine("  This test needs the TEST_INTERP library as well.");
+
+            newton_coef_1d_test();
+
+            newton_value_1d_test();
+
+            prob_num = Data_1D.p00_prob_num();
+
+            for (prob = 1; prob <= prob_num; prob++)
+            {
+                newton_interp_1d_test01(prob);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("NEWTON_INTERP_1D_TEST:");
+            Console.WriteLine("  Normal end of execution.");
+            Console.WriteLine("");
+        }
+
+        static void newton_coef_1d_test()
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    NEWTON_COEF_1D_TEST tests NEWTON_COEF_1D.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    10 July 2015
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            double[] cd;
+            int nd = 5;
+            double[] xd = {0.0, 1.0, 2.0, 3.0, 4.0};
+            double[] yd = {24.0, 0.0, 0.0, 0.0, 0.0};
+
+            Console.WriteLine("");
+            Console.WriteLine("NEWTON_COEF_1D_TEST");
+            Console.WriteLine("  NEWTON_COEF_1D sets the coefficients for a 1D Newton interpolation.");
+
+            typeMethods.r8vec2_print(nd, xd, yd, "  Interpolation data:");
+
+            cd = Newton1D.newton_coef_1d(nd, xd, yd);
+
+            typeMethods.r8vec_print(nd, cd, "  Newton interpolant coefficients:");
+        }
+
+        static void newton_value_1d_test()
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    NEWTON_VALUE_1D_TEST tests NEWTON_VALUE_1D.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    10 July 2015
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            double[] cd = {24.0, -24.0, +12.0, -4.0, 1.0};
+            int nd = 5;
+            int ni = 16;
+            double x_hi;
+            double x_lo;
+            double[] xd = {0.0, 1.0, 2.0, 3.0, 4.0};
+            double[] xi;
+            double[] yi;
+
+            Console.WriteLine("");
+            Console.WriteLine("NEWTON_VALUE_1D_TEST");
+            Console.WriteLine("  NEWTON_VALUE_1D evaluates a Newton 1d interpolant.");
+
+            typeMethods.r8vec2_print(nd, xd, cd, "  The Newton interpolant data:");
+
+            x_lo = 0.0;
+            x_hi = 5.0;
+            xi = typeMethods.r8vec_linspace_new(ni, x_lo, x_hi);
+            yi = Newton1D.newton_value_1d(nd, xd, cd, ni, xi);
+
+            typeMethods.r8vec2_print(ni, xi, yi, "  Newton interpolant values:");
+        }
+
+        static void newton_interp_1d_test01(int prob)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    NEWTON_INTERP_1D_TEST01 tests NEWTON_INTERP_1D.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    10 July 2015
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            double[] cd;
+            string command_filename;
+            List<string> command_unit = new List<string>();
+            string data_filename;
+            List<string> data_unit = new List<string>();
+            int i;
+            double interp_error;
+            string interp_filename;
+            List<string> interp_unit = new List<string>();
+            int j;
+            double ld;
+            double li;
+            int nd;
+            int ni;
+            string output_filename;
+            string title;
+            double[] xd;
+            double[] xi;
+            double xmax;
+            double xmin;
+            double[] xy;
+            double[] yd;
+            double[] yi;
+            double ymax;
+            double ymin;
+
+            Console.WriteLine("");
+            Console.WriteLine("NEWTON_INTERP_1D_TEST01:");
+            Console.WriteLine("  Interpolate data from TEST_INTERP problem #" + prob + "");
+
+            nd = TestInterp.p00_data_num(prob);
+            Console.WriteLine("  Number of data points = " + nd + "");
+
+            xy = TestInterp.p00_data(prob, 2, nd);
+
+            xd = new double[nd];
+            yd = new double[nd];
+
+            for (i = 0; i < nd; i++)
+            {
+                xd[i] = xy[0 + i * 2];
+                yd[i] = xy[1 + i * 2];
+            }
+
+            typeMethods.r8vec2_print(nd, xd, yd, "  X, Y data:");
+            //
+            //  Get the Newton coefficients.
+            //
+            cd = Newton1D.newton_coef_1d(nd, xd, yd);
+            //
+            //  #1:  Does interpolant match function at interpolation points?
+            //
+            ni = nd;
+            xi = typeMethods.r8vec_copy_new(ni, xd);
+            yi = Newton1D.newton_value_1d(nd, xd, cd, ni, xi);
+
+            interp_error = typeMethods.r8vec_norm_affine(ni, yi, yd) / (double) (ni);
+
+            Console.WriteLine("");
+            Console.WriteLine("  L2 interpolation error averaged per interpolant node = " + interp_error + "");
+
+            //
+            //  #2: Compare estimated curve length to piecewise linear (minimal) curve length.
+            //  Assume data is sorted, and normalize X and Y dimensions by (XMAX-XMIN) and
+            //  (YMAX-YMIN).
+            //
+            xmin = typeMethods.r8vec_min(nd, xd);
+            xmax = typeMethods.r8vec_max(nd, xd);
+            ymin = typeMethods.r8vec_min(nd, yd);
+            ymax = typeMethods.r8vec_max(nd, yd);
+
+            ni = 501;
+            xi = typeMethods.r8vec_linspace_new(ni, xmin, xmax);
+            yi = Newton1D.newton_value_1d(nd, xd, cd, ni, xi);
+
+            ld = 0.0;
+            for (i = 0; i < nd - 1; i++)
+            {
+                ld = ld + Math.Sqrt(Math.Pow((xd[i + 1] - xd[i]) / (xmax - xmin), 2)
+                                    + Math.Pow((yd[i + 1] - yd[i]) / (ymax - ymin), 2));
+            }
+
+            li = 0.0;
+            for (i = 0; i < ni - 1; i++)
+            {
+                li = li + Math.Sqrt(Math.Pow((xi[i + 1] - xi[i]) / (xmax - xmin), 2)
+                                    + Math.Pow((yi[i + 1] - yi[i]) / (ymax - ymin), 2));
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("  Normalized length of piecewise linear interpolant = " + ld + "");
+            Console.WriteLine("  Normalized length of Newton interpolant           = " + li + "");
+
+            //
+            //  Create data file.
+            //
+            data_filename = "data" + (prob) + ".txt";
+            for (j = 0; j < nd; j++)
+            {
+                data_unit.Add("  " + xd[j]
+                                   + "  " + yd[j] + "");
+            }
+
+            File.WriteAllLines(data_filename, data_unit);
+            Console.WriteLine("");
+            Console.WriteLine("  Created graphics data file \"" + data_filename + "\".");
+            //
+            //  Create interp file.
+            //
+            ni = 501;
+            xmin = typeMethods.r8vec_min(nd, xd);
+            xmax = typeMethods.r8vec_max(nd, xd);
+            xi = typeMethods.r8vec_linspace_new(ni, xmin, xmax);
+            yi = Newton1D.newton_value_1d(nd, xd, cd, ni, xi);
+
+            interp_filename = "interp" + (prob) + ".txt";
+            for (j = 0; j < ni; j++)
+            {
+                interp_unit.Add("  " + xi[j]
+                                     + "  " + yi[j] + "");
+            }
+
+            File.WriteAllLines(interp_filename, interp_unit);
+            Console.WriteLine("  Created graphics interp file \"" + interp_filename + "\".");
+            //
+            //  Plot the data and the interpolant.
+            //
+            command_filename = "commands" + (prob) + ".txt";
+
+            output_filename = "plot" + (prob) + ".png";
+
+            command_unit.Add("# " + command_filename + "");
+            command_unit.Add("#");
+            command_unit.Add("# Usage:");
+            command_unit.Add("#  gnuplot < " + command_filename + "");
+            command_unit.Add("#");
+            command_unit.Add("set term png");
+            command_unit.Add("set output '" + output_filename + "'");
+            command_unit.Add("set xlabel '<---X--->'");
+            command_unit.Add("set ylabel '<---Y--->'");
+            command_unit.Add("set title 'Data versus Newton polynomial interpolant'");
+            command_unit.Add("set grid");
+            command_unit.Add("set style data lines");
+            command_unit.Add("plot '" + data_filename
+                                      + "' using 1:2 with points pt 7 ps 2 lc rgb 'blue',\\");
+            command_unit.Add("     '" + interp_filename
+                                      + "' using 1:2 lw 3 linecolor rgb 'red'");
+
+            File.WriteAllLines(command_filename, command_unit);
+            Console.WriteLine("  Created graphics command file \"" + command_filename + "\".");
+        }
+    }
+}
---
+++ Common/Burkardt/Interpolation/Newton1D.cs
@@ -0,0 +1,154 @@
+﻿using System;
+
+namespace Burkardt.Interpolation
+{
+    public static class Newton1D
+    {
+        public static double[] newton_coef_1d(int nd, double[] xd, double[] yd)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    NEWTON_COEF_1D computes coefficients of a Newton 1D interpolant.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    08 July 2015
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Reference:
+            //
+            //    Carl deBoor,
+            //    A Practical Guide to Splines,
+            //    Springer, 2001,
+            //    ISBN: 0387953663,
+            //    LC: QA1.A647.v27.
+            //
+            //  Parameters:
+            //
+            //    Input, int ND, the number of data points.
+            //
+            //    Input, double XD[ND], the X values at which data was taken.
+            //    These values must be distinct.
+            //
+            //    Input, double YD[ND], the corresponding Y values.
+            //
+            //    Output, double NEWTON_COEF_1D[ND], the divided difference coefficients.
+            //
+        {
+            double[] cd;
+            int i;
+            int j;
+            //
+            //  Copy the data values.
+            //
+            cd = new double[nd];
+
+            for (i = 0; i < nd; i++)
+            {
+                cd[i] = yd[i];
+            }
+
+            //
+            //  Make sure the abscissas are distinct.
+            //
+            for (i = 0; i < nd; i++)
+            {
+                for (j = i + 1; j < nd; j++)
+                {
+                    if (xd[i] - xd[j] == 0.0)
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("NEWTON_COEF_1D - Fatal error!");
+                        Console.WriteLine("  Two entries of XD are equal!");
+                        Console.WriteLine("  XD[" + i + "] = " + xd[i] + "");
+                        Console.WriteLine("  XD[" + j + "] = " + xd[j] + "");
+                        return null;
+                    }
+                }
+            }
+
+            //
+            //  Compute the divided differences.
+            //
+            for (i = 1; i <= nd - 1; i++)
+            {
+                for (j = nd - 1; i <= j; j--)
+                {
+                    cd[j] = (cd[j] - cd[j - 1]) / (xd[j] - xd[j - i]);
+                }
+            }
+
+            return cd;
+        }
+
+        public static double[] newton_value_1d(int nd, double[] xd, double[] cd, int ni, double[] xi)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    NEWTON_VALUE_1D evaluates a Newton 1D interpolant.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    08 July 2015
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Reference:
+            //
+            //    Carl deBoor,
+            //    A Practical Guide to Splines,
+            //    Springer, 2001,
+            //    ISBN: 0387953663,
+            //    LC: QA1.A647.v27.
+            //
+            //  Parameters:
+            //
+            //    Input, int ND, the order of the difference table.
+            //
+            //    Input, double XD[ND], the X values of the difference table.
+            //
+            //    Input, double CD[ND], the divided differences.
+            //
+            //    Input, int NI, the number of interpolation points.
+            //
+            //    Input, double XI[NI], the interpolation points.
+            //
+            //    Output, double NEWTON_VALUE_1D[NI], the interpolation values.
+            //
+        {
+            int i;
+            int j;
+            double[] yi;
+
+            yi = new double[ni];
+
+            for (j = 0; j < ni; j++)
+            {
+                yi[j] = cd[nd - 1];
+                for (i = 2; i <= nd; i++)
+                {
+                    yi[j] = cd[nd - i] + (xi[j] - xd[nd - i]) * yi[j];
+                }
+            }
+
+            return yi;
+        }
+    }
+}

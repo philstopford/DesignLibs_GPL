f1414177e274f87c5c7c9a2cbbef4da2d55ec637
philstopford <phil.stopford@gmail.com>
2021-07-28 15:56:08 -0500

    Triangle Properties

--- Common/Burkardt/Line/Line.cs
+++ Common/Burkardt/Line/Line.cs
@@ -5,6 +5,162 @@ namespace Burkardt.LineNS
 {
     public class Line
     {
+        public static void lines_imp_int ( double a1, double b1, double c1, double a2, double b2, 
+        double c2, ref int ival, ref double[] p )
+
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    LINES_IMP_INT determines where two implicit lines intersect in 2D.
+        //
+        //  Discussion:
+        //
+        //    The implicit form of a line in 2D is:
+        //
+        //      A * X + B * Y + C = 0
+        //
+        //    22 May 2004: Thanks to John Asmuth for pointing out that the 
+        //    B array was not being deallocated on exit.
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license. 
+        //
+        //  Modified:
+        //
+        //    30 June 2005
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, double A1, B1, C1, define the first line.
+        //    At least one of A1 and B1 must be nonzero.
+        //
+        //    Input, double A2, B2, C2, define the second line.
+        //    At least one of A2 and B2 must be nonzero.
+        //
+        //    Output, int &IVAL, reports on the intersection.
+        //    -1, both A1 and B1 were zero.
+        //    -2, both A2 and B2 were zero.
+        //     0, no intersection, the lines are parallel.
+        //     1, one intersection point, returned in P.
+        //     2, infinitely many intersections, the lines are identical.
+        //
+        //    Output, double P[2], if IVAL = 1, then P contains
+        //    the intersection point.  Otherwise, P = 0.
+        //
+        {
+            double[] a = new double[2 * 2];
+            double[] b;
+
+            p[0] = 0.0;
+            p[1] = 0.0;
+            //
+            //  Refuse to handle degenerate lines.
+            //
+            if (a1 == 0.0 && b1 == 0.0)
+            {
+                ival = -1;
+                return;
+            }
+            else if (a2 == 0.0 && b2 == 0.0)
+            {
+                ival = -2;
+                return;
+            }
+
+            //
+            //  Set up a linear system, and compute its inverse.
+            //
+            a[0 + 0 * 2] = a1;
+            a[0 + 1 * 2] = b1;
+            a[1 + 0 * 2] = a2;
+            a[1 + 1 * 2] = b2;
+
+            b = typeMethods.r8mat_inverse_2d(a);
+            //
+            //  If the inverse exists, then the lines intersect.
+            //  Multiply the inverse times -C to get the intersection point.
+            //
+            if (b != null)
+            {
+
+                ival = 1;
+                p[0] = -b[0 + 0 * 2] * c1 - b[0 + 1 * 2] * c2;
+                p[1] = -b[1 + 0 * 2] * c1 - b[1 + 1 * 2] * c2;
+            }
+            //
+            //  If the inverse does not exist, then the lines are parallel
+            //  or coincident.  Check for parallelism by seeing if the
+            //  C entries are in the same ratio as the A or B entries.
+            //
+            else
+            {
+
+                ival = 0;
+
+                if (a1 == 0.0)
+                {
+                    if (b2 * c1 == c2 * b1)
+                    {
+                        ival = 2;
+                    }
+                }
+                else
+                {
+                    if (a2 * c1 == c2 * a1)
+                    {
+                        ival = 2;
+                    }
+                }
+            }
+        }
+
+        public static bool line_imp_is_degenerate ( double a, double b, double c )
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    LINE_IMP_IS_DEGENERATE finds if an implicit point is degenerate in 2D.
+            //
+            //  Discussion:
+            //
+            //    The implicit form of a line in 2D is:
+            //
+            //      A * X + B * Y + C = 0
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license. 
+            //
+            //  Modified:
+            //
+            //    10 July 2005
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Parameters:
+            //
+            //    Input, double A, B, C, the implicit line parameters.
+            //
+            //    Output, bool LINE_IMP_IS_DEGENERATE, is true if the
+            //    line is degenerate.
+            //
+        {
+            bool value;
+
+            value = ( a * a + b * b == 0.0 );
+
+            return value;
+        }
+        
         public static bool line_exp_is_degenerate_nd(int dim_num, double[] p1, double[] p2)
 
             //****************************************************************************80
@@ -123,6 +279,63 @@ namespace Burkardt.LineNS
             return p4;
         }
 
+        public static void line_exp2imp ( double[] p1, double[] p2, ref double a, ref double b, 
+        ref double c )
+
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    LINE_EXP2IMP_2D converts an explicit line to implicit form in 2D.
+        //
+        //  Discussion:
+        //
+        //    The explicit form of a line in 2D is:
+        //
+        //      the line through P1 and P2
+        //
+        //    The implicit form of a line in 2D is:
+        //
+        //      A * X + B * Y + C = 0
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license. 
+        //
+        //  Modified:
+        //
+        //    30 June 2005
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, double P1[2], P2[2], two distinct points on the line. 
+        //
+        //    Output, double &A, &B, &C, three coefficients which describe
+        //    the line that passes through P1 and P2.
+        //
+        {
+            //
+            //  Take care of degenerate cases.
+            //
+            if ( typeMethods.r8vec_eq ( 2, p1, p2 ) )
+            {
+                Console.WriteLine("");
+                Console.WriteLine("LINE_EXP2IMP - Fatal error!");
+                Console.WriteLine("  P1 = P2");
+                Console.WriteLine("  P1 = " + p1[0] + " " + p1[1] + "");
+                Console.WriteLine("  P2 = " + p2[0] + " " + p2[1] + "");
+                return;
+            }
+
+            a = p2[1] - p1[1];
+            b = p1[0] - p2[0];
+            c = p2[0] * p1[1] - p1[0] * p2[1];
+
+        }
         public static void line_exp2imp_2d(double[] p1, double[] p2, ref double a, ref double b,
                 ref double c, int p1Index = 0, int p2Index = 0)
 
@@ -219,6 +432,128 @@ namespace Burkardt.LineNS
             value = (a * a + b * b == 0.0);
 
             return value;
+        }
+
+        public static void lines_exp_int(double[] p1, double[] p2, double[] p3, double[] p4,
+                ref int ival, ref double[] p)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    LINES_EXP_INT determines where two explicit lines intersect in 2D.
+            //
+            //  Discussion:
+            //
+            //    The explicit form of a line in 2D is:
+            //
+            //      the line through P1 and P2.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license. 
+            //
+            //  Modified:
+            //
+            //    30 June 2005
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Parameters:
+            //
+            //    Input, double P1[2], P2[2], define the first line.
+            //
+            //    Input, double P3[2], P4[2], define the second line.
+            //
+            //    Output, int &IVAL, reports on the intersection:
+            //    0, no intersection, the lines may be parallel or degenerate.
+            //    1, one intersection point, returned in P.
+            //    2, infinitely many intersections, the lines are identical.
+            //
+            //    Output, double P[2], if IVAl = 1, then P contains
+            //    the intersection point.  Otherwise, P = 0.
+            //
+        {
+            double a1 = 0.0;
+            double a2 = 0.0;
+            double b1 = 0.0;
+            double b2 = 0.0;
+            double c1 = 0.0;
+            double c2 = 0.0;
+            bool point_1 = false;
+            bool point_2 = false;
+
+            ival = 0;
+            p[0] = 0.0;
+            p[1] = 0.0;
+            //
+            //  Check whether either line is a point.
+            //
+            if (typeMethods.r8vec_eq(2, p1, p2))
+            {
+                point_1 = true;
+            }
+            else
+            {
+                point_1 = false;
+            }
+
+            if (typeMethods.r8vec_eq(2, p3, p4))
+            {
+                point_2 = true;
+            }
+            else
+            {
+                point_2 = false;
+            }
+
+            //
+            //  Convert the lines to ABC format.
+            //
+            if (!point_1)
+            {
+                line_exp2imp(p1, p2, ref a1, ref b1, ref c1);
+            }
+
+            if (!point_2)
+            {
+                line_exp2imp(p3, p4, ref a2, ref b2, ref c2);
+            }
+
+            //
+            //  Search for intersection of the lines.
+            //
+            if (point_1 && point_2)
+            {
+                if (typeMethods.r8vec_eq(2, p1, p3))
+                {
+                    ival = 1;
+                    typeMethods.r8vec_copy(2, p1, ref p);
+                }
+            }
+            else if (point_1)
+            {
+                if (a2 * p1[0] + b2 * p1[1] == c2)
+                {
+                    ival = 1;
+                    typeMethods.r8vec_copy(2, p1, ref p);
+                }
+            }
+            else if (point_2)
+            {
+                if (a1 * p3[0] + b1 * p3[1] == c1)
+                {
+                    ival = 1;
+                    typeMethods.r8vec_copy(2, p3, ref p);
+                }
+            }
+            else
+            {
+                lines_imp_int(a1, b1, c1, a2, b2, c2, ref ival, ref p);
+            }
+
         }
 
         public static void lines_exp_int_2d(double[] p1, double[] p2, double[] p3, double[] p4,
@@ -455,6 +790,186 @@ namespace Burkardt.LineNS
                     }
                 }
             }
+        }
+        
+        public static double segment_point_dist ( double[] p1, double[] p2, double[] p, int p1Index = 0, int p2Index = 0, int pIndex = 0 )
+
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    SEGMENT_POINT_DIST: distance ( line segment, point ) in 2D.
+        //
+        //  Discussion:
+        //
+        //    A line segment is the finite portion of a line that lies between
+        //    two points.
+        //
+        //    The nearest point will satisfy the condition
+        //
+        //      PN = (1-T) * P1 + T * P2.
+        //
+        //    T will always be between 0 and 1.
+        //
+        //    Thanks to Kirill Speransky for pointing out that a previous version
+        //    of this routine was incorrect, 02 May 2006.
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    03 May 2006
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, double P1[2], P2[2], the endpoints of the line segment.
+        //
+        //    Input, double P[2], the point whose nearest neighbor on the line
+        //    segment is to be determined.
+        //
+        //    Output, double SEGMENT_POINT_DIST, the distance from the point
+        //    to the line segment.
+        //
+        {
+            double bot;
+            double dist;
+            int i;
+            double t;
+            double[] pn = new double[2];
+            //
+            //  If the line segment is actually a point, then the answer is easy.
+            //
+            if ( typeMethods.r8vec_eq ( 2, p1, p2, startIndexA1:p1Index, startIndexA2:p2Index ) )
+            {
+                t = 0.0;
+            }
+            else
+            {
+                bot = 0.0;
+                for ( i = 0; i < 2; i++ )
+                {
+                    bot = bot + Math.Pow ( p2[p2Index + i] - p1[p1Index + i], 2 );
+                }
+
+                t = 0.0;
+                for ( i = 0; i < 2; i++ )
+                {
+                    t = t + ( p[pIndex + i] - p1[p1Index + i] ) * ( p2[p2Index + i] - p1[p1Index + i] );
+                }
+
+                t = t / bot;
+                t = Math.Max ( t, 0.0 );
+                t = Math.Min ( t, 1.0 );
+            }
+
+            for ( i = 0; i < 2; i++ )
+            {
+                pn[i] = p1[p1Index + i] + t * ( p2[p2Index + i] - p1[p1Index + i] );
+            }
+
+            dist = 0.0;
+            for ( i = 0; i < 2; i++ )
+            {
+                dist = dist + Math.Pow ( p[pIndex + i] - pn[i], 2 );
+            }
+            dist = Math.Sqrt ( dist );
+
+            return dist;
+        }
+        
+        public static void segment_point_near ( double[] p1, double[] p2, double[] p,
+        ref double[] pn, ref double dist, ref double t, int p1Index = 0, int p2Index = 0, int pIndex = 0, int pnIndex = 0 )
+
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    SEGMENT_POINT_NEAR finds the point on a line segment nearest a point in 2D.
+        //
+        //  Discussion:
+        //
+        //    A line segment is the finite portion of a line that lies between
+        //    two points.
+        //
+        //    The nearest point will satisfy the condition:
+        //
+        //      PN = (1-T) * P1 + T * P2.
+        //
+        //    and T will always be between 0 and 1.
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    04 July 2005
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, double P1[2], P2[2], the two endpoints of the line segment.
+        //
+        //    Input, double P[2], the point whose nearest neighbor
+        //    on the line segment is to be determined.
+        //
+        //    Output, double PN[2], the point on the line segment which is nearest P.
+        //
+        //    Output, double &DIST, the distance from the point to the nearest point
+        //    on the line segment.
+        //
+        //    Output, double &T, the relative position of the point Pn to the
+        //    points P1 and P2.
+        //
+        {
+            double bot;
+            int i;
+            //
+            //  If the line segment is actually a point, then the answer is easy.
+            //
+            if ( typeMethods.r8vec_eq ( 2, p1, p2, startIndexA1:p1Index, startIndexA2:p2Index ) )
+            {
+                t = 0.0;
+            }
+            else
+            {
+                bot = 0.0;
+                for ( i = 0; i < 2; i++ )
+                {
+                    bot = bot + Math.Pow ( p2[p2Index + i] - p1[p1Index + i], 2 );
+                }
+
+                t = 0.0;
+                for ( i = 0; i < 2; i++ )
+                {
+                    t = t + ( p[pIndex + i] - p1[p1Index + i] ) * ( p2[p2Index + i] - p1[p1Index + i] );
+                }
+
+                t = t / bot;
+                t = Math.Max ( t, 0.0 );
+                t = Math.Min ( t, 1.0 );
+            }
+
+            for ( i = 0; i < 2; i++ )
+            {
+                pn[pnIndex + i] = p1[p1Index + i] + t * ( p2[p2Index + i] - p1[p1Index + i] );
+            }
+
+            dist = 0.0;
+            for ( i = 0; i < 2; i++ )
+            {
+                dist = dist + Math.Pow ( p[pIndex + i] - pn[pnIndex + i], 2 );
+            }
+            dist = Math.Sqrt ( dist );
         }
     }
 }
--- Common/Burkardt/Types/triangle.cs
+++ Common/Burkardt/Types/triangle.cs
@@ -635,6 +635,101 @@ namespace Burkardt.Types
 
             return true;
         }
+        
+        public static double triangle_diameter_2d ( double[] t )
+
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    TRIANGLE_DIAMETER computes the diameter of a triangle in 2D.
+        //
+        //  Discussion:
+        //
+        //    The diameter of a triangle is the diameter of the smallest circle
+        //    that can be drawn around the triangle.  At least two of the vertices
+        //    of the triangle will intersect the circle, but not necessarily
+        //    all three!
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license. 
+        //
+        //  Modified:
+        //
+        //    09 November 2015
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, double T[2*3], the triangle vertices.
+        //
+        //    Output, double TRIANGLE_DIAMETER, the diameter of the triangle.
+        //
+        {
+            double a;
+            double b;
+            double c;
+            double diam;
+            double s;
+            //
+            //  Compute the (squares of) the lengths of the sides.
+            //
+            a = Math.Sqrt ( Math.Pow ( t[0+1*2] - t[0+0*2], 2 ) + Math.Pow ( t[1+1*2] - t[1+0*2], 2 ) );
+            b = Math.Sqrt ( Math.Pow ( t[0+2*2] - t[0+1*2], 2 ) + Math.Pow ( t[1+2*2] - t[1+1*2], 2 ) );
+            c = Math.Sqrt ( Math.Pow ( t[0+0*2] - t[0+2*2], 2 ) + Math.Pow ( t[1+0*2] - t[1+2*2], 2 ) );
+            //
+            //  Take care of a zero side.
+            //
+            if ( a == 0.0 )
+            {
+                return Math.Sqrt ( b );
+            }
+            else if ( b == 0.0 )
+            {
+                return Math.Sqrt ( c );
+            }
+            else if ( c == 0.0 )
+            {
+                return Math.Sqrt ( a );
+            }
+            //
+            //  Make A the largest.
+            //
+            if ( a < b )
+            {
+                s = a;
+                a = b;
+                b = s;
+            }
+
+            if ( a < c )
+            {
+                s = a;
+                a = c;
+                c = s;
+            }
+            //
+            //  If A is very large...
+            //
+            if ( b + c < a )
+            {
+                diam = Math.Sqrt ( a );
+            }
+            else
+            {
+                a = Math.Sqrt ( a );
+                b = Math.Sqrt ( b );
+                c = Math.Sqrt ( c );
+                diam = 2.0 * a * b * c / Math.Sqrt ( ( a + b + c ) * ( - a + b + c ) 
+                                                              * ( a - b + c ) * ( a + b - c ) );
+            }
+
+            return diam;
+        }
 
         public static double[] triangle_edge_length_2d(double[] t)
 
@@ -984,6 +1079,111 @@ namespace Burkardt.Types
             return value;
         }
 
+        public static double triangle_point_dist ( double[] t, double[] p )
+
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    TRIANGLE_POINT_DIST: distance ( triangle, point ) in 2D.
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license. 
+        //
+        //  Modified:
+        //
+        //    01 June 2010
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, double T[2*3], the triangle vertices.
+        //
+        //    Input, double P[2], the point which is to be checked.
+        //
+        //    Output, double TRIANGLE_POINT_DIST, the distance from the point to the triangle.
+        //    DIST is zero if the point lies exactly on the triangle.
+        //
+        {
+            double value;
+
+            value =                 Line.segment_point_dist ( t, t, p, p1Index:+0*2, p2Index:+1*2 );
+            value = Math.Min ( value, Line.segment_point_dist ( t, t, p, p1Index:+1*2, p2Index:+2*2 ) );
+            value = Math.Min ( value, Line.segment_point_dist ( t, t, p, p1Index:+2*2, p2Index:+0*2 ) );
+
+            return value;
+        }
+        
+        public static void triangle_point_near ( double[] t, double[] p, ref double[] pn, 
+        ref double dist )
+
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    TRIANGLE_POINT_NEAR computes the nearest triangle point to a point in 2D.
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license. 
+        //
+        //  Modified:
+        //
+        //    01 June 2010
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, double T[2*3], the triangle vertices.
+        //
+        //    Input, double P[2], the point whose nearest neighbor
+        //    on the line is to be determined.
+        //
+        //    Output, double PN[2], the nearest point to P.
+        //
+        //    Output, double &DIST, the distance from the point to the triangle.
+        //
+        {
+            double dist12 = 0;
+            double dist23 = 0;
+            double dist31 = 0;
+            double tval = 0;
+            double[] pn12 = new double[2];
+            double[] pn23 = new double[2];
+            double[] pn31 = new double[2];
+            //
+            //  Find the distance to each of the line segments that make up the edges
+            //  of the triangle.
+            //
+            Line.segment_point_near ( t, t, p, ref pn12, ref dist12, ref tval, p1Index:+0*2, p2Index:+1*2 );
+
+            Line.segment_point_near ( t, t, p, ref pn23, ref dist23, ref tval, p1Index:+1*2, p2Index:+2*2 );
+
+            Line.segment_point_near ( t, t, p, ref pn31, ref dist31, ref tval, p1Index:+2*2, p2Index:+0*2 );
+
+            if ( dist12 <= dist23 && dist12 <= dist31 )
+            {
+                dist = dist12;
+                r8vec_copy ( 2, pn12, ref pn );
+            }
+            else if ( dist23 <= dist12 && dist23 <= dist31 )
+            {
+                dist = dist23;
+                r8vec_copy ( 2, pn23, ref pn );
+            }
+            else
+            {
+                dist = dist31;
+                r8vec_copy ( 2, pn31, ref pn );
+            }
+        }
         public static void triangle_points_plot(string file_name, double[] node_xy, int node_show,
                 int point_num, double[] point_xy, int point_show)
 
@@ -2135,6 +2335,93 @@ namespace Burkardt.Types
 
             File.WriteAllLines(node_file, output);
 
+        }
+        
+        public static double[] triangle_xsi_to_xy ( double[] t, double[] xsi )
+
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    TRIANGLE_XSI_TO_XY converts from barycentric to XY coordinates in 2D.
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    04 July 2005
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, double T[2*3], the triangle vertices.
+        //
+        //    Input, double XSI[3], the barycentric coordinates of a point.
+        //
+        //    Output, double TRIANGLE_XSI_TO_XY[2], the Cartesian coordinates of the point.
+        //
+        {
+            double[] p;
+
+            p = new double[2];
+
+            p[0] = xsi[0] * t[0+0*2] + xsi[1] * t[0+1*2] + xsi[2] * t[0+2*2];
+            p[1] = xsi[0] * t[1+0*2] + xsi[1] * t[1+1*2] + xsi[2] * t[1+2*2];
+
+            return p;
+        }
+
+        public static double[] triangle_xy_to_xsi ( double[] t, double[] p )
+
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    TRIANGLE_XY_TO_XSI converts from XY to barycentric in 2D.
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    04 July 2005
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, double T[2*3], the triangle vertices.
+        //
+        //    Input, double P[2], the XY coordinates of a point.
+        //
+        //    Output, double TRIANGLE_XY_TO_XSI[3], the barycentric coordinates of the point.
+        //
+        {
+            double det;
+            double[] xsi;
+
+            xsi = new double[3];
+
+            det = ( t[0+0*2] - t[0+2*2] ) * ( t[1+1*2] - t[1+2*2] )
+                  - ( t[0+1*2] - t[0+2*2] ) * ( t[1+0*2] - t[1+2*2] );
+
+            xsi[0] = (   ( t[1+1*2] - t[1+2*2] ) * ( p[0] - t[0+2*2] )
+                         - ( t[0+1*2] - t[0+2*2] ) * ( p[1] - t[1+2*2] ) ) / det;
+
+            xsi[1] = ( - ( t[1+0*2] - t[1+2*2] ) * ( p[0] - t[0+2*2] )
+                       + ( t[0+0*2] - t[0+2*2] ) * ( p[1] - t[1+2*2] ) ) / det;
+
+            xsi[2] = 1.0 - xsi[0] - xsi[1];
+
+            return xsi;
         }
     }
 }

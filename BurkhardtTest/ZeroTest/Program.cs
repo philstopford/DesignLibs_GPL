3e4cd66fcb8e5d6ba4c682c5bee25905c6321b2e
philstopford <phil.stopford@gmail.com>
2021-07-29 17:15:37 -0500

    Zero Test

---
+++ BurkhardtTest/ZeroTest/Program.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using Burkardt;
+
+namespace ZeroTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    zero_test() tests zero().
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    31 May 2021
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            double a;
+            double b;
+
+            Console.WriteLine("");
+            Console.WriteLine("zero_test():");
+            Console.WriteLine("  zero() seeks a root of a function F(X)");
+            Console.WriteLine("  in an interval [A,B].");
+
+            a = 1.0;
+            b = 2.0;
+            zero_example(a, b, f_01, "f_01(x) = sin ( x ) - x / 2");
+
+            a = 0.0;
+            b = 1.0;
+            zero_example(a, b, f_02, "f_02(x) = 2 * x - exp ( - x )");
+
+            a = -1.0;
+            b = 0.5;
+            zero_example(a, b, f_03, "f_03(x) = x * exp ( - x )");
+
+            a = 0.0001;
+            b = 20.0;
+            zero_example(a, b, f_04, "f_04(x) = exp ( x ) - 1 / ( 100 * x * x )");
+
+            a = -5.0;
+            b = 2.0;
+            zero_example(a, b, f_05, "f_05(x) = (x+3) * (x-1) * (x-1)");
+            //
+            //  Terminate.
+            //
+            Console.WriteLine("");
+            Console.WriteLine("zero_test():");
+            Console.WriteLine("  Normal end of execution.");
+            Console.WriteLine("");
+        }
+
+        static void zero_example(double a, double b, Func<double, double> f, string title)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    zero_example() tests zero() on one test function.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    31 May 2021
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Input:
+            //
+            //    double A, B, the endpoints of the change of sign interval.
+            //
+            //    double F ( double x ), the name of a user-supplied
+            //    function which evaluates the function whose zero is being sought.
+            //
+            //    string TITLE, a title for the problem.
+            //
+        {
+            int calls = 0;
+            double fa;
+            double fb;
+            double fz;
+            double t;
+            double z;
+
+            t = double.Epsilon;
+
+            z = Zero.zero(a, b, t, f, ref calls);
+            fz = f(z);
+            fa = f(a);
+            fb = f(b);
+
+            Console.WriteLine("");
+            Console.WriteLine("  " + title + "");
+            Console.WriteLine("");
+            Console.WriteLine("           A                 Z             B");
+            Console.WriteLine("         F(A)              F(Z)          F(B)");
+            Console.WriteLine("  " + a.ToString().PadLeft(14)
+                                   + "  " + z.ToString().PadLeft(14)
+                                   + "  " + b.ToString().PadLeft(14) + "");
+            Console.WriteLine("  " + fa.ToString().PadLeft(14)
+                                   + "  " + fz.ToString().PadLeft(14)
+                                   + "  " + fb.ToString().PadLeft(14) + "");
+            Console.WriteLine("  Number of calls to F = " + calls + "");
+
+            return;
+        }
+
+        static double f_01(double x)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    F_01 evaluates sin ( x ) - x / 2.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    13 April 2008
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Input:
+            //
+            //    double X, the point at which F is to be evaluated.
+            //
+            //  Output:
+            //
+            //    double F_01, the value of the function at X.
+            //
+        {
+            double value;
+
+            value = Math.Sin(x) - 0.5 * x;
+
+            return value;
+        }
+
+        static double f_02(double x)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    F_02 evaluates 2*x-exp(-x).
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    13 April 2008
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Input:
+            //
+            //    double X, the point at which F is to be evaluated.
+            //
+            //  Output:
+            //
+            //    double F_02, the value of the function at X.
+            //
+        {
+            double value;
+
+            value = 2.0 * x - Math.Exp(-x);
+
+            return value;
+        }
+
+        static double f_03(double x)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    F_03 evaluates x*exp(-x).
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    13 April 2008
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Input:
+            //
+            //    double X, the point at which F is to be evaluated.
+            //
+            //  Output:
+            //
+            //    double F_03, the value of the function at X.
+            //
+        {
+            double value;
+
+            value = x * Math.Exp(-x);
+
+            return value;
+        }
+
+        static double f_04(double x)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    F_04 evaluates exp(x) - 1 / (100*x*x).
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    13 April 2008
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Input:
+            //
+            //    double X, the point at which F is to be evaluated.
+            //
+            //  Output:
+            //
+            //    double F_04, the value of the function at X.
+            //
+        {
+            double value;
+
+            value = Math.Exp(x) - 1.0 / 100.0 / x / x;
+
+            return value;
+        }
+
+        static double f_05(double x)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    F_05 evaluates (x+3)*(x-1)*(x-1).
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    13 April 2008
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Input:
+            //
+            //    double X, the point at which F is to be evaluated.
+            //
+            //  Output:
+            //
+            //    double F_05, the value of the function at X.
+            //
+        {
+            double value;
+
+            value = (x + 3.0) * (x - 1.0) * (x - 1.0);
+
+            return value;
+        }
+    }
+}
---
+++ Common/Burkardt/Zero.cs
@@ -0,0 +1,194 @@
+﻿using System;
+
+namespace Burkardt
+{
+    public static class Zero
+    {
+        public static double zero(double a, double b, double t, Func<double, double> f, ref int calls)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    zero() seeks a root of a function F(X) in an interval [A,B].
+            //
+            //  Discussion:
+            //
+            //    The interval [A,B] must be a change of sign interval for F.
+            //    That is, F(A) and F(B) must be of opposite signs.  Then
+            //    assuming that F is continuous implies the existence of at least
+            //    one value C between A and B for which F(C) = 0.
+            //
+            //    The location of the zero is determined to within an accuracy
+            //    of 4 * EPSILON * abs ( C ) + 2 * T.
+            //
+            //    Thanks to Thomas Secretin for pointing out a transcription error in the
+            //    setting of the value of P, 11 February 2013.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    13 July 2021
+            //
+            //  Author:
+            //
+            //    Original FORTRAN77 version by Richard Brent.
+            //    C++ version by John Burkardt.
+            //
+            //  Reference:
+            //
+            //    Richard Brent,
+            //    Algorithms for Minimization Without Derivatives,
+            //    Dover, 2002,
+            //    ISBN: 0-486-41998-3,
+            //    LC: QA402.5.B74.
+            //
+            //  Input:
+            //
+            //    double A, B, the endpoints of the change of sign interval.
+            //
+            //    double T, a positive error tolerance.
+            //
+            //    double F(double x), the name of a user-supplied function.
+            //
+            //  Output:
+            //
+            //    int &CALLS, the number of calls to the function F.
+            //
+            //    double ZERO, the estimated value of a zero of the function F.
+            //
+        {
+            double c;
+            double d;
+            double e;
+            double fa;
+            double fb;
+            double fc;
+            double m;
+            double p;
+            double q;
+            double r;
+            double s;
+            double sa;
+            double sb;
+            double tol;
+
+            calls = 0;
+            //
+            //  Make local copies of A and B.
+            //
+            sa = a;
+            fa = f(sa);
+            calls = calls + 1;
+
+            sb = b;
+            fb = f(sb);
+            calls = calls + 1;
+
+            c = sa;
+            fc = fa;
+            e = sb - sa;
+            d = e;
+
+            for (;;)
+            {
+                if (Math.Abs(fc) < Math.Abs(fb))
+                {
+                    sa = sb;
+                    sb = c;
+                    c = sa;
+                    fa = fb;
+                    fb = fc;
+                    fc = fa;
+                }
+
+                tol = 2.0 * double.Epsilon * Math.Abs(sb) + t;
+                m = 0.5 * (c - sb);
+
+                if (Math.Abs(m) <= tol || fb == 0.0)
+                {
+                    break;
+                }
+
+                if (Math.Abs(e) < tol || Math.Abs(fa) <= Math.Abs(fb))
+                {
+                    e = m;
+                    d = e;
+                }
+                else
+                {
+                    s = fb / fa;
+
+                    if (sa == c)
+                    {
+                        p = 2.0 * m * s;
+                        q = 1.0 - s;
+                    }
+                    else
+                    {
+                        q = fa / fc;
+                        r = fb / fc;
+                        p = s * (2.0 * m * q * (q - r) - (sb - sa) * (r - 1.0));
+                        q = (q - 1.0) * (r - 1.0) * (s - 1.0);
+                    }
+
+                    if (0.0 < p)
+                    {
+                        q = -q;
+                    }
+                    else
+                    {
+                        p = -p;
+                    }
+
+                    s = e;
+                    e = d;
+
+                    if (2.0 * p < 3.0 * m * q - Math.Abs(tol * q) &&
+                        p < Math.Abs(0.5 * s * q))
+                    {
+                        d = p / q;
+                    }
+                    else
+                    {
+                        e = m;
+                        d = e;
+                    }
+                }
+
+                sa = sb;
+                fa = fb;
+
+                if (tol < Math.Abs(d))
+                {
+                    sb = sb + d;
+                }
+                else if (0.0 < m)
+                {
+                    sb = sb + tol;
+                }
+                else
+                {
+                    sb = sb - tol;
+                }
+
+                fb = f(sb);
+                calls = calls + 1;
+
+                if ((0.0 < fb && 0.0 < fc) || (fb <= 0.0 && fc <= 0.0))
+                {
+                    c = sa;
+                    fc = fa;
+                    e = sb - sa;
+                    d = e;
+                }
+            }
+
+            return sb;
+        }
+
+    }
+}

2619d891960bc11cce157e1a07bdc7ce7feb1aa9
philstopford <phil.stopford@gmail.com>
2021-08-08 16:58:05 -0500

    TetMesh Linear-Quadratic

--- Common/Burkardt/Tetrahedron/TetMesh.cs
+++ Common/Burkardt/Tetrahedron/TetMesh.cs
@@ -6,6 +6,99 @@ namespace Burkardt.TetrahedronNS
 {
     public static class TetMesh
     {
+        public static void tet_mesh_base_zero(int node_num, int element_order, int element_num,
+                ref int[] element_node)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    TET_MESH_BASE_ZERO ensures that the element definition is zero-based.
+            //
+            //  Discussion:
+            //
+            //    The ELEMENT_NODE array contains nodes indices that form elements.
+            //    The convention for node indexing might start at 0 or at 1.
+            //    Since a C++ program will naturally assume a 0-based indexing, it is
+            //    necessary to check a given element definition and, if it is actually
+            //    1-based, to convert it.
+            //
+            //    This function attempts to detect 1-based node indexing and correct it.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    27 September 2009
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Parameters:
+            //
+            //    Input, int NODE_NUM, the number of nodes.
+            //
+            //    Input, int ELEMENT_ORDER, the order of the elements.
+            //
+            //    Input, int ELEMENT_NUM, the number of elements.
+            //
+            //    Input/output, int ELEMENT_NODE[ELEMENT_ORDER*ELEMENT_NUM], the element
+            //    definitions.
+            //
+        {
+            int element;
+            int node;
+            int node_max;
+            int node_min;
+            int order;
+            //
+            //  If the element information is 1-based, make it 0-based.
+            //
+            node_min = node_num + 1;
+            node_max = -1;
+            for (element = 0; element < element_num; element++)
+            {
+                for (order = 0; order < element_order; order++)
+                {
+                    node = element_node[order + element * element_order];
+                    node_min = Math.Min(node_min, node);
+                    node_max = Math.Max(node_max, node);
+                }
+            }
+
+            if (node_min == 1 && node_max == node_num)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("TET_MESH_BASE_ZERO:");
+                Console.WriteLine("  The element indexing appears to be 1-based!");
+                Console.WriteLine("  This will be converted to 0-based.");
+                for (element = 0; element < element_num; element++)
+                {
+                    for (order = 0; order < element_order; order++)
+                    {
+                        element_node[order + element * element_order] =
+                            element_node[order + element * element_order] - 1;
+                    }
+                }
+            }
+            else if (node_min == 0 && node_max == node_num - 1)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("TET_MESH_BASE_ZERO:");
+                Console.WriteLine("  The element indexing appears to be 0-based!");
+                Console.WriteLine("  No conversion is necessary.");
+            }
+            else
+            {
+                Console.WriteLine("");
+                Console.WriteLine("TET_MESH_BASE_ZERO - Warning!");
+                Console.WriteLine("  The element indexing is not of a recognized type.");
+            }
+        }
+
         public static int[] tet_mesh_neighbor_tets(int tetra_order, int tetra_num,
                 int[] tetra_node)
 

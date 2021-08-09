69f893eb98f0d393da411564070819eee2c6c7e3
philstopford <phil.stopford@gmail.com>
2021-08-09 18:00:26 -0500

    Truncated Normal Rule

---
+++ BurkhardtTest/TruncatedNormalRuleTest/Program.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using Burkardt;
+using Burkardt.Types;
+
+namespace TruncatedNormalRuleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    MAIN is the main program for TRUNCATED_NORMAL_RULE.
+            //
+            //  Discussion:
+            //
+            //    This program computes a truncated normal quadrature rule
+            //    and writes it to a file.
+            //
+            //    The user specifies:
+            //    * option: 0/1/2/3 for none, lower, upper, double truncation.
+            //    * N, the number of points in the rule;
+            //    * MU, the mean of the original normal distribution;
+            //    * SIGMA, the standard deviation of the original normal distribution,
+            //    * A, the left endpoint (for options 1 or 3)
+            //    * B, the right endpoint (for options 2 or 3);
+            //    * FILENAME, the root name of the output files.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    20 September 2013
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            double a;
+            double b;
+            string filename;
+            int iarg;
+            double[] moment = new double[1];
+            double mu;
+            int n;
+            int option;
+            double[] r;
+            double sigma;
+            double[] w;
+            double[] x;
+
+            Console.WriteLine("");
+            Console.WriteLine("TRUNCATED_NORMAL_RULE");
+            Console.WriteLine("");
+            Console.WriteLine("  For the (truncated) Gaussian probability density function");
+            Console.WriteLine("    pdf(x) = exp(-0.5*((x-MU)/SIGMA)^2) / SIGMA / sqrt ( 2 * pi )");
+            Console.WriteLine("  compute an N-point quadrature rule for approximating");
+            Console.WriteLine("    Integral ( A <= x <= B ) f(x) pdf(x) dx");
+            Console.WriteLine("");
+            Console.WriteLine("  The value of OPTION determines the truncation interval [A,B]:");
+            Console.WriteLine("  0: (-oo,+oo)");
+            Console.WriteLine("  1: [A,+oo)");
+            Console.WriteLine("  2: (-oo,B]");
+            Console.WriteLine("  3: [A,B]");
+            Console.WriteLine("");
+            Console.WriteLine("  The user specifies OPTION, N, MU, SIGMA, A, B and FILENAME.");
+            Console.WriteLine("");
+            Console.WriteLine("  FILENAME is used to generate 3 files:");
+            Console.WriteLine("");
+            Console.WriteLine("    filename_w.txt - the weight file");
+            Console.WriteLine("    filename_x.txt - the abscissa file.");
+            Console.WriteLine("    filename_r.txt - the region file, listing A and B.");
+
+            iarg = 0;
+            //
+            //  Get OPTION.
+            //
+            try
+            {
+                option = Convert.ToInt32(args[iarg]);
+            }
+            catch
+            {
+                Console.WriteLine("");
+                Console.WriteLine("  Enter the value of OPTION 0/1/2/3:  ");
+                option = Convert.ToInt32(Console.ReadLine());
+            }
+
+            if (option < 0 || 3 < option)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("TRUNCATED_NORMAL_RULE - Fatal error!");
+                Console.WriteLine("  0 <= OPTION <= 3 was required.");
+                return;
+            }
+
+            //
+            //  Get N.
+            //
+            iarg = iarg + 1;
+            try
+            {
+                n = Convert.ToInt32(args[iarg]);
+            }
+            catch
+            {
+                Console.WriteLine("");
+                Console.WriteLine("  Enter the value of N (1 or greater)");
+                n = Convert.ToInt32(Console.ReadLine());
+            }
+
+            //
+            //  Get MU.
+            //
+            iarg = iarg + 1;
+            try
+            {
+                mu = Convert.ToDouble(args[iarg]);
+            }
+            catch
+            {
+                Console.WriteLine("");
+                Console.WriteLine("  Enter MU, the mean value of the normal distribution:");
+                mu = Convert.ToDouble(Console.ReadLine());
+            }
+
+            //
+            //  Get SIGMA.
+            //
+            iarg = iarg + 1;
+            try
+            {
+                sigma = Convert.ToDouble(args[iarg]);
+            }
+            catch
+            {
+                Console.WriteLine("");
+                Console.WriteLine("  Enter SIGMA, the standard deviation of the normal distribution:");
+                sigma = Convert.ToDouble(Console.ReadLine());
+            }
+
+            sigma = Math.Abs(sigma);
+            //
+            //  Get A.
+            //
+            if (option == 1 || option == 3)
+            {
+                iarg = iarg + 1;
+                try
+                {
+                    a = Convert.ToDouble(args[iarg]);
+                }
+                catch
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("  Enter the left endpoint A:");
+                    a = Convert.ToDouble(Console.ReadLine());
+                }
+            }
+            else
+            {
+                a = -typeMethods.r8_huge();
+            }
+
+            //
+            //  Get B.
+            //
+            if (option == 2 || option == 3)
+            {
+                iarg = iarg + 1;
+                try
+                {
+                    b = Convert.ToDouble(args[iarg]);
+                }
+                catch
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("  Enter the right endpoint B:");
+                    b = Convert.ToDouble(Console.ReadLine());
+                }
+            }
+            else
+            {
+                b = typeMethods.r8_huge();
+            }
+
+            if (b <= a)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("TRUNCATED_NORMAL_RULE - Fatal error!");
+                Console.WriteLine("  A < B required!");
+                return;
+            }
+
+            //
+            //  Get FILENAME:
+            //
+            iarg = iarg + 1;
+            try
+            {
+                filename = args[iarg];
+            }
+            catch
+            {
+                Console.WriteLine("");
+                Console.WriteLine("  Enter FILENAME, the \"root name\" of the quadrature files).");
+                filename = Console.ReadLine();
+            }
+
+            //
+            //  Input summary.
+            //
+            Console.WriteLine("");
+            Console.WriteLine("  OPTION = " + option + "");
+            Console.WriteLine("  N = " + n + "");
+            Console.WriteLine("  MU = " + mu + "");
+            Console.WriteLine("  SIGMA = " + sigma + "");
+            Console.WriteLine("  A = " + a + "");
+            Console.WriteLine("  B = " + b + "");
+            Console.WriteLine("  FILENAME = \"" + filename + "\".");
+            //
+            //  Compute the moments.
+            //
+            if (option == 0)
+            {
+                moment = Moments.moments_normal(2 * n + 1, mu, sigma);
+            }
+            else if (option == 1)
+            {
+                moment = Moments.moments_truncated_normal_a(2 * n + 1, mu, sigma, a);
+            }
+            else if (option == 2)
+            {
+                moment = Moments.moments_truncated_normal_b(2 * n + 1, mu, sigma, b);
+            }
+            else if (option == 3)
+            {
+                moment = Moments.moments_truncated_normal_ab(2 * n + 1, mu, sigma, a, b);
+            }
+
+            //
+            //  Construct the rule from the moments.
+            //
+            w = new double[n];
+            x = new double[n];
+
+            Moments.moment_method(n, moment, ref x, ref w);
+            //
+            //  Output the rule.
+            //
+            r = new double[2];
+            r[0] = a;
+            r[1] = b;
+
+            QuadratureRule.rule_write(n, filename, x, w, r);
+
+            Console.WriteLine("");
+            Console.WriteLine("TRUNCATED_NORMAL_RULE:");
+            Console.WriteLine("  Normal end of execution.");
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ Common/Burkardt/Quadrature/Moments.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using Burkardt.Probability;
+using Burkardt.Types;
+
+namespace Burkardt
+{
+    public static class Moments
+    {
+        public static void moment_method(int n, double[] moment, ref double[] x, ref double[] w)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    MOMENT_METHOD computes a quadrature rule by the method of moments.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    18 September 2013
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Reference:
+            //
+            //    Gene Golub, John Welsch,
+            //    Calculation of Gaussian Quadrature Rules,
+            //    Mathematics of Computation,
+            //    Volume 23, Number 106, April 1969, pages 221-230.
+            //
+            //  Parameters:
+            //
+            //    Input, int N, the order of the quadrature rule.
+            //
+            //    Input, double MOMENT[2*N+1], moments 0 through 2*N.
+            //
+            //    Output, double X[N], W[N], the points and weights of the quadrature rule.
+            //
+        {
+            double[] alpha;
+            double[] beta;
+            bool debug;
+            int flag = 0;
+            double[] h;
+            int i;
+            int it_max;
+            int it_num = 0;
+            int j;
+            double[] jacobi;
+            double[] r;
+            int rot_num = 0;
+            double[] v;
+
+            debug = false;
+
+            if (debug)
+            {
+                typeMethods.r8vec_print(2 * n + 1, moment, "  Moments:");
+            }
+
+            //
+            //  Define the N+1 by N+1 Hankel matrix H(I,J) = moment(I+J).
+            //
+            h = new double[(n + 1) * (n + 1)];
+
+            for (i = 0; i <= n; i++)
+            {
+                for (j = 0; j <= n; j++)
+                {
+                    h[i + j * (n + 1)] = moment[i + j];
+                }
+            }
+
+            if (debug)
+            {
+                typeMethods.r8mat_print(n + 1, n + 1, h, "  Hankel matrix:");
+            }
+
+            //
+            //  Compute R, the upper triangular Cholesky factor of H.
+            //
+            r = typeMethods.r8mat_cholesky_factor_upper(n + 1, h, ref flag);
+
+            if (flag != 0)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("MOMENT_METHOD - Fatal error!");
+                Console.WriteLine("  R8MAT_CHOLESKY_FACTOR_UPPER returned FLAG = " + flag + "");
+                return;
+            }
+
+            if (debug)
+            {
+                typeMethods.r8mat_print(n + 1, n + 1, r, "  Cholesky factor:");
+            }
+
+            //
+            //  Compute ALPHA and BETA from R, using Golub and Welsch's formula.
+            //
+            alpha = new double[n];
+
+            alpha[0] = r[0 + 1 * (n + 1)] / r[0 + 0 * (n + 1)];
+            for (i = 1; i < n; i++)
+            {
+                alpha[i] = r[i + (i + 1) * (n + 1)] / r[i + i * (n + 1)]
+                           - r[i - 1 + i * (n + 1)] / r[i - 1 + (i - 1) * (n + 1)];
+            }
+
+            beta = new double[n - 1];
+
+            for (i = 0; i < n - 1; i++)
+            {
+                beta[i] = r[i + 1 + (i + 1) * (n + 1)] / r[i + i * (n + 1)];
+            }
+
+            //
+            //  Compute the points and weights from the moments.
+            //
+            jacobi = new double[n * n];
+
+            for (j = 0; j < n; j++)
+            {
+                for (i = 0; i < n; i++)
+                {
+                    jacobi[i + j * n] = 0.0;
+                }
+            }
+
+            for (i = 0; i < n; i++)
+            {
+                jacobi[i + i * n] = alpha[i];
+            }
+
+            for (i = 0; i < n - 1; i++)
+            {
+                jacobi[i + (i + 1) * n] = beta[i];
+                jacobi[i + 1 + i * n] = beta[i];
+            }
+
+            if (debug)
+            {
+                typeMethods.r8mat_print(n, n, jacobi, "  The Jacobi matrix:");
+            }
+
+            //
+            //  Get the eigendecomposition of the Jacobi matrix.
+            //
+            it_max = 100;
+            v = new double[n * n];
+
+            Jacobi.jacobi_eigenvalue(n, jacobi, it_max, ref v, ref x, ref it_num, ref rot_num);
+
+            if (debug)
+            {
+                typeMethods.r8mat_print(n, n, v, "  Eigenvector");
+            }
+
+            for (i = 0; i < n; i++)
+            {
+                w[i] = moment[0] * Math.Pow(v[0 + i * n], 2);
+            }
+        }
+
+        public static double[] moments_normal(int m, double mu, double sigma)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    MOMENTS_NORMAL returns moments of the standard Normal distribution.
+            //
+            //  Discussion:
+            //
+            //    pdf(x) = exp ( -((x-mu)/sigma)^2/2 ) / sigma / sqrt ( pi * 2 )
+            //    mu(k) = integral ( -oo < x < +oo ) x^k pdf(x) dx
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    18 September 2013
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Parameters:
+            //
+            //    Input, int M, the number of moments desired.
+            //
+            //    Input, double MU, SIGMA, the mean and standard deviation.
+            //
+            //    Output, double W(0:M-1), the weighted integrals of X^0 
+            //    through X^(M-1).
+            //
+        {
+            int j;
+            int j_hi;
+            int k;
+            double t;
+            double[] w;
+
+            w = new double[m];
+
+            for (k = 0; k < m; k++)
+            {
+                t = 0.0;
+                j_hi = k / 2;
+                for (j = 0; j <= j_hi; j++)
+                {
+                    t = t + typeMethods.r8_choose(k, 2 * j) * typeMethods.r8_factorial2(2 * j - 1)
+                                                            * Math.Pow(sigma, 2 * j) * Math.Pow(mu, k - 2 * j);
+                }
+
+                w[k] = t;
+            }
+
+            return w;
+        }
+
+        public static double[] moments_truncated_normal_ab(int m, double mu, double sigma,
+                double a, double b)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    MOMENTS_TRUNCATED_NORMAL_AB: moments of truncated Normal distribution.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    19 September 2013
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Parameters:
+            //
+            //    Input, int M, the number of moments desired.
+            //
+            //    Input, double MU, SIGMA, the mean and standard deviation.
+            //
+            //    Input, double A, B, the lower and upper truncation limits.
+            //    A < B.
+            //
+            //    Output, double W(0:M-1), the weighted integrals of X^0 
+            //    through X^(M-1).
+            //
+        {
+            int order;
+            double[] w;
+
+            w = new double[m];
+
+            for (order = 0; order < m; order++)
+            {
+                w[order] = Truncated.truncated_normal_ab_moment(order, mu, sigma, a, b);
+            }
+
+            return w;
+        }
+
+        public static double[] moments_truncated_normal_a(int m, double mu, double sigma,
+                double a)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    MOMENTS_TRUNCATED_NORMAL_A: moments of lower truncated Normal.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    19 September 2013
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Parameters:
+            //
+            //    Input, int M, the number of moments desired.
+            //
+            //    Input, double MU, SIGMA, the mean and standard deviation.
+            //
+            //    Input, double A, the lower truncation limit.
+            //    A < B.
+            //
+            //    Output, double W(0:M-1), the weighted integrals of X^0 
+            //    through X^(M-1).
+            //
+        {
+            int order;
+            double[] w;
+
+            w = new double[m];
+
+            for (order = 0; order < m; order++)
+            {
+                w[order] = Truncated.truncated_normal_a_moment(order, mu, sigma, a);
+            }
+
+            return w;
+        }
+
+        public static double[] moments_truncated_normal_b(int m, double mu, double sigma,
+                double b)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    MOMENTS_TRUNCATED_NORMAL_B: moments of upper truncated Normal.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    19 September 2013
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Parameters:
+            //
+            //    Input, int M, the number of moments desired.
+            //
+            //    Input, double MU, SIGMA, the mean and standard deviation.
+            //
+            //    Input, double B, the upper truncation limit.
+            //    A < B.
+            //
+            //    Output, double W(0:M-1), the weighted integrals of X^0 
+            //    through X^(M-1).
+            //
+        {
+            int order;
+            double[] w;
+
+            w = new double[m];
+
+            for (order = 0; order < m; order++)
+            {
+                w[order] = Truncated.truncated_normal_b_moment(order, mu, sigma, b);
+            }
+
+            return w;
+        }
+    }
+}

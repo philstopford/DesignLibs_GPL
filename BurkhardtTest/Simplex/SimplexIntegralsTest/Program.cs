bb7edecad35463b04fb9bdc88dd27de2e1348849
philstopford <phil.stopford@gmail.com>
2021-08-18 12:20:47 -0500

    Simplex Integrals

---
+++ BurkhardtTest/Simplex/SimplexIntegralsTest/Program.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using Burkardt.MonomialNS;
+using Burkardt.SimplexNS;
+using Burkardt.Types;
+using Burkardt.Uniform;
+
+namespace SimplexIntegralsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    MAIN is the main program for SIMPLEX_INTEGRALS_TEST.
+            //
+            //  Discussion:
+            //
+            //    SIMPLEX_INTEGRALS_TEST tests the SIMPLEX_INTEGRALS library.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    16 January 2014
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            Console.WriteLine("");
+            Console.WriteLine("SIMPLEX_INTEGRALS_TEST");
+            Console.WriteLine("  Test the SIMPLEX_INTEGRALS library.");
+
+            test01();
+            test02();
+
+            Console.WriteLine("");
+            Console.WriteLine("SIMPLEX_INTEGRALS_TEST");
+            Console.WriteLine("  Normal end of execution.");
+            Console.WriteLine("");
+        }
+
+        static void test01()
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    TEST01 compares exact and estimated integrals in 3D.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    16 January 2014
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            int[] e;
+            double error;
+            double exact;
+            int m = 3;
+            int n = 4192;
+            double result;
+            int seed;
+            int test;
+            int test_num = 20;
+            double[] value;
+            double[] x;
+
+            Console.WriteLine("");
+            Console.WriteLine("TEST01");
+            Console.WriteLine("  Estimate monomial integrals using Monte Carlo");
+            Console.WriteLine("  over the interior of the unit simplex in M dimensions.");
+            //
+            //  Get sample points.
+            //
+            seed = 123456789;
+            x = Integrals.simplex01_sample(m, n, ref seed);
+
+            Console.WriteLine("");
+            Console.WriteLine("  Number of sample points used is " + n + "");
+            //
+            //  Randomly choose exponents.
+            //
+            Console.WriteLine("");
+            Console.WriteLine("  We randomly choose the exponents.");
+            Console.WriteLine("");
+            Console.WriteLine("  Ex  Ey  Ez     MC-Estimate      Exact           Error");
+            Console.WriteLine("");
+
+            for (test = 1; test <= test_num; test++)
+            {
+                e = UniformRNG.i4vec_uniform_ab_new(m, 0, 4, ref seed);
+
+                value = Monomial.monomial_value(m, n, e, x);
+
+                result = Integrals.simplex01_volume(m) * typeMethods.r8vec_sum(n, value)
+                         / (double)(n);
+
+                exact = Integrals.simplex01_monomial_integral(m, e);
+                error = Math.Abs(result - exact);
+
+                Console.WriteLine("  " + e[0].ToString().PadLeft(2)
+                                       + "  " + e[1].ToString().PadLeft(2)
+                                       + "  " + e[2].ToString().PadLeft(2)
+                                       + "  " + result.ToString().PadLeft(14)
+                                       + "  " + exact.ToString().PadLeft(14)
+                                       + "  " + error.ToString().PadLeft(14) + "");
+            }
+
+        }
+
+        static void test02()
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    TEST02 compares exact and estimated integrals in 6D.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    16 January 2014
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            int[] e;
+            double error;
+            double exact;
+            int m = 6;
+            int n = 4192;
+            double result;
+            int seed;
+            int test;
+            int test_num = 20;
+            double[] value;
+            double[] x;
+
+            Console.WriteLine("");
+            Console.WriteLine("TEST02");
+            Console.WriteLine("  Estimate monomial integrals using Monte Carlo");
+            Console.WriteLine("  over the interior of the unit simplex in M dimensions.");
+            //
+            //  Get sample points.
+            //
+            seed = 123456789;
+            x = Integrals.simplex01_sample(m, n, ref seed);
+
+            Console.WriteLine("");
+            Console.WriteLine("  Number of sample points used is " + n + "");
+            //
+            //  Randomly choose exponents.
+            //
+            Console.WriteLine("");
+            Console.WriteLine("  We randomly choose the exponents.");
+            Console.WriteLine("");
+            Console.WriteLine("  E1  E2  E3  E4  E5  E6     MC-Estimate      Exact           Error");
+            Console.WriteLine("");
+
+            for (test = 1; test <= test_num; test++)
+            {
+                e = UniformRNG.i4vec_uniform_ab_new(m, 0, 4, ref seed);
+
+                value = Monomial.monomial_value(m, n, e, x);
+
+                result = Integrals.simplex01_volume(m) * typeMethods.r8vec_sum(n, value)
+                         / (double)(n);
+
+                exact = Integrals.simplex01_monomial_integral(m, e);
+                error = Math.Abs(result - exact);
+
+                Console.WriteLine("  " + e[0].ToString().PadLeft(2)
+                                       + "  " + e[1].ToString().PadLeft(2)
+                                       + "  " + e[2].ToString().PadLeft(2)
+                                       + "  " + e[3].ToString().PadLeft(2)
+                                       + "  " + e[4].ToString().PadLeft(2)
+                                       + "  " + e[5].ToString().PadLeft(2)
+                                       + "  " + result.ToString().PadLeft(14)
+                                       + "  " + exact.ToString().PadLeft(14)
+                                       + "  " + error.ToString().PadLeft(14) + "");
+            }
+
+        }
+    }
+}
---
+++ Common/Burkardt/Simplex/Integrals.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using Burkardt.Types;
+using Burkardt.Uniform;
+
+namespace Burkardt.SimplexNS
+{
+    public static class Integrals
+    {
+        public static double simplex01_monomial_integral(int m, int[] e)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    SIMPLEX01_MONOMIAL_INTEGRAL: integrals in the unit simplex in M dimensions.
+            //
+            //  Discussion:
+            //
+            //    The monomial is F(X) = product ( 1 <= I <= M ) X(I)^E(I).
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license. 
+            //
+            //  Modified:
+            //
+            //    16 January 2014
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Parameters:
+            //
+            //    Input, int M, the spatial dimension.
+            //
+            //    Input, int E[M], the exponents.  
+            //    Each exponent must be nonnegative.
+            //
+            //    Output, double SIMPLEX01_MONOMIAL_INTEGRAL, the integral.
+            //
+        {
+            int i;
+            double integral;
+            int j;
+            int k;
+
+            for (i = 0; i < m; i++)
+            {
+                if (e[i] < 0)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("SIMPLEX01_MONOMIAL_INTEGRAL - Fatal error!");
+                    Console.WriteLine("  All exponents must be nonnegative.");
+                    return (1);
+                }
+            }
+
+            k = 0;
+            integral = 1.0;
+
+            for (i = 0; i < m; i++)
+            {
+                for (j = 1; j <= e[i]; j++)
+                {
+                    k = k + 1;
+                    integral = integral * (double)(j) / (double)(k);
+                }
+            }
+
+            for (i = 0; i < m; i++)
+            {
+                k = k + 1;
+                integral = integral / (double)(k);
+            }
+
+            return integral;
+        }
+
+        public static double[] simplex01_sample(int m, int n, ref int seed)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    SIMPLEX01_SAMPLE samples the unit simplex in M dimensions.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license. 
+            //
+            //  Modified:
+            //
+            //    16 January 2015
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Reference:
+            //
+            //    Reuven Rubinstein,
+            //    Monte Carlo Optimization, Simulation, and Sensitivity 
+            //    of Queueing Networks,
+            //    Krieger, 1992,
+            //    ISBN: 0894647644,
+            //    LC: QA298.R79.
+            //
+            //  Parameters:
+            //
+            //    Input, int M, the spatial dimension.
+            //
+            //    Input, int N, the number of points.
+            //
+            //    Input/output, int &SEED, a seed for the random 
+            //    number generator.
+            //
+            //    Output, double SIMPLEX01_SAMPLE_01[M*N], the points.
+            //
+        {
+            double[] e;
+            double e_sum;
+            int i;
+            int j;
+            double[] x;
+
+            x = new double[m * n];
+
+            for (j = 0; j < n; j++)
+            {
+                e = UniformRNG.r8vec_uniform_01_new(m + 1, ref seed);
+
+                for (i = 0; i < m + 1; i++)
+                {
+                    e[i] = -Math.Log(e[i]);
+                }
+
+                e_sum = typeMethods.r8vec_sum(m + 1, e);
+
+                for (i = 0; i < m; i++)
+                {
+                    x[i + j * m] = e[i] / e_sum;
+                }
+            }
+
+            return x;
+        }
+
+        public static double simplex01_volume(int m)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    SIMPLEX01_VOLUME returns the volume of the unit simplex in M dimensions.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license. 
+            //
+            //  Modified:
+            //
+            //    16 January 2014
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Parameters:
+            //
+            //    Input, int M, the spatial dimension.
+            //
+            //    Output, double SIMPLEX01_VOLUME, the volume.
+            //
+        {
+            int i;
+            double volume;
+
+            volume = 1.0;
+            for (i = 1; i <= m; i++)
+            {
+                volume = volume / (double)(i);
+            }
+
+            return volume;
+        }
+    }
+}

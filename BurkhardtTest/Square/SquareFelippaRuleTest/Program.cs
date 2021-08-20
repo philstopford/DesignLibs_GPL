11e79506d726e567a0ed93479e5847dba63179c7
philstopford <phil.stopford@gmail.com>
2021-08-20 16:09:18 -0500

    Square Felippa

---
+++ BurkhardtTest/Square/SquareFelippaRuleTest/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Burkardt.Square;
+
+namespace SquareFelippaRuleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    MAIN is the main program for SQUARE_FELIPPA_RULE_TEST.
+            //
+            //  Discussion:
+            //
+            //    SQUARE_FELIPPA_RULE_TEST tests the SQUARE_FELIPPA_RULE library.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license. 
+            //
+            //  Modified:
+            //
+            //    06 September 2014
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+        {
+            int degree_max;
+
+            Console.WriteLine("");
+            Console.WriteLine("SQUARE_FELIPPA_RULE_TEST");
+            Console.WriteLine("  Test the SQUARE_FELIPPA_RULE library.");
+
+            degree_max = 4;
+            FelippaRule.square_monomial_test(degree_max);
+
+            degree_max = 5;
+            FelippaRule.square_quad_test(degree_max);
+
+            Console.WriteLine("");
+            Console.WriteLine("SQUARE_FELIPPA_RULE_TEST");
+            Console.WriteLine("  Normal end of execution.");
+            Console.WriteLine("");
+        }
+    }
+}

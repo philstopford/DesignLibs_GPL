ebfa944c71d6e4a03f8b7dbc7dc76b8cb419154b
philstopford <phil.stopford@gmail.com>
2021-10-05 14:28:01 -0500

    Update Program.cs

--- BurkhardtTest/Triangle/TriangleSymmetricQuadratureTest/Program.cs
+++ BurkhardtTest/Triangle/TriangleSymmetricQuadratureTest/Program.cs
@@ -129,9 +129,63 @@ namespace TriangleSymmetricQuadratureTest
             Console.WriteLine("");
         }
 
-        private static int rule_full_size(int degree)
+        static int rule_full_size ( int mmax )
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    RULE_FULL_SIZE returns the full size of the requested quadrature rule.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU GPL license.
+            //
+            //  Modified:
+            //
+            //    30 June 2014
+            //
+            //  Author:
+            //
+            //    John Burkardt.
+            //
+            //  Reference:
+            //
+            //    Hong Xiao, Zydrunas Gimbutas,
+            //    A numerical algorithm for the construction of efficient quadrature
+            //    rules in two and higher dimensions,
+            //    Computers and Mathematics with Applications,
+            //    Volume 59, 2010, pages 663-676.
+            //
+            //  Parameters:
+            //
+            //    Input, int MMAX, the degree of the quadrature (the maximum degree of
+            //    the polynomials of two variables that are integrated
+            //    exactly.  1 <= MMAX <= 50.
+            //
+            //    Output, int RULE_FULL_SIZE, the number of nodes in the full rule.
+            //
         {
-            throw new NotImplementedException();
+            int[] nnodes = {
+                1,   3,   6,   6,   7,  12,  15,  16,  19,  25, 
+                28,  33,  37,  42,  49,  55,  60,  67,  73,  79, 
+                87,  96, 103, 112, 120, 130, 141, 150, 159, 171, 
+                181, 193, 204, 214, 228, 243, 252, 267, 282, 295, 
+                309, 324, 339, 354, 370, 385, 399, 423, 435, 453 };
+            int npts = 0;
+
+            if ( 1 <= mmax && mmax <= 50 )
+            {
+                npts = nnodes[mmax-1];
+            }
+            else
+            {
+                Console.WriteLine("");
+                Console.WriteLine("RULE_FULL_SIZE - Fatal error!");
+                Console.WriteLine("  Degree MMAX must be between 1 and 50.");
+            }
+
+            return npts;
         }
 
         public static void test01()

fe9efb6ac001875548d246cfe8b88fb1576b4e6f
philstopford <phil.stopford@gmail.com>
2021-10-20 16:20:30 -0500

    FEM 2D Poisson Rectangle Linear

--- Common/Burkardt/Solve/Solve.cs
+++ Common/Burkardt/Solve/Solve.cs
@@ -304,6 +304,155 @@ namespace Burkardt.SolveNS
             return x;
         }
 
+        public static double[] r8ge_fs_new(int n, double[] a, double[] b)
+
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    R8GE_FS_NEW factors and solves a R8GE system.
+            //
+            //  Discussion:
+            //
+            //    The R8GE storage format is used for a "general" M by N matrix.
+            //    A physical storage space is made for each logical entry.  The two
+            //    dimensional logical array is mapped to a vector, in which storage is
+            //    by columns.
+            //
+            //    R8GE_FS does not save the LU factors of the matrix, and hence cannot
+            //    be used to efficiently solve multiple linear systems, or even to
+            //    factor A at one time, and solve a single linear system at a later time.
+            //
+            //    R8GE_FS uses partial pivoting, but no pivot vector is required.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    04 December 2003
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Parameters:
+            //
+            //    Input, int N, the order of the matrix.
+            //    N must be positive.
+            //
+            //    Input/output, double A[N*N].
+            //    On input, A is the coefficient matrix of the linear system.
+            //    On output, A is in unit upper triangular form, and
+            //    represents the U factor of an LU factorization of the
+            //    original coefficient matrix.
+            //
+            //    Input, double B[N], the right hand side of the linear system.
+            //
+            //    Output, double R8GE_FS_NEW[N], the solution of the linear system.
+            //
+        {
+            int i;
+            int ipiv;
+            int j;
+            int jcol;
+            double piv;
+            double t;
+            double[] x;
+
+            x = new double[n];
+
+            for (i = 0; i < n; i++)
+            {
+                x[i] = b[i];
+            }
+
+            for (jcol = 1; jcol <= n; jcol++)
+            {
+                //
+                //  Find the maximum element in column I.
+                //
+                piv = Math.Abs(a[jcol - 1 + (jcol - 1) * n]);
+                ipiv = jcol;
+                for (i = jcol + 1; i <= n; i++)
+                {
+                    if (piv < Math.Abs(a[i - 1 + (jcol - 1) * n]))
+                    {
+                        piv = Math.Abs(a[i - 1 + (jcol - 1) * n]);
+                        ipiv = i;
+                    }
+                }
+
+                if (piv == 0.0)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("R8GE_FS_NEW - Fatal error!");
+                    Console.WriteLine("  Zero pivot on step " + jcol + "");
+                    return null;
+                }
+
+                //
+                //  Switch rows JCOL and IPIV, and X.
+                //
+                if (jcol != ipiv)
+                {
+                    for (j = 1; j <= n; j++)
+                    {
+                        t = a[jcol - 1 + (j - 1) * n];
+                        a[jcol - 1 + (j - 1) * n] = a[ipiv - 1 + (j - 1) * n];
+                        a[ipiv - 1 + (j - 1) * n] = t;
+                    }
+
+                    t = x[jcol - 1];
+                    x[jcol - 1] = x[ipiv - 1];
+                    x[ipiv - 1] = t;
+                }
+
+                //
+                //  Scale the pivot row.
+                //
+                t = a[jcol - 1 + (jcol - 1) * n];
+                a[jcol - 1 + (jcol - 1) * n] = 1.0;
+                for (j = jcol + 1; j <= n; j++)
+                {
+                    a[jcol - 1 + (j - 1) * n] = a[jcol - 1 + (j - 1) * n] / t;
+                }
+
+                x[jcol - 1] = x[jcol - 1] / t;
+                //
+                //  Use the pivot row to eliminate lower entries in that column.
+                //
+                for (i = jcol + 1; i <= n; i++)
+                {
+                    if (a[i - 1 + (jcol - 1) * n] != 0.0)
+                    {
+                        t = -a[i - 1 + (jcol - 1) * n];
+                        a[i - 1 + (jcol - 1) * n] = 0.0;
+                        for (j = jcol + 1; j <= n; j++)
+                        {
+                            a[i - 1 + (j - 1) * n] = a[i - 1 + (j - 1) * n] + t * a[jcol - 1 + (j - 1) * n];
+                        }
+
+                        x[i - 1] = x[i - 1] + t * x[jcol - 1];
+                    }
+                }
+            }
+
+            //
+            //  Back solve.
+            //
+            for (jcol = n; 2 <= jcol; jcol--)
+            {
+                for (i = 1; i < jcol; i++)
+                {
+                    x[i - 1] = x[i - 1] - a[i - 1 + (jcol - 1) * n] * x[jcol - 1];
+                }
+            }
+
+            return x;
+        }
+
         public static double[] r8ge_fss_new(int n, double[] a, int nb, double[] b )
 
         //****************************************************************************80

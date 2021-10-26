c40a6f67df3b6ba40482cd1a000707d7c60629cc
philstopford <phil.stopford@gmail.com>
2021-10-26 13:11:24 -0500

    HB IO Test

--- Common/Burkardt/Types/i4vec.cs
+++ Common/Burkardt/Types/i4vec.cs
@@ -1488,6 +1488,99 @@ namespace Burkardt.Types
             return a;
         }
 
+        public static void i4vec_print_part ( int n, int[] a, int max_print, string title )
+
+        //****************************************************************************80
+        //
+        //  Purpose:
+        //
+        //    I4VEC_PRINT_PART prints "part" of an I4VEC.
+        //
+        //  Discussion:
+        //
+        //    The user specifies MAX_PRINT, the maximum number of lines to print.
+        //
+        //    If N, the size of the vector, is no more than MAX_PRINT, then
+        //    the entire vector is printed, one entry per line.
+        //
+        //    Otherwise, if possible, the first MAX_PRINT-2 entries are printed,
+        //    followed by a line of periods suggesting an omission,
+        //    and the last entry.
+        //
+        //  Licensing:
+        //
+        //    This code is distributed under the GNU LGPL license.
+        //
+        //  Modified:
+        //
+        //    27 February 2010
+        //
+        //  Author:
+        //
+        //    John Burkardt
+        //
+        //  Parameters:
+        //
+        //    Input, int N, the number of entries of the vector.
+        //
+        //    Input, int A[N], the vector to be printed.
+        //
+        //    Input, int MAX_PRINT, the maximum number of lines
+        //    to print.
+        //
+        //    Input, string TITLE, a title.
+        //
+        {
+            int i;
+
+            if ( max_print <= 0 )
+            {
+                return;
+            }
+
+            if ( n <= 0 )
+            {
+                return;
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine(title + "");
+            Console.WriteLine("");
+
+            if ( n <= max_print )
+            {
+                for ( i = 0; i < n; i++ )
+                {
+                    Console.WriteLine("  " + i.ToString().PadLeft(8)
+                        + "  " + a[i].ToString().PadLeft(8) + "");
+                }
+            }
+            else if ( 3 <= max_print )
+            {
+                for ( i = 0; i < max_print - 2; i++ )
+                {
+                    Console.WriteLine("  " + i.ToString().PadLeft(8)
+                        + ": " + a[i].ToString().PadLeft(8) + "");
+                }
+                Console.WriteLine("  ........  ........");
+                i = n - 1;
+                Console.WriteLine("  " + i.ToString().PadLeft(8)
+                    + ": " + a[i].ToString().PadLeft(8) + "");
+            }
+            else
+            {
+                for ( i= 0; i < max_print - 1; i++ )
+                {
+                    Console.WriteLine("  " + i.ToString().PadLeft(8)
+                        + ": " + a[i].ToString().PadLeft(8) + "");
+                }
+                i = max_print - 1;
+                Console.WriteLine("  " + i.ToString().PadLeft(8)
+                    + ": " + a[i].ToString().PadLeft(8)
+                    + "  " + "...more entries...");
+            }
+        }
+        
         public static void i4vec_print(int n, int[] a, string title)
 
             //****************************************************************************80

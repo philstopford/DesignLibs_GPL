a1a54e5ffd3fcb2d7e7fbd913cf84246abace89b
philstopford <phil.stopford@gmail.com>
2021-11-12 11:14:36 -0600

    Spring ODE

---
+++ BurkhardtTest/OrdinaryDifferentialEquation/Spring2ODE/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Spring2ODE
+{
+    class Program
+    {
+        static void Main(string[] args)
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    MAIN is the main program for SPRING_ODE2.
+            //
+            //  Discussion:
+            //
+            //    This is a revision of the SPRING_ODE code.
+            //
+            //    In this revision of the program, we want to use vectors (C arrays) to 
+            //    store the data, and we want to write the data out to a file in a form 
+            //    that Gnuplot (or other plotting programs) can use.
+            //
+            //    Hooke's law for a spring observes that the restoring force is
+            //    proportional to the displacement: F = - k x
+            //
+            //    Newton's law relates the force to acceleration: F = m a
+            //
+            //    Putting these together, we have
+            //
+            //      m * d^2 x/dt^2 = - k * x
+            //
+            //    We can add a damping force with coefficient c:
+            //
+            //      m * d^2 x/dt^2 = - k * x - c * dx/dt
+            //
+            //    If we write this as a pair of first order equations for (x,v), we have
+            //
+            //          dx/dt = v
+            //      m * dv/dt = - k * x - c * v
+            //
+            //    and now we can approximate these values for small time steps.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    09 October 2013
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Parameters:
+            //
+            //    None
+            //
+        {
+            double c;
+            string command_filename = "spring_ode2_commands.txt";
+            List<string> command_unit = new List<string>();
+            string data_filename = "spring_ode2_data.txt";
+            List<string> data_unit = new List<string>();
+            double dt;
+            int i;
+            double k;
+            double m;
+            int n = 101;
+            double[] t = new double[101];
+            double t_final;
+            double[] v = new double[101];
+            double[] x = new double[101];
+
+            Console.WriteLine("");
+            Console.WriteLine("SPRING_ODE2");
+            Console.WriteLine("  Approximate the solution of a spring equation.");
+            Console.WriteLine("  Write data to a file for use by gnuplot.");
+            //
+            //  Data
+            //
+            m = 1.0;
+            k = 1.0;
+            c = 0.3;
+            t_final = 20.0f;
+            dt = t_final / (double) (n - 1);
+            //
+            //  Store the initial conditions in entry 0.
+            //
+            t[0] = 0.0;
+            x[0] = 1.0;
+            v[0] = 0.0;
+            //
+            //  Compute the approximate solution at equally spaced times 
+            //  in entries 1 through N-1.
+            //
+            for (i = 1; i < n; i++)
+            {
+                t[i] = (double) (i) * t_final / (double) (n - 1);
+                x[i] = x[i - 1] + dt * v[i - 1];
+                v[i] = v[i - 1] + (dt / m) * (-k * x[i - 1] - c * v[i - 1]);
+            }
+
+            //
+            //  Create the plot data file.
+            //
+            for (i = 0; i < n; i++)
+            {
+                data_unit.Add("  " + t[i].ToString().PadLeft(14)
+                                   + "  " + x[i].ToString().PadLeft(14)
+                                   + "  " + v[i].ToString().PadLeft(14) + "");
+            }
+
+            File.WriteAllLines(data_filename, data_unit);
+
+            Console.WriteLine("  Created data file \"" + data_filename + "\".");
+            //
+            //  Create the plot command file.
+            //
+            command_unit.Add("# " + command_filename + "");
+            command_unit.Add("#");
+            command_unit.Add("# Usage:");
+            command_unit.Add("#  gnuplot < " + command_filename + "");
+            command_unit.Add("#");
+            command_unit.Add("set term png");
+            command_unit.Add("set output 'xv_time.png'");
+            command_unit.Add("set xlabel '<--- T --->'");
+            command_unit.Add("set ylabel '<--- X(T), V(T) --->'");
+            command_unit.Add("set title 'Position and Velocity versus Time'");
+            command_unit.Add("set grid");
+            command_unit.Add("set style data lines");
+            command_unit.Add("plot '" + data_filename
+                                      + "' using 1:2 lw 3 linecolor rgb 'blue',"
+                                      + " '' using 1:3 lw 3 linecolor rgb 'red'");
+            command_unit.Add("set output 'xv_phase.png'");
+            command_unit.Add("set xlabel '<--- X(T) --->'");
+            command_unit.Add("set ylabel '<--- V(T) --->'");
+            command_unit.Add("set title 'Position versus Velocity'");
+            command_unit.Add("set grid");
+            command_unit.Add("set style data lines");
+            command_unit.Add("plot '" + data_filename
+                                      + "' using 2:3 lw 3 linecolor rgb 'green'");
+            command_unit.Add("quit");
+
+            File.WriteAllLines(command_filename, command_unit);
+            Console.WriteLine("  Created command file '" + command_filename + "'");
+
+            Console.WriteLine("");
+            Console.WriteLine("SPRING_ODE2:");
+            Console.WriteLine("  Normal end of execution.");
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ BurkhardtTest/OrdinaryDifferentialEquation/SpringODE/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+
+namespace SpringODE
+{
+    class Program
+    {
+        static void Main(string[] args)
+            //****************************************************************************80
+            //
+            //  Purpose:
+            //
+            //    MAIN is the main program for SPRING_ODE.
+            //
+            //  Discussion:
+            //
+            //    This is a simple example of how to plot when you don't have a plotter.
+            //    This is a particular kind of "ASCII graphics", or "typewriter graphics"
+            //    or "lineprinter graphics", and shows you how valuable an illustration 
+            //    can be, even when it's as crude as this example.
+            //
+            //    Hooke's law for a spring observes that the restoring force is
+            //    proportional to the displacement: F = - k x
+            //
+            //    Newton's law relates the force to acceleration: F = m a
+            //
+            //    Putting these together, we have
+            //
+            //      m * d^2 x/dt^2 = - k * x
+            //
+            //    We can add a damping force with coefficient c:
+            //
+            //      m * d^2 x/dt^2 = - k * x - c * dx/dt
+            //
+            //    If we write this as a pair of first order equations for (x,v), we have
+            //
+            //          dx/dt = v
+            //      m * dv/dt = - k * x - c * v
+            //
+            //    and now we can approximate these values for small time steps.
+            //
+            //    Note that the plotting assumes that the value of X will always be
+            //    between -1 and +1.  If the initial condition uses V = 0, and X starts
+            //    between -1 and +1, then this will be OK.
+            //
+            //  Licensing:
+            //
+            //    This code is distributed under the GNU LGPL license.
+            //
+            //  Modified:
+            //
+            //    15 May 2012
+            //
+            //  Author:
+            //
+            //    John Burkardt
+            //
+            //  Parameters:
+            //
+            //    None
+            //
+        {
+            float c;
+            float dt;
+            int i;
+            int j;
+            float k;
+            float m;
+            int n;
+            int p;
+            float t_final;
+            float v;
+            float v_old;
+            float x;
+            float x_old;
+            char[] z = new char[21];
+
+            Console.WriteLine("");
+            Console.WriteLine("SPRING_ODE");
+            Console.WriteLine("  Approximate the solution of a spring equation.");
+            Console.WriteLine("  Display the solution with line printer graphics.");
+            Console.WriteLine("");
+            //
+            //  Data
+            //
+            m = 1.0f;
+            k = 1.0f;
+            c = 0.3f;
+            t_final = 20.0f;
+            n = 100;
+            dt = t_final / (float) (n);
+            //
+            //  Initial conditions.
+            //
+            x = 1.0f;
+            v = 0.0f;
+            //
+            //  Compute the approximate solution at equally spaced times.
+            //
+            for (i = 0; i <= n; i++)
+            {
+                x_old = x;
+                v_old = v;
+
+                //  t = ( float ) ( i ) * t_final / ( float ) ( n );
+                x = x_old + dt * v_old;
+                v = v_old + (dt / m) * (-k * x_old - c * v_old);
+                //
+                //  Approximate the position of X in [-1,+1] to within 1/10.
+                //
+                p = (int) (10 * (1.0 + x));
+
+                if (p < 0)
+                {
+                    p = 0;
+                }
+                else if (20 < p)
+                {
+                    p = 20;
+                }
+
+                //
+                //  Fill in the next line of the plot, placing 'x' in the p position.
+                //
+                for (j = 0; j <= 20; j++)
+                {
+                    if ((i % 10) == 0)
+                    {
+                        z[j] = '-';
+                    }
+                    else
+                    {
+                        z[j] = ' ';
+                    }
+                }
+
+                z[0] = '|';
+                z[5] = '.';
+                z[10] = '+';
+                z[15] = '.';
+                z[20] = '|';
+
+                z[p] = 'x';
+                string cout = "";
+                for (j = 0; j <= 20; j++)
+                {
+                    cout += z[j];
+                }
+
+                Console.WriteLine(cout);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("SPRING_ODE:");
+            Console.WriteLine("  Normal end of execution.");
+            Console.WriteLine("");
+        }
+    }
+}

54055d1ea4b96203749ed86fc15c2bfeded3e94d
philstopford <phil.stopford@gmail.com>
2021-11-23 18:08:10 -0600

    Update Program.cs

--- Common/gdsTest/Program.cs
+++ Common/gdsTest/Program.cs
@@ -4,10 +4,10 @@ namespace gdsTest;
 
 internal class Program
 {
-    private static void Main(string[] args)
+    private static void Main()
     {
         // string filename = "C:\\Users\\phil\\OneDrive\\Documents\\Visual Studio 2015\\Projects\\MonteCarlo_Csharp\\oasTest\\test.gds";
-        string filename = "C:\\Users\\phil\\OneDrive\\Documents\\Visual Studio 2015\\Projects\\MonteCarlo_Csharp\\gdsFiles\\clip1.gds";
+        const string filename = "C:\\Users\\phil\\OneDrive\\Documents\\Visual Studio 2015\\Projects\\MonteCarlo_Csharp\\gdsFiles\\clip1.gds";
         // string filename = "C:\\Users\\phil\\OneDrive\\Documents\\Visual Studio 2015\\Projects\\MonteCarlo_Csharp\\gdsFiles\\doetest.gds";
 
         GeoCore g = new();

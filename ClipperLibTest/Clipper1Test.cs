98e89c1f28095b436b6db4df999f2e0fddeca6cd
philstopford <phil.stopford@gmail.com>
2022-02-25 20:30:43 -0600

    Clean-up test

--- ClipperLibTest/Clipper1Test.cs
+++ ClipperLibTest/Clipper1Test.cs
@@ -1,6 +1,5 @@
 ﻿
 using ClipperLib1;
-using PolyTree = ClipperLib2.PolyTree;
 
 namespace ClipperLib1Test;
 
@@ -10,33 +9,10 @@ using Paths = List<List<IntPoint>>;
 public static class Clipper1Test
 {
     const double keyhole_sizing = 500;
-    public enum type { outer, cutter }
-
-    private static Paths[] pGetDecomposed(Paths source)
-    {
-        Paths[] ret = new Paths[2];
-        ret[0] = new Paths();
-        ret[1] = new Paths();
-
-        foreach (Path t in source)
-        {
-            int r = (int)type.outer;
-            if (!Clipper.Orientation(t))
-            {
-                r = (int)type.cutter;
-            }
-
-            ret[r].Add(new Path(t));
-        }
-
-        return ret;
-    }
-
     public static void test1()
     {
         Console.WriteLine("Clipper1 Test1");
-        Path outer = new();
-        /*
+        Path outer = new()
         {
             new IntPoint(-200000, -200000),
             new IntPoint(200000, -200000),
@@ -44,28 +20,8 @@ public static class Clipper1Test
             new IntPoint(-200000, 200000),
             new IntPoint(-200000, -200000)
         };
-        */
-        int x = -200000;
-        int y = -200000;
-        for (x = -200000; x <= 200000; x += 10000)
-        {
-            outer.Add(new IntPoint(x, y));
-        }
-        for (; y <= 200000; y += 10000)
-        {
-            outer.Add(new IntPoint(x, y));
-        }
-        for (; x >= -200000; x -= 10000)
-        {
-            outer.Add(new IntPoint(x, y));
-        }
-        for (; y >= -200000; y -= 10000)
-        {
-            outer.Add(new IntPoint(x, y));
-        }
 
-        Path inner1 = new();
-        /*
+        Path inner1 = new()
         {
             new IntPoint(-100000, -100000),
             new IntPoint(-100000, 100000),
@@ -73,26 +29,6 @@ public static class Clipper1Test
             new IntPoint(100000, -100000),
             new IntPoint(-100000, -100000)
         };
-        */        
-        x = -100000;
-        y = -100000;
-        for (; y <= 100000; y += 10000)
-        {
-            inner1.Add(new IntPoint(x, y));
-        }
-        for (; x <= 100000; x += 10000)
-        {
-            inner1.Add(new IntPoint(x, y));
-        }
-        for (; y >= -100000; y -= 10000)
-        {
-            inner1.Add(new IntPoint(x, y));
-        }
-        for (; x >= -100000; x -= 10000)
-        {
-            inner1.Add(new IntPoint(x, y));
-        }
-        
         Paths kHSource = new()
         {
             outer,
--- ClipperLibTest/Clipper2Test.cs
+++ ClipperLibTest/Clipper2Test.cs
@@ -9,88 +9,27 @@ using Paths = List<List<Point64>>;
 public static class Clipper2Test
 {
     const double keyhole_sizing = 500;
-    public enum type { outer, cutter }
-
-    private static Paths[] pGetDecomposed(Paths source)
-    {
-        Paths[] ret = new Paths[2];
-        ret[0] = new Paths();
-        ret[1] = new Paths();
-
-        foreach (Path t in source)
-        {
-            int r = (int)type.outer;
-            if (!ClipperFunc.Orientation(t))
-            {
-                r = (int)type.cutter;
-            }
-
-            ret[r].Add(new Path(t));
-        }
-
-        return ret;
-    }
     public static void test1()
     {
         Console.WriteLine("Clipper2 Test1");
-        Path outer = new();
-        /*
+        Path outer = new()
         {
-            new IntPoint(-200000, -200000),
-            new IntPoint(200000, -200000),
-            new IntPoint(200000, 200000),
-            new IntPoint(-200000, 200000),
-            new IntPoint(-200000, -200000)
+            new Point64(-200000, -200000),
+            new Point64(200000, -200000),
+            new Point64(200000, 200000),
+            new Point64(-200000, 200000),
+            new Point64(-200000, -200000)
         };
-        */
-        int x = -200000;
-        int y = -200000;
-        for (x = -200000; x <= 200000; x += 10000)
-        {
-            outer.Add(new Point64(x, y));
-        }
-        for (; y <= 200000; y += 10000)
-        {
-            outer.Add(new Point64(x, y));
-        }
-        for (; x >= -200000; x -= 10000)
-        {
-            outer.Add(new Point64(x, y));
-        }
-        for (; y >= -200000; y -= 10000)
-        {
-            outer.Add(new Point64(x, y));
-        }
 
-        Path inner1 = new();
-        /*
+        Path inner1 = new()
         {
-            new IntPoint(-100000, -100000),
-            new IntPoint(-100000, 100000),
-            new IntPoint(100000, 100000),
-            new IntPoint(100000, -100000),
-            new IntPoint(-100000, -100000)
+            new Point64(-100000, -100000),
+            new Point64(-100000, 100000),
+            new Point64(100000, 100000),
+            new Point64(100000, -100000),
+            new Point64(-100000, -100000)
         };
-        */        
-        x = -100000;
-        y = -100000;
-        for (; y <= 100000; y += 10000)
-        {
-            inner1.Add(new Point64(x, y));
-        }
-        for (; x <= 100000; x += 10000)
-        {
-            inner1.Add(new Point64(x, y));
-        }
-        for (; y >= -100000; y -= 10000)
-        {
-            inner1.Add(new Point64(x, y));
-        }
-        for (; x >= -100000; x -= 10000)
-        {
-            inner1.Add(new Point64(x, y));
-        }
-     
+
         Paths kHSource = new()
         {
             outer,

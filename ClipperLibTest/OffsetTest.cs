6908b2fe032c922602c3470e746c9abd58d987a9
philstopford <phil.stopford@gmail.com>
2022-03-16 12:18:18 -0500

    Offset test

---
+++ ClipperLibTest/OffsetTest.cs
@@ -0,0 +1,50 @@
+﻿namespace ClipperLibTest;
+
+using Path64 = List<ClipperLib2.Point64>;
+using Paths64 = List<List<ClipperLib2.Point64>>;
+using Path = List<ClipperLib1.IntPoint>;
+using Paths = List<List<ClipperLib1.IntPoint>>;
+
+public static class OffsetTest
+{
+    private static Path64 test = new()
+    {
+        new (0, 0),
+        new (0, 8),
+        new (4, 8),
+        new (4, 4),
+        new (16, 4),
+        new (16, 16),
+        new (4, 16),
+        new (4, 10),
+        new (0, 10),
+        new (0,20),
+        new (20,20),
+        new (20,0)
+
+    };
+    
+    public static void compare()
+    {
+        Path test1 = new();
+        for (int pt = 0; pt < test.Count; pt++)
+        {
+            test1.Add(new ClipperLib1.IntPoint(test[pt].X, test[pt].Y));
+        }
+        ClipperLib1.ClipperOffset co1 = new();
+        co1.AddPath(test1, ClipperLib1.JoinType.jtMiter, ClipperLib1.EndType.etClosedPolygon);
+        Paths c1up = new();
+        co1.Execute(ref c1up, 2.0);
+        co1.Clear();
+        co1.AddPaths(c1up, ClipperLib1.JoinType.jtMiter, ClipperLib1.EndType.etClosedPolygon);
+        Paths c1down = new();
+        co1.Execute(ref c1down, -2.0);
+        
+        ClipperLib2.ClipperOffset co2 = new();
+        co2.AddPath(test, ClipperLib2.JoinType.Miter, ClipperLib2.EndType.Polygon);
+        Paths64 c2up = ClipperLib2.ClipperFunc.Paths64(co2.Execute(2.0));
+        co2.Clear();
+        co2.AddPaths(c2up, ClipperLib2.JoinType.Miter, ClipperLib2.EndType.Polygon);
+        Paths64 c2down = ClipperLib2.ClipperFunc.Paths64(co2.Execute(-2.0));
+    }
+}
--- ClipperLibTest/Program.cs
+++ ClipperLibTest/Program.cs
@@ -6,6 +6,8 @@ using ClipperLibTest;
 
 Console.WriteLine("Comparison tests for Clipper 1 and Clipper 2");
 
+OffsetTest.compare();
+
 STest.compare();
 
 SubtractionTest.compare();

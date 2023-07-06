890ecd31601e1b1265dac3069afbd31908234583
philstopford <phil.stopford@gmail.com>
2023-07-05 19:54:40 -0500

    Update skeleton.cs

--- Common/geoWrangler/skeleton.cs
+++ Common/geoWrangler/skeleton.cs
@@ -1,9 +1,19 @@
+using System;
+using System.Collections.Generic;
 using Clipper2Lib;
 
 namespace geoWrangler;
 
 public static partial class GeoWrangler
 {
+    private static void ZFillCallback(PointD bot1, PointD top1, PointD bot2, PointD top2, ref PointD pt)
+    {
+        if (pt.z > -100)
+        {
+            pt.z = -1; // Tag our intersection points.
+        }
+    }
+
     public static PathsD skeleton(PathsD source)
     {
         PathsD ret = new();
@@ -15,30 +25,109 @@ public static partial class GeoWrangler
         return ret;
     }
     
-    public static PathD skeleton(PathD source)
+    public static PathD skeleton(PathD source_)
     {
+        PathD source = new(source_);
         PathD working = new(source);
         working.Reverse();
+
+        const int multirays = 1;
+        const int totalrays_per_point = 1 + (2 * multirays);
         
-        RayCast rc = new(working, source, 1000, false);
+        RayCast rc = new(working, source, 1000, false, multisampleRayCount: multirays);
         PathsD rays = rc.getRays();
 
         ClipperD c = new();
+        c.ZCallback = ZFillCallback;
         c.AddOpenSubject(rays);
         c.AddClip(source);
 
         PathsD unused = new();
+        PathsD rays_clipped = new();
+
+        c.Execute(ClipType.Intersection, FillRule.EvenOdd, unused, rays_clipped);
+
+        rays_clipped.RemoveAt(rays_clipped.Count - 1);
         PathsD rays_c1 = new();
+        
+        foreach (PathD ray in rays_clipped)
+        {
+            // Get source index from ray
+            int index = (int)ray[0].z;
+            if (index >= -1)
+            {
+                index = (int)ray[1].z;
+            }
 
-        c.Execute(ClipType.Intersection, FillRule.EvenOdd, unused, rays_c1);
+            // Invalid ray - probably due to fully overlapping with an edge. Reject this ray and move on.
+            if (index > -100)
+            {
+                continue;
+            }
 
-        PathD median = new();
+            index = index + 100;
+            // Working is reversed from source, so let's compensate.
+            index = (source.Count-1) + index;
+
+            PathD ray_corrected = new();
+            ray_corrected.Add(new (ray[0].x, ray[0].y, index));
+            ray_corrected.Add(new (ray[1].x, ray[1].y, index));
+                
+            // Do we need to reverse the ray?
+            if ((ray_corrected[1].x == source[index].x) && (ray_corrected[1].y == source[index].y))
+            {
+                ray_corrected.Reverse();
+            }
+            
+            rays_c1.Add(ray_corrected);
+        }
+
+        Dictionary<string, PointD> initial = new();
+        Dictionary<string, PathD> displacements = new();
+        
         foreach (PathD ray in rays_c1)
         {
-            PointD v0 = new(ray[0]);
-            PointD dist = GeoWrangler.distanceBetweenPoints_point(ray[1], ray[0]);
-            PointD v1 = new(ray[0].x + (0.5 * dist.x), ray[0].y + (0.5 * dist.y));
-            median.Add(v1);
+            string key = "P" + ray[0].z;
+
+            if (!initial.ContainsKey(key))
+            {
+                initial.Add(key, new(ray[0]));
+            }
+            PointD dist = distanceBetweenPoints_point(ray[1], ray[0]);
+            if (!displacements.ContainsKey(key))
+            {
+                displacements.Add(key, new PathD());
+            }
+            displacements[key].Add(dist);
+        }
+
+        PathD median = new();
+
+        for (int i = 0; i < initial.Count; i++)
+        {
+            string key = "P" + i;
+            try
+            {
+                PointD start = new(initial[key]);
+
+                // Figure out our average displacement.
+                PointD displacement = new(0, 0);
+                int displacement_count = displacements[key].Count;
+                for (int d = 0; d < displacement_count; d++)
+                {
+                    displacement.x += displacements[key][d].x;
+                    displacement.y += displacements[key][d].y;
+                }
+
+                displacement.x /= displacement_count;
+                displacement.y /= displacement_count;
+
+                median.Add(new(start.x + (0.5 * displacement.x), start.y + (0.5 * displacement.y)));
+
+            }
+            catch (Exception e)
+            {
+            }
         }
 
         median = removeDuplicates(median);

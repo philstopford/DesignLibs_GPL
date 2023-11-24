47b8c2d5c96c6eb88f5a46b9c6cebd73bc51d132
philstopford <phil.stopford@gmail.com>
2023-11-23 23:28:21 -0600

    Add tests for GeoAnalysis
    Add tests for analysis library. Angle pending.

---
+++ geoAnalysisTest/Program.cs
@@ -0,0 +1,198 @@
+﻿using Clipper2Lib;
+using geoAnalysis;
+using NUnit.Framework;
+
+namespace geoAnalysisTest;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        testAreaHandler();
+        testAreaHandler_perPoly();
+        testDistanceHandler();
+        testDistanceHandler_multi();
+        testChordHandler();
+    }
+
+    static void testAreaHandler()
+    {
+        PathsD aPaths = new()
+        {
+            Clipper.MakePath(new double[]
+            {
+                0, 0,
+                0, 100,
+                100, 100,
+                100, 0
+            })
+        };
+        PathsD bPaths = new()
+        {
+            Clipper.MakePath(new double[]
+            {
+                -10, -10,
+                -10, 110,
+                110, 110,
+                110, -10,
+            })
+        };
+        AreaHandler aH = new AreaHandler(aPaths, bPaths, true, false);
+        Assert.AreEqual(100*100, aH.area);
+    }
+
+    static void testAreaHandler_perPoly()
+    {
+        PathsD aPaths = new()
+        {
+            Clipper.MakePath(new double[]
+            {
+                0, 0,
+                0, 10,
+                10, 10,
+                10, 0
+            }),
+            Clipper.MakePath(new double[]
+            {
+                20, 20,
+                20, 50,
+                50, 50,
+                50, 20
+            })
+        };
+        PathsD bPaths = new()
+        {
+            Clipper.MakePath(new double[]
+            {
+                -10, -10,
+                -10, 110,
+                110, 110,
+                110, -10,
+            })
+        };
+        AreaHandler aH = new AreaHandler(aPaths, bPaths, true, true);
+        Assert.AreEqual(10*10, aH.area);
+    }
+
+    static void testDistanceHandler()
+    {
+        PathsD aPaths = new()
+        {
+            Clipper.MakePath(new double[]
+            {
+                0, 0,
+                0, 10,
+                10, 10,
+                10, 0
+            })
+        };
+        PathsD bPaths = new()
+        {
+            Clipper.MakePath(new double[]
+            {
+                20, 20,
+                20, 50,
+                50, 50,
+                50, 20
+            })
+        };
+        DistanceHandler dH = new DistanceHandler(false, aPaths, bPaths, (int)DistanceHandler.spacingCalcModes.spacing, false);
+        Assert.AreEqual("14.142135623730951", dH.distanceString);
+        
+        DistanceHandler dH2 = new DistanceHandler(false, aPaths, bPaths, (int)DistanceHandler.spacingCalcModes.spacingOld, false);
+        Assert.AreEqual("14.142135623730951", dH2.distanceString);
+        
+        DistanceHandler dH3 = new DistanceHandler(false, aPaths, bPaths, (int)DistanceHandler.spacingCalcModes.enclosure, false);
+        Assert.AreEqual("-14.142135623730951", dH3.distanceString);
+
+        DistanceHandler dH4 = new DistanceHandler(false, aPaths, bPaths, (int)DistanceHandler.spacingCalcModes.enclosureOld, false);
+        Assert.AreEqual("-14.142135623730951", dH4.distanceString);
+    }
+    
+    static void testDistanceHandler_multi()
+    {
+        PathsD aPaths = new()
+        {
+            Clipper.MakePath(new double[]
+            {
+                0, 0,
+                0, 10,
+                10, 10,
+                10, 0
+            }),
+            Clipper.MakePath(new double[]
+            {
+                20, 20,
+                20, 107,
+                105, 107,
+                105, 20
+            })
+        };
+        PathsD bPaths = new()
+        {
+            Clipper.MakePath(new double[]
+            {
+                -10, -10,
+                -10, 110,
+                110, 110,
+                110, -10,
+            })
+        };
+        DistanceHandler dH = new DistanceHandler(false, aPaths, bPaths, (int)DistanceHandler.spacingCalcModes.spacing, false);
+        Assert.AreEqual("-5.830951894845301", dH.distanceString);
+        
+        DistanceHandler dH2 = new DistanceHandler(false, aPaths, bPaths, (int)DistanceHandler.spacingCalcModes.spacingOld, false);
+        Assert.AreEqual("-5.830951894845301", dH2.distanceString);
+        
+        DistanceHandler dH3 = new DistanceHandler(false, aPaths, bPaths, (int)DistanceHandler.spacingCalcModes.enclosure, false);
+        Assert.AreEqual("5.830951894845301", dH3.distanceString);
+        
+        DistanceHandler dH4 = new DistanceHandler(false, aPaths, bPaths, (int)DistanceHandler.spacingCalcModes.enclosureOld, false);
+        Assert.AreEqual("5.830951894845301", dH4.distanceString);
+    }
+    
+    static void testChordHandler()
+    {
+        PathsD aPaths = new()
+        {
+            Clipper.MakePath(new double[]
+            {
+                -100,-20,
+                -100, 20,
+                100,20,
+                100,-20
+            }),
+        };
+        PathsD bPaths = new()
+        {
+            Clipper.MakePath(new double[]
+            {
+                -30, -50,
+                -30, 50,
+                30, 50,
+                30, -50
+            })
+        };
+        // This only gets the aChordLengths
+        ChordHandler cH = new ChordHandler(aPaths, bPaths, 0.01, (int)ChordHandler.chordCalcElements.a);
+        Assert.AreEqual(60, cH.aChordLengths[0]);
+        Assert.AreEqual(60, cH.aChordLengths[1]);
+        Assert.AreEqual(0, cH.bChordLengths[0]);
+        Assert.AreEqual(0, cH.bChordLengths[1]);
+        
+        // This only gets the bChordLengths
+        ChordHandler cH2 = new ChordHandler(aPaths, bPaths, 0.01, (int)ChordHandler.chordCalcElements.b);
+        Assert.AreEqual(0, cH2.aChordLengths[0]);
+        Assert.AreEqual(0, cH2.aChordLengths[1]);
+        Assert.AreEqual(40, cH2.bChordLengths[0]);
+        Assert.AreEqual(40, cH2.bChordLengths[1]);
+        
+        // This gets the full set
+        ChordHandler cH3 = new ChordHandler(aPaths, bPaths, 0.01, (int)ChordHandler.chordCalcElements.b + 1);
+        Assert.AreEqual(60, cH3.aChordLengths[0]);
+        Assert.AreEqual(60, cH3.aChordLengths[1]);
+        Assert.AreEqual(40, cH3.bChordLengths[0]);
+        Assert.AreEqual(40, cH3.bChordLengths[1]);
+    }
+    
+}

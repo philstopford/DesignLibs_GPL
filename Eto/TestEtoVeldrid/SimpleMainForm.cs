ed4cd8d32a0b84abc861faf852139afc5311d37e
Phil Stopford <phil.stopford@gmail.com>
2024-02-10 23:28:57 -0600

    Also adding a simple test
    This currently fails as well.

---
+++ Eto/TestEtoVeldrid/SimpleMainForm.cs
@@ -0,0 +1,101 @@
+﻿using Eto.Forms;
+using Eto.Veldrid;
+using System;
+using Veldrid;
+using VeldridEto;
+
+namespace TestEtoVeldrid
+{
+	public partial class SimpleMainForm : Form
+	{
+		VeldridSurface Surface;
+
+		VeldridDriver Driver;
+
+		private bool _veldridReady = false;
+		public bool VeldridReady
+		{
+			get { return _veldridReady; }
+			private set
+			{
+				_veldridReady = value;
+
+				SetUpVeldrid();
+			}
+		}
+
+		private bool _formReady = false;
+		public bool FormReady
+		{
+			get { return _formReady; }
+			set
+			{
+				_formReady = value;
+
+				SetUpVeldrid();
+			}
+		}
+
+		public SimpleMainForm() : this(VeldridSurface.PreferredBackend)
+		{
+		}
+		public SimpleMainForm(GraphicsBackend backend)
+		{
+			InitializeComponent();
+
+			Shown += (sender, e) => FormReady = true;
+
+			// A depth buffer isn't strictly necessary for this project, which uses
+			// only 2D vertex coordinates, but it's helpful to create one for the
+			// sake of demonstration.
+			//
+			// The "improved" resource binding model changes how resource slots are
+			// assigned in the Metal backend, allowing it to work like the others,
+			// so the numbers used in calls to CommandList.SetGraphicsResourceSet
+			// will make more sense to developers used to e.g. OpenGL or Direct3D.
+			var options = new GraphicsDeviceOptions(
+				false,
+				PixelFormat.R32_Float,
+				false,
+				ResourceBindingModel.Improved);
+
+			Surface = new VeldridSurface(backend, options);
+			Surface.Size = new Eto.Drawing.Size(200, 200);
+
+			Content = Surface;
+
+			OVPSettings unused = new();
+			Driver = new VeldridDriver(ref unused, ref Surface);
+			Driver.ExecutableDirectory = AppContext.BaseDirectory;
+			Driver.ShaderSubdirectory = "shaders";
+			/*
+			{
+				Surface = Surface
+			};
+			*/
+
+			Surface.VeldridInitialized += (sender, e) =>
+			{
+				Driver.SetUpVeldrid();
+
+				VeldridReady = true;
+			};
+
+			// TODO: Make this binding actually work both ways.
+			CmdAnimate.Bind<bool>("Checked", Driver, "Animate");
+			CmdClockwise.Bind<bool>("Checked", Driver, "Clockwise");
+		}
+
+		private void SetUpVeldrid()
+		{
+			if (!(FormReady && VeldridReady))
+			{
+				return;
+			}
+
+			Title = $"Veldrid backend: {Surface.Backend.ToString()}";
+
+			Driver.Clock.Start();
+		}
+	}
+}
---
+++ Eto/TestEtoVeldrid/SimpleMainForm.eto.cs
@@ -0,0 +1,44 @@
+using Eto.Drawing;
+using Eto.Forms;
+
+namespace TestEtoVeldrid
+{
+	public partial class SimpleMainForm : Form
+	{
+		public CheckCommand CmdAnimate { get; } = new CheckCommand
+		{
+			MenuText = "Animate",
+			ToolTip = "Click window content to toggle animation",
+			Checked = true
+		};
+		public CheckCommand CmdClockwise { get; } = new CheckCommand
+		{
+			MenuText = "&Clockwise",
+			ToolTip = "Press C to toggle direction",
+			Checked = true
+		};
+
+		private void InitializeComponent()
+		{
+			Title = "Veldrid in Eto";
+			ClientSize = new Size(400, 350);
+
+			var quitCommand = new Command { MenuText = "Quit", Shortcut = Application.Instance.CommonModifier | Keys.Q };
+			quitCommand.Executed += (sender, e) => Application.Instance.Quit();
+
+			var aboutCommand = new Command { MenuText = "About..." };
+			aboutCommand.Executed += (sender, e) => new AboutDialog().ShowDialog(this);
+
+			Menu = new MenuBar
+			{
+				Items =
+				{
+					new ButtonMenuItem { Text = "&File" },
+					new ButtonMenuItem { Text = "&View", Items = { CmdAnimate, CmdClockwise } }
+				},
+				QuitItem = quitCommand,
+				AboutItem = aboutCommand,
+			};
+		}
+	}
+}

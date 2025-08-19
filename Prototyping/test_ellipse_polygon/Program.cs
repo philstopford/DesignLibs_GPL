098cf6e85927a772ccf8bc3d883ab8e461ef7d40
philstopford <phil.stopford@gmail.com>
2025-08-19 15:11:02 -0700

    Add more complexity

--- Prototyping/test_ellipse_polygon/Program.cs
+++ Prototyping/test_ellipse_polygon/Program.cs
@@ -19,9 +19,11 @@ class QuadraticBezierSamplingSwitcher_Polygon
         original_path.Add(new(0,0));
         original_path.Add(new(0,100));
         original_path.Add(new(100,100));
+        original_path.Add(new(90,50));
         original_path.Add(new(100,0));
         original_path.Add(new(75,0));
         original_path.Add(new(75,50));
+        original_path.Add(new(50,40));
         original_path.Add(new(25,50));
         original_path.Add(new(25,0));
         original_path.Add(new(0,0));
